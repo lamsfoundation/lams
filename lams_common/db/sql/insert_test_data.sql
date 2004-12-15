a5ef2fafbee7fa548b0dbb3f06024236f07ba3c5
fyang <>
2004-12-15 22:33:26 +0000

    add some test data

--- lams_common/db/sql/insert_test_data.sql
+++ lams_common/db/sql/insert_test_data.sql
@@ -7,12 +7,15 @@ INSERT INTO lams_organisation VALUES (4, 'LAMS', 'Lams Project Team',3,3,NOW(),n
 INSERT INTO lams_organisation VALUES (5, 'MAMS', 'Mams Project Team',3,3,NOW(),null);
 
 INSERT INTO lams_user VALUES(1, 'sysadmin','sysadmin','Mr','Fei','Yang',null,null,null,'Sydney','NSW','Australia',null,null,null,null,'fyang@melcoe.mq.edu.au',0,NOW(),1,null,null);
-INSERT INTO lams_user VALUES(2, 'lams','lams','Mr','Kevin','Han',null,null,null,'Sydney','NSW','Australia',null,null,null,null,'fyang@melcoe.mq.edu.au',0,NOW(),1,null,null);
+INSERT INTO lams_user VALUES(2, 'lams','lams','Mr','Kevin','Han',null,null,null,'Sydney','NSW','Australia',null,null,null,null,'khan@melcoe.mq.edu.au',0,NOW(),2,null,null);
+INSERT INTO lams_user VALUES(3, 'demo','demo','Mr','Jacky','Fang',null,null,null,'Sydney','NSW','Australia',null,null,null,null,'jfang@melcoe.mq.edu.au',0,NOW(),3,null,null);
 
 INSERT INTO lams_user_organisation VALUES (1, 1, 1);
 INSERT INTO lams_user_organisation VALUES (2, 2, 2);
 INSERT INTO lams_user_organisation VALUES (3, 3, 2);
 INSERT INTO lams_user_organisation VALUES (4, 4, 2);
+INSERT INTO lams_user_organisation VALUES (5, 2, 3);
+INSERT INTO lams_user_organisation VALUES (6, 3, 3);
 
 INSERT INTO lams_user_organisation_role VALUES (1, 1, 1);
 INSERT INTO lams_user_organisation_role VALUES (2, 2, 2);
@@ -25,6 +28,9 @@ INSERT INTO lams_user_organisation_role VALUES (8, 4, 2);
 INSERT INTO lams_user_organisation_role VALUES (9, 4, 3);
 INSERT INTO lams_user_organisation_role VALUES (10, 4, 4);
 INSERT INTO lams_user_organisation_role VALUES (11, 4, 5);
+INSERT INTO lams_user_organisation_role VALUES (12, 5, 4);
+INSERT INTO lams_user_organisation_role VALUES (13, 5, 5);
+INSERT INTO lams_user_organisation_role VALUES (14, 6, 3);
 
 INSERT INTO lams_authentication_method VALUES (1, 'Lams', 1);
 INSERT INTO lams_authentication_method VALUES (2, 'WebAuth', 2);
