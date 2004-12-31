3f80f42891cd062d8d769854a57e7efeec02eb2a
fmalikoff <>
2004-12-31 21:37:35 +0000

    Checking in work done before Christmas in case of PC crash

--- lams_contentrepository/db/sql/create_content_repository_tables.sql
+++ lams_contentrepository/db/sql/create_content_repository_tables.sql
@@ -5,9 +5,55 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS lams_cr_node_version_property;
 DROP TABLE IF EXISTS lams_cr_node_version;
 DROP TABLE IF EXISTS lams_cr_node;
-DROP TABLE IF EXISTS lams_cr_workspace_tools;
-DROP TABLE IF EXISTS lams_cr_tool;
+DROP TABLE IF EXISTS lams_cr_workspace_credential;
+DROP TABLE IF EXISTS lams_cr_credential;
 DROP TABLE IF EXISTS lams_cr_workspace;
+
+
+--
+-- Table structure for table 'lams_cr_workspace'
+--
+
+CREATE TABLE lams_cr_workspace (
+  workspace_id bigint(20) unsigned NOT NULL auto_increment,
+  name varchar(255) NOT NULL default '0',
+  root_node_id bigint(20) unsigned default '0',
+  PRIMARY KEY  (workspace_id),
+  UNIQUE KEY workspace_id (workspace_id,name),
+  KEY name (name)
+) TYPE=InnoDB COMMENT='Content repository workspace';
+
+--
+-- Table structure for table 'lams_cr_credential'
+--
+
+CREATE TABLE lams_cr_credential (
+  credential_id bigint(20) unsigned NOT NULL auto_increment,
+  name varchar(255) NOT NULL default '0',
+  password varchar(255) NOT NULL default '0',
+  PRIMARY KEY  (credential_id),
+  UNIQUE KEY name (name)
+) TYPE=InnoDB COMMENT='Records the identification properties for a tool.';
+
+--
+-- Table structure for table 'lams_cr_workspace_credential'
+--
+
+CREATE TABLE lams_cr_workspace_credential (
+  workspace_id bigint(20) unsigned NOT NULL default '0',
+  credential_id bigint(20) unsigned NOT NULL default '0',
+  PRIMARY KEY  (credential_id,workspace_id),
+  INDEX (credential_id),
+  INDEX (workspace_id),
+  CONSTRAINT FK_lams_cr_workspace_credential_1 
+  		FOREIGN KEY (credential_id) 
+  		REFERENCES lams_cr_credential(credential_id) 
+  		ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT FK_lams_cr_workspace_credential_2 
+  		FOREIGN KEY (workspace_id) 
+  		REFERENCES lams_cr_workspace (workspace_id) 
+  		ON DELETE NO ACTION ON UPDATE NO ACTION
+) TYPE=InnoDB COMMENT='Maps which tools access which workspaces';
 
 ---
 --- Table structure for table 'lams_cr_node'
@@ -15,10 +61,15 @@ DROP TABLE IF EXISTS lams_cr_workspace;
 
 CREATE TABLE lams_cr_node (
   node_id bigint(20) unsigned NOT NULL auto_increment,
+  workspace_id bigint(20) unsigned NOT NULL default '0',
   path varchar(255) NOT NULL default '0',
   PRIMARY KEY  (node_id),
   UNIQUE KEY node_id (node_id),
-  KEY path (path)
+  KEY workspace_id (workspace_id),
+  CONSTRAINT FK_lams_cr_node_1 
+  		FOREIGN KEY (workspace_id) 
+  		REFERENCES lams_cr_workspace (workspace_id) 
+  		ON DELETE NO ACTION ON UPDATE NO ACTION
 ) TYPE=InnoDB COMMENT='The main table containing the node definition';
 
 --
@@ -37,7 +88,7 @@ CREATE TABLE lams_cr_node_version (
   created_date_time timestamp(14) NOT NULL,
   parent_node_id bigint(20) unsigned default '0',
   parent_version_id bigint(20) unsigned default '0',
-  PRIMARY KEY  (id),
+  PRIMARY KEY  (nv_id),
   INDEX (node_id, version_id),
   INDEX (parent_node_id),
   CONSTRAINT FK_lams_cr_node_version_1 
@@ -65,50 +116,5 @@ CREATE TABLE lams_cr_node_version_property (
   		REFERENCES lams_cr_node_version (node_id, version_id) 
   		ON DELETE NO ACTION ON UPDATE NO ACTION
 ) TYPE=InnoDB COMMENT='Records the property for a node';
-
---
--- Table structure for table 'lams_cr_workspace'
---
-
-CREATE TABLE lams_cr_workspace (
-  workspace_id bigint(20) unsigned NOT NULL auto_increment,
-  name varchar(255) NOT NULL default '0',
-  root_node_id bigint(20) unsigned default '0',
-  PRIMARY KEY  (workspace_id),
-  UNIQUE KEY workspace_id (workspace_id,name),
-  KEY name (name)
-) TYPE=InnoDB COMMENT='Content repository workspace';
---
--- Table structure for table 'lams_cr_tool'
---
-
-CREATE TABLE lams_cr_tool (
-  tool_id bigint(20) unsigned NOT NULL auto_increment,
-  name varchar(255) NOT NULL default '0',
-  password varchar(255) NOT NULL default '0',
-  PRIMARY KEY  (tool_id),
-  UNIQUE KEY id (tool_id,name),
-  KEY name (name)
-) TYPE=InnoDB COMMENT='Records the identification properties for a tool.';
-
---
--- Table structure for table 'lams_cr_workspace_tools'
---
-
-CREATE TABLE lams_cr_workspace_tools (
-  workspace_id bigint(20) unsigned NOT NULL default '0',
-  tool_id bigint(20) unsigned NOT NULL default '0',
-  PRIMARY KEY  (tool_id,workspace_id),
-  INDEX (tool_id),
-  INDEX (workspace_id),
-  CONSTRAINT FK_lams_cr_workspace_tools_1 
-  		FOREIGN KEY (tool_id) 
-  		REFERENCES lams_cr_tool(tool_id) 
-  		ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT FK_lams_cr_workspace_tools_2 
-  		FOREIGN KEY (workspace_id) 
-  		REFERENCES lams_cr_workspace (workspace_id) 
-  		ON DELETE NO ACTION ON UPDATE NO ACTION
-) TYPE=InnoDB COMMENT='Maps which tools access which workspaces';
 
 SET FOREIGN_KEY_CHECKS=1;
---
+++ lams_contentrepository/db/sql/insert_test_data.sql
@@ -0,0 +1,8 @@
+
+DELETE from lams_cr_workspace_credential  ;
+DELETE from lams_cr_credential ;
+DELETE from lams_cr_workspace  ;
+
+INSERT INTO lams_cr_credential (credential_id, name, password) VALUES (1, 'atool','atool');
+INSERT INTO lams_cr_workspace  (workspace_id, name) VALUES (1, 'atoolWorkspace');
+INSERT INTO lams_cr_workspace_credential  (workspace_id, credential_id) VALUES  (1,1);
