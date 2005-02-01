4ef234ccd60438cc74e211bf2e84a094c237c1b9
dgarth <>
2005-02-01 05:31:07 +0000

    Replaced lessons with lesson

--- lams_common/src/java/org/lamsfoundation/lams/lesson/LessonClass.java
+++ lams_common/src/java/org/lamsfoundation/lams/lesson/LessonClass.java
@@ -9,64 +9,43 @@ package org.lamsfoundation.lams.lesson;
 import java.util.Set;
 import org.lamsfoundation.lams.learningdesign.Grouping;
 import org.lamsfoundation.lams.learningdesign.Group;
+
 /**
- * A type of Grouping that represents all the Learners in a Lesson.
- * The LessonClass is used as the default Grouping.
+ * A type of Grouping that represents all the Learners in a Lesson. The
+ * LessonClass is used as the default Grouping.
  * 
  * @author chris
  */
-public class LessonClass extends Grouping
-{
-    
-    private Group staffGroup;
+public class LessonClass extends Grouping {
 
-    /**
-     * Holds value of property lessons.
-     */
-    private Set lessons;
-    
-    
-    public Group getStaffGroup()
-    {
-        return this.staffGroup;
-    }
-    
-    public void setStaffGroup(Group staffGroup)
-    {
-        this.staffGroup =  staffGroup;
-    }
+	private Group staffGroup;
 
-    /**
-     * Getter for property lessons.
-     * @return Value of property lessons.
-     */
-    public Set getLessons()
-    {
+	private Lesson lesson;
 
-        return this.lessons;
-    }
+	/** Creates a new instance of LessonClass */
+	public LessonClass() {
+	}
+	
+	/** full constructor */
+	public LessonClass(Long groupingId, Integer groupingTypeId, Set groups,
+			Set activities, Group staffGroup, Lesson lesson) {
+		super(groupingId, groupingTypeId, groups, activities);
+		this.staffGroup = staffGroup;
+		this.lesson = lesson;
+	}
 
-    /**
-     * Setter for property lessons.
-     * @param lessons New value of property lessons.
-     */
-    public void setLessons(Set lessons)
-    {
+	public Group getStaffGroup() {
+		return this.staffGroup;
+	}
 
-        this.lessons = lessons;
-    }
-    
-    /** Creates a new instance of LessonClass */
-    public LessonClass()
-    {
-    }
-    
-    /** full constructor */
-    public LessonClass(Long groupingId, Integer groupingTypeId, Set groups, Set activities, Group staffGroup, Set lessons)
-    {
-        super(groupingId, groupingTypeId, groups, activities);
-        this.staffGroup = staffGroup;
-        this.lessons = lessons;
-    }
-    
+	public void setStaffGroup(Group staffGroup) {
+		this.staffGroup = staffGroup;
+	}
+
+	public Lesson getLesson() {
+		return lesson;
+	}
+	public void setLesson(Lesson lesson) {
+		this.lesson = lesson;
+	}
 }
