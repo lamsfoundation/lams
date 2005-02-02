a887603945b15c59b30f161c9f01cac5adf16f73
jfang <>
2005-02-02 01:09:21 +0000

    *** empty log message ***

---
+++ lams_common/src/java/org/lamsfoundation/lams/tool/ProgressOutputData.java
@@ -0,0 +1,32 @@
+/* ********************************************************************************
+ *  Copyright Notice
+ *  =================
+ * This file contains propriety information of LAMS Foundation. 
+ * Copying or reproduction with prior written permission is prohibited.
+ * Copyright (c) 2004 
+ * Created on 2004-12-7
+ ******************************************************************************** */
+
+package org.lamsfoundation.lams.tool;
+
+
+/**
+ * Value object that wraps the output of progress engine. As any modification
+ * of progress output data is not expected, this object needs to be remaining
+ * immutable.
+ * 
+ * @author Jacky Fang 2004-12-7
+ * 
+ */
+public class ProgressOutputData
+{
+
+    /**
+     * 
+     */
+    public ProgressOutputData()
+    {
+        super();
+    }
+
+}
