1a5c1e3865540907cd8f4503b067f111ae63755b
mminhas <>
2005-02-08 06:02:52 +0000

    Additional data access functions added

--- lams_common/src/java/org/lamsfoundation/lams/learningdesign/dao/ITransitionDAO.java
+++ lams_common/src/java/org/lamsfoundation/lams/learningdesign/dao/ITransitionDAO.java
@@ -3,6 +3,10 @@
  */
 package org.lamsfoundation.lams.learningdesign.dao;
 
+
+
+import java.util.List;
+
 import org.lamsfoundation.lams.learningdesign.Transition;
 
 /**
@@ -11,9 +15,27 @@ import org.lamsfoundation.lams.learningdesign.Transition;
 public interface ITransitionDAO extends IBaseDAO {
 	
 	/**
-	 * @param transitionID
-	 * @return Transition populated Transition object
+	 * @param ID The id of the required Transition
+	 * @return Returns the list of Transition objects with id = ID
 	 */
-	public Transition getTransitionById(Long transitionID);
+	public List getTransitionById(Integer ID);
+	
+	/**
+	 * @param transitionID The transitionID of the required Transition
+	 * @return Returns the Transition object corresponding to the transitionID
+	 */
+	public Transition getTransitionByTransitionID(Long transitionID);
+	
+	/**
+	 * @param toActivityID The to_activity_id of the required Transition
+	 * @return Returns the list of Transition objects where to_activity_id = activityID
+	 */
+	public List getTransitionByToActivityID(Long toActivityID);
+	
+	/**
+	 * @param fromActivityID The from_activity_id of the required Transition
+	 * @return Returns the list of Transition objects where from_activity_id = activityID
+	 */
+	public List getTransitionByfromActivityID(Long fromActivityID);
 
 }
--- lams_common/src/java/org/lamsfoundation/lams/learningdesign/dao/hibernate/TransitionDAO.java
+++ lams_common/src/java/org/lamsfoundation/lams/learningdesign/dao/hibernate/TransitionDAO.java
@@ -6,6 +6,11 @@
  */
 package org.lamsfoundation.lams.learningdesign.dao.hibernate;
 
+import java.util.List;
+
+import net.sf.hibernate.Hibernate;
+import net.sf.hibernate.type.Type;
+
 import org.lamsfoundation.lams.learningdesign.Transition;
 import org.lamsfoundation.lams.learningdesign.dao.ITransitionDAO;
 
@@ -17,12 +22,40 @@ import org.lamsfoundation.lams.learningdesign.dao.ITransitionDAO;
  */
 public class TransitionDAO extends BaseDAO implements ITransitionDAO {
 
+	private static final String TABLENAME ="lams_learning_transition";
+	private static final String FIND_BY_TO_ACTIVITY = "from " + TABLENAME +" in class " + Transition.class.getName()+ " where to_activity_id =?";
+	private static final String FIND_BY_FROM_ACTIVITY = "from " + TABLENAME +" in class " + Transition.class.getName()+ " where from_activity_id =?";
+	
+	
 	/* (non-Javadoc)
-	 * @see org.lamsfoundation.lams.learningdesign.dao.interfaces.ITransitionDAO#getTransitionById(java.lang.Long)
+	 * @see org.lamsfoundation.lams.learningdesign.dao.ITransitionDAO#getTransitionById(java.lang.Integer)
 	 */
-	public Transition getTransitionById(Long transitionID) {
+	public List getTransitionById(Integer ID) {		
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.lamsfoundation.lams.learningdesign.dao.ITransitionDAO#getTransitionByTransitionID(java.lang.Long)
+	 */
+	public Transition getTransitionByTransitionID(Long transitionID) {
+		return (Transition)this.getHibernateTemplate().get(Transition.class,transitionID);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.lamsfoundation.lams.learningdesign.dao.ITransitionDAO#getTransitionByToActivityID(java.lang.Long)
+	 */
+	public List getTransitionByToActivityID(Long toActivityID) {		
+		List list = this.getHibernateTemplate().find(FIND_BY_TO_ACTIVITY,new Object[]{toActivityID}, new Type[]{Hibernate.LONG});
+		return list;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.lamsfoundation.lams.learningdesign.dao.ITransitionDAO#getTransitionByfromActivityID(java.lang.Long)
+	 */
+	public List getTransitionByfromActivityID(Long fromActivityID) {
+		List list = this.getHibernateTemplate().find(FIND_BY_FROM_ACTIVITY,new Object[]{fromActivityID}, new Type[]{Hibernate.LONG});
+		return list;
 	}
 
 }
