11ff451cbe44553ec5f3539dba6ddd790e2bd249
dgarth <>
2005-02-08 04:38:32 +0000

    Holds the names for global struts action mappings

---
+++ lams_learning/src/java/org/lamsfoundation/lams/learning/web/util/ActionMappings.java
@@ -0,0 +1,21 @@
+/*
+ * Created on 8/02/2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.lamsfoundation.lams.learning.web.util;
+
+/**
+ * @author dgarth
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class ActionMappings {
+
+    public static final String ERROR = "error";
+    public static final String NO_SESSION_ERROR = "noSessionError";
+    public static final String DOUBLE_SUBMIT_ERROR = "doubleSubmitError";
+
+}
