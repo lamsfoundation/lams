36c2ed010b8a3893b895d781be1a219f2380e62d
dgarth <>
2005-02-21 05:32:49 +0000

    Added GPL header

--- lams_learning/test/java/org/lamsfoundation/lams/BaseTestCase.java
+++ lams_learning/test/java/org/lamsfoundation/lams/BaseTestCase.java
@@ -1,3 +1,24 @@
+/*
+Copyright (C) 2005 LAMS Foundation (http://lamsfoundation.org)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+USA
+
+http://www.gnu.org/licenses/gpl.txt
+*/
+
 package org.lamsfoundation.lams;
 
 import net.sf.hibernate.Session;
@@ -11,15 +32,10 @@ import org.springframework.orm.hibernate.SessionHolder;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import junit.framework.TestCase;
-/*
- * Created on Dec 4, 2004
- */
 
 /**
- * @author manpreet
+ * @author daveg
  *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 public class BaseTestCase extends TestCase {
 	
