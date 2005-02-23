5527b41a5f478e30cd0f6b100a217d1046604343
jfang <>
2005-02-23 04:39:23 +0000

    GPL license added

--- lams_monitoring/src/java/org/lamsfoundation/lams/monitoring/service/IMonitoringService.java
+++ lams_monitoring/src/java/org/lamsfoundation/lams/monitoring/service/IMonitoringService.java
@@ -1,12 +1,23 @@
-/* ********************************************************************************
- *  Copyright Notice
- *  =================
- * This file contains propriety information of LAMS Foundation. 
- * Copying or reproduction with prior written permission is prohibited.
- * Copyright (c) 2005 
- * Created on 2/02/2005
- ******************************************************************************** */
-
+/*
+ *Copyright (C) 2005 LAMS Foundation (http://lamsfoundation.org)
+ *
+ *This program is free software; you can redistribute it and/or modify
+ *it under the terms of the GNU General Public License as published by
+ *the Free Software Foundation; either version 2 of the License, or
+ *(at your option) any later version.
+ *
+ *This program is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *GNU General Public License for more details.
+ *
+ *You should have received a copy of the GNU General Public License
+ *along with this program; if not, write to the Free Software
+ *Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ *USA
+ *
+ *http://www.gnu.org/licenses/gpl.txt
+ */
 package org.lamsfoundation.lams.monitoring.service;
 
 import java.util.List;
--- lams_monitoring/src/java/org/lamsfoundation/lams/monitoring/service/MonitoringService.java
+++ lams_monitoring/src/java/org/lamsfoundation/lams/monitoring/service/MonitoringService.java
@@ -1,12 +1,23 @@
-/* ********************************************************************************
- *  Copyright Notice
- *  =================
- * This file contains propriety information of LAMS Foundation. 
- * Copying or reproduction with prior written permission is prohibited.
- * Copyright (c) 2005 
- * Created on 2/02/2005
- ******************************************************************************** */
-
+/*
+ *Copyright (C) 2005 LAMS Foundation (http://lamsfoundation.org)
+ *
+ *This program is free software; you can redistribute it and/or modify
+ *it under the terms of the GNU General Public License as published by
+ *the Free Software Foundation; either version 2 of the License, or
+ *(at your option) any later version.
+ *
+ *This program is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *GNU General Public License for more details.
+ *
+ *You should have received a copy of the GNU General Public License
+ *along with this program; if not, write to the Free Software
+ *Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ *USA
+ *
+ *http://www.gnu.org/licenses/gpl.txt
+ */
 package org.lamsfoundation.lams.monitoring.service;
 
 import java.util.HashSet;
@@ -28,9 +39,6 @@ import org.lamsfoundation.lams.tool.service.ILamsToolService;
 import org.lamsfoundation.lams.tool.service.LamsToolServiceException;
 import org.lamsfoundation.lams.usermanagement.Organisation;
 import org.lamsfoundation.lams.usermanagement.User;
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 
 /**
  * <p>This is the major service facade for all monitoring functionalities. It is 
@@ -42,9 +50,7 @@ import org.springframework.context.ApplicationContextAware;
  * 
  * @author Jacky Fang 2/02/2005
  */
-public class MonitoringService implements
-                              IMonitoringService,
-                              ApplicationContextAware
+public class MonitoringService implements IMonitoringService
 {
 
     //---------------------------------------------------------------------
@@ -54,7 +60,6 @@ public class MonitoringService implements
     private ILessonClassDAO lessonClassDAO;
     private ILamsToolService lamsToolService;
     private IAuthoringService authoringService;
-    private ApplicationContext context;
 
 
     //---------------------------------------------------------------------
@@ -82,14 +87,6 @@ public class MonitoringService implements
     public void setLessonDAO(ILessonDAO lessonDAO)
     {
         this.lessonDAO = lessonDAO;
-    }
-
-    /**
-     * @see org.springframework.context.ApplicationContextAware#setApplicationContext(org.springframework.context.ApplicationContext)
-     */
-    public void setApplicationContext(ApplicationContext context) throws BeansException
-    {
-        this.context = context;
     }
 
     /**
