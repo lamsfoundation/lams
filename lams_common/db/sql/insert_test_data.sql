b9fb56c5233bb5f2b5c9ec7a4e3bb0454612b12a
fmalikoff <>
2005-04-08 06:55:41 +0000

    Added test data for the content repository

--- lams_common/db/sql/insert_test_data.sql
+++ lams_common/db/sql/insert_test_data.sql
@@ -717,4 +717,31 @@ insert into lams_learning_design(learning_design_ui_id,description,title,first_a
 								values
 								(1,'Test Learning Design','Test Learning Design title',20,1,1,0,1,
 								'Help Text',1,'20041223','1.0',null,1);																
+
+-- insert the test data for the content repository
+INSERT INTO lams_cr_credential (credential_id, name, password) VALUES (1, 'atool','atool');
+INSERT INTO lams_cr_workspace  (workspace_id, name) VALUES (1, 'atoolWorkspace');
+INSERT INTO lams_cr_workspace_credential  (wc_id, workspace_id, credential_id) VALUES  (1, 1,1);
+INSERT INTO lams_cr_workspace  (workspace_id, name) VALUES (2, 'atoolWorkspace2');
+INSERT INTO lams_cr_workspace_credential  (wc_id, workspace_id, credential_id) VALUES  (2, 2,1);
+
+INSERT INTO lams_cr_node (node_id, workspace_id, type, created_date_time, path, next_version_id) VALUES (1, 1, 'DATANODE', 20050106103100, "/test",3);
+INSERT INTO lams_cr_node_version (nv_id, node_id, version_id, created_date_time)
+VALUES (1, 1, 1, 20050106103100);
+INSERT INTO lams_cr_node_version (nv_id, node_id, version_id, created_date_time)
+VALUES (2, 1, 2, 20050106110000);
+
+INSERT INTO lams_cr_node_version_property( id, nv_id, name, value, type) VALUES (1,1,"APPSPEC","abcd",1);
+INSERT INTO lams_cr_node_version_property( id, nv_id, name, value, type) VALUES (2,1,"VERSIONDESC","Initial Version.",1);
+INSERT INTO lams_cr_node_version_property( id, nv_id, name, value, type) VALUES (3,2,"APPSEC","The quick brown fox jumped over the lazy dog.",1);
+INSERT INTO lams_cr_node_version_property( id, nv_id, name, value, type) VALUES (4,2,"VERSIONDESC","The second version.",1);
+
+-- Note : this file node is missing its file as can't setup the repository
+INSERT INTO lams_cr_node (node_id, workspace_id, type, created_date_time, path, next_version_id) VALUES (2, 1, 'FILENODE', 20050106103100, "", 2);
+INSERT INTO lams_cr_node_version (nv_id, node_id, version_id, created_date_time)
+VALUES (3, 2, 1, 20050106103100);
+INSERT INTO lams_cr_node_version_property( id, nv_id, name, value, type) VALUES (5,3,"VERSIONDESC","Initial Version.",1);
+INSERT INTO lams_cr_node_version_property( id, nv_id, name, value, type) VALUES (6,3,"FILENAME","nofile.txt",1);
+INSERT INTO lams_cr_node_version_property( id, nv_id, name, value, type) VALUES (7,3,"MIMETYPE","unknown/unknown",1);														
+
 SET FOREIGN_KEY_CHECKS=1;
