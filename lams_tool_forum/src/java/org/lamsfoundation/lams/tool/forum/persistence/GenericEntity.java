1aa774d5ddb86cca5b65d6fa157b4faa737adf72
conradb <>
2005-06-17 05:13:20 +0000

    changed native to identity

--- lams_tool_forum/src/java/org/lamsfoundation/lams/tool/forum/persistence/GenericEntity.java
+++ lams_tool_forum/src/java/org/lamsfoundation/lams/tool/forum/persistence/GenericEntity.java
@@ -26,7 +26,7 @@ public class GenericEntity {
 	 * Gets the GenericEntity's id
 	 *
 	 * @return the id
-	 * @hibernate.id column="ID" generator-class="native"
+	 * @hibernate.id column="ID" generator-class="identity"
 	 */
 	public Long getId() {
 		return id;
