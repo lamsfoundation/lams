76633511daff2260c901aeaa285f5dadf36d7868
conradb <>
2005-06-17 04:35:51 +0000

    Permisiion Manager not yet implemented Skeleton code

---
+++ lams_tool_forum/src/java/org/lamsfoundation/lams/tool/forum/permissions/Permission.java
@@ -0,0 +1,15 @@
+package org.lamsfoundation.lams.tool.forum.permissions;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: conradb
+ * Date: 14/06/2005
+ * Time: 12:42:04
+ * To change this template use File | Settings | File Templates.
+ */
+public class Permission {
+    public static byte READ = 1;
+    public static byte WRITE = 2;
+    public static byte MODERATE = 3;
+    public static byte ALL = 4;
+}
---
+++ lams_tool_forum/src/java/org/lamsfoundation/lams/tool/forum/permissions/PermissionManager.java
@@ -0,0 +1,14 @@
+package org.lamsfoundation.lams.tool.forum.permissions;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: conradb
+ * Date: 17/06/2005
+ * Time: 14:22:37
+ * To change this template use File | Settings | File Templates.
+ */
+public interface PermissionManager {
+
+   public boolean hasPermission(Long userID, byte permission);
+
+}
