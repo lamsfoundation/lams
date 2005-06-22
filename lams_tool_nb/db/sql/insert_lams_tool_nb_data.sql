0fd3af4e71c48d1f797d0e809889e51519efb18c
mtruong <>
2005-06-22 06:34:47 +0000

    Database model has changed to account for the unique identifier field uid, present in both content and session table. The corresponding sql scripts have been updated to reflect this change as well.

--- lams_tool_nb/db/sql/insert_lams_tool_nb_data.sql
+++ lams_tool_nb/db/sql/insert_lams_tool_nb_data.sql
@@ -1,7 +1,8 @@
 
 -- test data for noticeboard content table
 
-INSERT INTO tl_lanb11_content (nb_content_id,
+INSERT INTO tl_lanb11_content ( uid,
+								nb_content_id,
 								title,
 								content,
 								online_instructions,
@@ -11,7 +12,8 @@ INSERT INTO tl_lanb11_content (nb_content_id,
 								creator_user_id,
 								date_created,
 								date_updated) 
-VALUES ('2500',
+VALUES (null,
+		'2500',
 		'Welcome',
 		'Welcome to these activities',
 		'Enter the online instructions here',
@@ -21,51 +23,17 @@ VALUES ('2500',
 		'2300',
 		now(),
 		NULL);
-		
-INSERT INTO tl_lanb11_session (	nb_session_id,
-								nb_content_id,
+
+
+INSERT INTO tl_lanb11_session ( uid,
+								nb_session_id,
+								nb_content_uid,
 								session_start_date,
 								session_end_date,
 								session_status)
-VALUES ('2400',
-		'2500',
+VALUES (null,
+		'2400',
+		LAST_INSERT_ID(),
 		now(),
 		now(),
-		'Inactive');
-
-
--- test data for noticeboard content table
-
-INSERT INTO tl_lanb11_content (nb_content_id,
-								title,
-								content,
-								online_instructions,
-								offline_instructions,
-								define_later,
-								force_offline,
-								creator_user_id,
-								date_created,
-								date_updated) 
-VALUES ('5000',
-		'Test Noticeboard',
-		'Test Noticeboard: Welcome to these activities',
-		'Please Enter the online instructions here',
-		'Please Enter the offline instructions here',
-		0,
-		0,
-		'2300',
-		now(),
-		NULL);
-		
-INSERT INTO tl_lanb11_session (	nb_session_id,
-								nb_content_id,
-								session_start_date,
-								session_end_date,
-								session_status)
-VALUES ('6000',
-		'5000',
-		now(),
-		now(),
-		'Inactive');
-
-
+		'NOT_ATTEMPTED');
--- lams_tool_nb/db/sql/lams_tool_nb.sql
+++ lams_tool_nb/db/sql/lams_tool_nb.sql
@@ -1,5 +1,6 @@
 CREATE TABLE lams.tl_lanb11_content (
-       nb_content_id BIGINT(20) NOT NULL
+       uid BIGINT(20) NOT NULL AUTO_INCREMENT
+     , nb_content_id BIGINT(20) UNIQUE NOT NULL
      , title VARCHAR(255)
      , content TEXT
      , online_instructions TEXT
@@ -9,18 +10,19 @@ CREATE TABLE lams.tl_lanb11_content (
      , creator_user_id BIGINT(20)
      , date_created DATETIME
      , date_updated DATETIME
-     , PRIMARY KEY (nb_content_id)
+     , PRIMARY KEY (uid)
 )TYPE=InnoDB;
 
 CREATE TABLE lams.tl_lanb11_session (
-       nb_session_id BIGINT(20) NOT NULL
-     , nb_content_id BIGINT(20) NOT NULL
+       uid BIGINT(20) NOT NULL AUTO_INCREMENT
+     , nb_session_id BIGINT(20) UNIQUE NOT NULL
+     , nb_content_uid BIGINT(20) NOT NULL
      , session_start_date DATETIME
      , session_end_date DATETIME
      , session_status VARCHAR(100)
-     , PRIMARY KEY (nb_session_id)
-     , INDEX (nb_content_id)
-     , CONSTRAINT FK_tl_lanb11_session_1 FOREIGN KEY (nb_content_id)
-                  REFERENCES lams.tl_lanb11_content (nb_content_id)
+     , PRIMARY KEY (uid)
+     , INDEX (nb_content_uid)
+     , CONSTRAINT FK_tl_lanb11_session_1 FOREIGN KEY (nb_content_uid)
+                  REFERENCES lams.tl_lanb11_content (uid)
 )TYPE=InnoDB;
 
