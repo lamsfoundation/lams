d982244c98351c4369d1401b1b4b27581ac4806f
fmalikoff <>
2005-07-27 04:24:36 +0000

    Added the Instructions popup window needed for the downloading of instructions files.

---
+++ lams_central/web/common.js
@@ -0,0 +1,18 @@
+<!-- Common Javascript functions for LAMS -->
+
+	var instructionsWindow = null;
+	/**
+	 * Launches the popup window for the instruction files
+	 */
+	function launchInstructionsPopup(url, formName, fieldName, type) {
+// add the mac test back in when we have the platform detection working.
+//		if(mac){
+//			window.open(url,'instructions','resizable,width=796,height=570,scrollbars');
+//		}else{
+			if(instructionsWindow && instructionsWindow.open && !instructionsWindow.closed){
+				instructionsWindow.close();
+			}
+			instructionsWindow = window.open(url,'instructions','resizable,width=796,height=570,scrollbars');
+			instructionsWindow.window.focus();
+//		}	
+	}
