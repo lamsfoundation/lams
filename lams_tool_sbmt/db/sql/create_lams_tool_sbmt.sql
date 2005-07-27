db64cb8384f669f4a03e51c601c20387166da46b
steven <>
2005-07-27 06:20:56 +0000

    Update sql script.

--- lams_tool_sbmt/db/sql/create_lams_tool_sbmt.sql
+++ lams_tool_sbmt/db/sql/create_lams_tool_sbmt.sql
@@ -1,11 +1,15 @@
 SET FOREIGN_KEY_CHECKS=0;
 
 CREATE TABLE tl_lasbmt11_content (
-       content_id BIGINT(20) NOT NULL
+       content_id BIGINT(20) NOT NULL DEFAULT 0
      , title VARCHAR(64) NOT NULL
-     , instructions VARCHAR(64) NOT NULL
-     , defineLater TINYINT(1)
-	 , runOffline TINYINT(1)
+     , instruction TEXT
+     , define_later TINYINT(1) NOT NULL DEFAULT 0
+     , run_offline TINYINT(1) NOT NULL DEFAULT 0
+     , offline_instruction TEXT
+     , online_instruction TEXT
+     , run_offline_instruction TEXT
+     , content_in_use TINYINT(1)
      , PRIMARY KEY (content_id)
 )TYPE=InnoDB;
 
@@ -15,38 +19,44 @@ CREATE TABLE tl_lasbmt11_report (
      , marks BIGINT(20)
      , date_marks_released DATETIME
      , PRIMARY KEY (report_id)
-)TYPE=InnoDB;
+);
 
 CREATE TABLE tl_lasbmt11_session (
        session_id BIGINT(20) NOT NULL AUTO_INCREMENT
-     , content_id BIGINT(20) NOT NULL
-     , status INT(11) NOT NULL
+     , status INT(11) NOT NULL DEFAULT 0
+     , content_id BIGINT(20)
      , PRIMARY KEY (session_id)
      , INDEX (content_id)
-     , CONSTRAINT FK_tl_lasbmt11_session_1 FOREIGN KEY (content_id)
-                  REFERENCES tl_lasbmt11_content (content_id)
+     , CONSTRAINT FKEC8C77C9FC4BEA1 FOREIGN KEY (content_id)
+                  REFERENCES tl_lasbmt11_content (content_id) ON DELETE NO ACTION ON UPDATE NO ACTION
+)TYPE=InnoDB;
+
+CREATE TABLE tl_lasbmt11_instruction_files (
+       file_id BIGINT(20) NOT NULL AUTO_INCREMENT
+     , uuid BIGINT(20)
+     , version_id BIGINT(20)
+     , content_id BIGINT(20)
+     , PRIMARY KEY (file_id)
+     , INDEX (content_id)
+     , CONSTRAINT FKA75538F9FC4BEA1 FOREIGN KEY (content_id)
+                  REFERENCES tl_lasbmt11_content (content_id) ON DELETE NO ACTION ON UPDATE NO ACTION
 )TYPE=InnoDB;
 
 CREATE TABLE tl_lasbmt11_submission_details (
        submission_id BIGINT(20) NOT NULL AUTO_INCREMENT
+     , filePath VARCHAR(250)
+     , fileDescription VARCHAR(250)
+     , date_of_submission DATETIME
+     , uuid BIGINT(20)
+     , version_id BIGINT(20)
+     , user_id BIGINT(20)
      , session_id BIGINT(20)
-     , report_id BIGINT(20)
-     , filePath VARCHAR(250) NOT NULL
-     , fileDescription VARCHAR(250) NOT NULL
-     , date_of_submission DATETIME NOT NULL
-     , uuid BIGINT(20) NOT NULL
-     , version_id BIGINT(20) NOT NULL
-     , user_id BIGINT(20) NOT NULL   
      , PRIMARY KEY (submission_id)
      , INDEX (session_id)
-     , CONSTRAINT FK_tl_lasbmt11_submission_details_1 FOREIGN KEY (session_id)
-                  REFERENCES tl_lasbmt11_session (session_id)
-     , INDEX (report_id)
-     , CONSTRAINT FK_tl_lasbmt11_submission_details_2 FOREIGN KEY (report_id)
-                  REFERENCES tl_lasbmt11_report (report_id)
+     , CONSTRAINT FK1411A53C630DDF64 FOREIGN KEY (session_id)
+                  REFERENCES tl_lasbmt11_session (session_id) ON DELETE NO ACTION ON UPDATE NO ACTION
 )TYPE=InnoDB;
 
-INSERT INTO `tl_lasbmt11_content` (content_id,title,instructions,defineLater,runOffline) values(1,"Java Submission","Submit your java programs",0,0);
+INSERT INTO `tl_lasbmt11_content` (content_id,title,instruction,define_later,run_offline) values(1,"Java Submission","Submit your java programs",0,0);
 INSERT INTO `tl_lasbmt11_session` (session_id,content_id,status) values(1,1,1);
 SET FOREIGN_KEY_CHECKS=1;
-
--- lams_tool_sbmt/db/sql/drop_lams_tool_sbmt.sql
+++ lams_tool_sbmt/db/sql/drop_lams_tool_sbmt.sql
@@ -4,4 +4,5 @@ DROP TABLE IF EXISTS tl_lasbmt11_session;
 DROP TABLE IF EXISTS tl_lasbmt11_report;
 DROP TABLE IF EXISTS tl_lasbmt11_submission_details;
 DROP TABLE IF EXISTS tl_lasbmt11_content;
+DROP TABLE IF EXISTS tl_lasbmt11_instruction_files;
 SET FOREIGN_KEY_CHECKS=1;
