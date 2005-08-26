6d258aecc0c812e10b46498c0a2edfc1d5c92f44
mtruong <>
2005-08-26 02:50:35 +0000

    *** empty log message ***

--- lams_tool_nb/db/sql/drop_lams_tool_nb.sql
+++ lams_tool_nb/db/sql/drop_lams_tool_nb.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS tl_lanb11_user;
 DROP TABLE IF EXISTS tl_lanb11_attachment;
+DROP TABLE IF EXISTS tl_lanb11_user;
 DROP TABLE IF EXISTS tl_lanb11_session;
 DROP TABLE IF EXISTS tl_lanb11_content;
 
