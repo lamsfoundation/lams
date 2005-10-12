d0ea2181118f9319a128291d34504f8ac54c7ea1
mtruong <>
2005-10-12 01:26:54 +0000

    *** empty log message ***

---
+++ lams_learning/src/java/org/lamsfoundation/lams/learning/export/ExportPortfolioConstants.java
@@ -0,0 +1,24 @@
+/*
+ * Created on Oct 11, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.lamsfoundation.lams.learning.export;
+
+/**
+ * @author mtruong
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class ExportPortfolioConstants {
+
+	private ExportPortfolioConstants()
+	{}
+	
+	public static final String MAIN_EXPORT_FILENAME = "export_main.html";
+	public static final String EXPORT_TMP_DIR = "/tempDirForExportFiles";
+	public static final String HOST = "http://localhost:8080";
+	
+}
