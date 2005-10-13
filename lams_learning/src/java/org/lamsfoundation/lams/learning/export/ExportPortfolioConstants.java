24da21a1c6c27b52079395a01057c25f1bda395b
mtruong <>
2005-10-13 07:46:26 +0000

    added subdirectory constant

--- lams_learning/src/java/org/lamsfoundation/lams/learning/export/ExportPortfolioConstants.java
+++ lams_learning/src/java/org/lamsfoundation/lams/learning/export/ExportPortfolioConstants.java
@@ -20,5 +20,6 @@ public class ExportPortfolioConstants {
 	public static final String MAIN_EXPORT_FILENAME = "export_main.html";
 	public static final String EXPORT_TMP_DIR = "/tempDirForExportFiles";
 	public static final String HOST = "http://localhost:8080";
+	public static final String SUBDIRECTORY_BASENAME = "Activity"; //subdirectory for each activity is ActivityXX where XX is the activity Id
 	
 }
