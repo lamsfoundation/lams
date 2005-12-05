ceedc33119051b5ff098397404fc6e8d53dab409
mtruong <>
2005-12-05 01:33:56 +0000

    added new constant - the url that points to the ExportNotSupportedServlet

--- lams_learning/src/java/org/lamsfoundation/lams/learning/export/ExportPortfolioConstants.java
+++ lams_learning/src/java/org/lamsfoundation/lams/learning/export/ExportPortfolioConstants.java
@@ -40,13 +40,15 @@ public class ExportPortfolioConstants {
 	{}
 	
 	public static final String DIR_SUFFIX_EXPORT = "export"; //will appear as the name of the tmp directory created. For eg. lamstmp_1129615396387_export
-	public static final String DIR_SUFFIX_ZIP = "exportZipFile";
+	public static final String DIR_SUFFIX_ZIP = "exportZipFile"; //suffix for the tmp directory. For eg. lamstmp_1129615396387_exportZipFile
 	public static final String MAIN_EXPORT_FILENAME = "export_main.html";
 	public static final String ZIP_FILENAME = "export.zip";
 	public static final String HOST = getServerURL();
 	public static final String SUBDIRECTORY_BASENAME = "Activity"; //subdirectory for each activity is ActivityXX where XX is the activity Id
 	public static final String PARAM_FILE_LOCATION = "fileLocation";
 	public static final String TEMP_DIRECTORY = FileUtil.TEMP_DIR;
+	public static final String URL_FOR_UNSUPPORTED_EXPORT = "learning/exportNotSupported";
+	
 	private static String getServerURL()
 	{
 	    return Configuration.get(ConfigurationKeys.SERVER_URL);
