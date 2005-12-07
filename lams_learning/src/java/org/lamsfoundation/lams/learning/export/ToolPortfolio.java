482dc0be4052e57bbad1a8dd38d93c87e5a9db54
mtruong <>
2005-12-07 00:35:49 +0000

    Previously known as the Portfolio object

---
+++ lams_learning/src/java/org/lamsfoundation/lams/learning/export/ToolPortfolio.java
@@ -0,0 +1,119 @@
+/***************************************************************************
+* Copyright (C) 2005 LAMS Foundation (http://lamsfoundation.org)
+* =============================================================
+* 
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+* USA
+* 
+* http://www.gnu.org/licenses/gpl.txt
+* ***********************************************************************/
+
+/*
+ * Created on Aug 30, 2005
+ *
+ */
+package org.lamsfoundation.lams.learning.export;
+
+/**
+ * @author mtruong
+ *
+ */
+public class ToolPortfolio {
+	
+	private Long activityId;
+	private String activityDescription;
+	private String activityName;
+	private String exportUrl;
+	/* The link to the tool page from the main export page */
+	private String toolLink;
+		
+	/**
+	 * @return Returns the toolLink.
+	 */
+	public String getToolLink() {
+		return toolLink;
+	}
+	/**
+	 * @param toolLink The toolLink to set.
+	 */
+	public void setToolLink(String toolLink) {
+		this.toolLink = toolLink;
+	}
+
+	public ToolPortfolio()
+	{
+		this.activityId = null;	
+		this.activityDescription = null;
+		this.activityName = null;
+		this.exportUrl = null;
+		this.toolLink = null;
+	}
+	
+	
+
+	/**
+	 * @return Returns the activityName.
+	 */
+	public String getActivityName() {
+		return activityName;
+	}
+	/**
+	 * @param activityName The activityName to set.
+	 */
+	public void setActivityName(String activityName) {
+		this.activityName = activityName;
+	}
+	
+	/**
+	 * @return Returns the exportUrl.
+	 */
+	public String getExportUrl() {
+		return exportUrl;
+	}
+	/**
+	 * @param exportUrl The exportUrl to set.
+	 */
+	public void setExportUrl(String exportUrl) {
+		this.exportUrl = exportUrl;
+	}
+		
+	/**
+	 * @return Returns the activityId.
+	 */
+	public Long getActivityId() {
+		return activityId;
+	}
+	/**
+	 * @param activityId The activityId to set.
+	 */
+	public void setActivityId(Long activityId) {
+		this.activityId = activityId;
+	}
+		
+	/**
+	 * @return Returns the activityDescription.
+	 */
+	public String getActivityDescription() {
+		return activityDescription;
+	}
+	/**
+	 * @param activityDescription The activityDescription to set.
+	 */
+	public void setActivityDescription(String activityDescription) {
+		this.activityDescription = activityDescription;
+	}
+	
+	
+}
