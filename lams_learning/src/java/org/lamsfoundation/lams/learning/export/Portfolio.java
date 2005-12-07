24f9e6e964b8d636125cdda9e7f77c4fee2447c5
mtruong <>
2005-12-07 00:35:28 +0000

    A portfolio object, now holds the attribute exportTmpDir - the temporary directory in which this export will place its files. and it contains an array of ToolPortfolio objects. ToolPortfolio was previously known as a  Portfolio object, now renamed to ToolPortfolio

--- lams_learning/src/java/org/lamsfoundation/lams/learning/export/Portfolio.java
+++ lams_learning/src/java/org/lamsfoundation/lams/learning/export/Portfolio.java
@@ -1,119 +1,51 @@
-/***************************************************************************
-* Copyright (C) 2005 LAMS Foundation (http://lamsfoundation.org)
-* =============================================================
-* 
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-* 
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-* 
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
-* USA
-* 
-* http://www.gnu.org/licenses/gpl.txt
-* ***********************************************************************/
-
 /*
- * Created on Aug 30, 2005
+ * Created on 5/12/2005
  *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
  */
 package org.lamsfoundation.lams.learning.export;
+
 
 /**
  * @author mtruong
  *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
  */
 public class Portfolio {
-	
-	private Long activityId;
-	private String activityDescription;
-	private String activityName;
-	private String exportUrl;
-	/* The link to the tool page from the main export page */
-	private String toolLink;
-		
-	/**
-	 * @return Returns the toolLink.
-	 */
-	public String getToolLink() {
-		return toolLink;
-	}
-	/**
-	 * @param toolLink The toolLink to set.
-	 */
-	public void setToolLink(String toolLink) {
-		this.toolLink = toolLink;
-	}
+    
+    private String exportTmpDir;
+    private ToolPortfolio[] toolPortfolios;
+    
+    public Portfolio()
+    {
+        this.exportTmpDir = null;
+        this.toolPortfolios = null;
+    }
 
-	public Portfolio()
-	{
-		this.activityId = null;	
-		this.activityDescription = null;
-		this.activityName = null;
-		this.exportUrl = null;
-		this.toolLink = null;
-	}
-	
-	
-
-	/**
-	 * @return Returns the activityName.
-	 */
-	public String getActivityName() {
-		return activityName;
-	}
-	/**
-	 * @param activityName The activityName to set.
-	 */
-	public void setActivityName(String activityName) {
-		this.activityName = activityName;
-	}
-	
-	/**
-	 * @return Returns the exportUrl.
-	 */
-	public String getExportUrl() {
-		return exportUrl;
-	}
-	/**
-	 * @param exportUrl The exportUrl to set.
-	 */
-	public void setExportUrl(String exportUrl) {
-		this.exportUrl = exportUrl;
-	}
-		
-	/**
-	 * @return Returns the activityId.
-	 */
-	public Long getActivityId() {
-		return activityId;
-	}
-	/**
-	 * @param activityId The activityId to set.
-	 */
-	public void setActivityId(Long activityId) {
-		this.activityId = activityId;
-	}
-		
-	/**
-	 * @return Returns the activityDescription.
-	 */
-	public String getActivityDescription() {
-		return activityDescription;
-	}
-	/**
-	 * @param activityDescription The activityDescription to set.
-	 */
-	public void setActivityDescription(String activityDescription) {
-		this.activityDescription = activityDescription;
-	}
-	
-	
+    /**
+     * @return Returns the exportTmpDir.
+     */
+    public String getExportTmpDir() {
+        return exportTmpDir;
+    }
+    /**
+     * @param exportTmpDir The exportTmpDir to set.
+     */
+    public void setExportTmpDir(String exportTmpDir) {
+        this.exportTmpDir = exportTmpDir;
+    }
+    /**
+     * @return Returns the toolPortfolios.
+     */
+    public ToolPortfolio[] getToolPortfolios() {
+        return toolPortfolios;
+    }
+    /**
+     * @param toolPortfolios The toolPortfolios to set.
+     */
+    public void setToolPortfolios(ToolPortfolio[] toolPortfolios) {
+        this.toolPortfolios = toolPortfolios;
+    }
 }
