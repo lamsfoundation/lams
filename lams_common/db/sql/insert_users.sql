5650a8e6cab167536ae00f6f29eb21bafd450a5f
fmalikoff <>
2005-12-09 11:22:39 +0000

    Added new test users (test1 through test4)

--- lams_common/db/sql/insert_users.sql
+++ lams_common/db/sql/insert_users.sql
@@ -9,7 +9,11 @@ insert into lams_workspace values(2,null,'MACQUIRE UNIVERSITY');
 insert into lams_workspace values(3,null,'MELCOE');
 insert into lams_workspace values(4,null,'LAMS');
 insert into lams_workspace values(5,null,'MAMS');
-insert into lams_workspace values(6,null,'Manpreet Minhas');
+insert into lams_workspace values(6,null,'Mary Morgan');
+insert into lams_workspace values(7,null,'One Test');
+insert into lams_workspace values(8,null,'Two Test');
+insert into lams_workspace values(9,null,'Three Test');
+insert into lams_workspace values(10,null,'Four Test');
 
 INSERT INTO lams_organisation VALUES (1, 'Root', 'Root Organisation',null,1,NOW(),1);
 INSERT INTO lams_organisation VALUES (2, 'Maquarie Uni', 'Macquarie University',1,2,NOW(),2);
@@ -30,6 +34,10 @@ INSERT INTO lams_user_organisation VALUES (8, 2, 4);
 INSERT INTO lams_user_organisation VALUES (9, 3, 4);
 INSERT INTO lams_user_organisation VALUES (10,5, 4);
 
+INSERT INTO lams_user_organisation VALUES (11,4, 5);
+INSERT INTO lams_user_organisation VALUES (12,4, 6);
+INSERT INTO lams_user_organisation VALUES (13,4, 7);
+INSERT INTO lams_user_organisation VALUES (14,4, 8);
 
 
 INSERT INTO lams_user_organisation_role VALUES (1, 1, 1);
@@ -47,7 +55,7 @@ INSERT INTO lams_user_organisation_role VALUES (12, 5, 4);
 INSERT INTO lams_user_organisation_role VALUES (13, 5, 5);
 INSERT INTO lams_user_organisation_role VALUES (14, 6, 3);
 
--- mmm user: author, learner, stagg
+-- mmm user: author, learner, staff
 INSERT INTO lams_user_organisation_role VALUES (15, 7, 3);
 INSERT INTO lams_user_organisation_role VALUES (16, 8, 3);
 INSERT INTO lams_user_organisation_role VALUES (17, 9, 3);
@@ -60,6 +68,20 @@ INSERT INTO lams_user_organisation_role VALUES (23, 7, 5);
 INSERT INTO lams_user_organisation_role VALUES (24, 8, 5);
 INSERT INTO lams_user_organisation_role VALUES (25, 9, 5);
 INSERT INTO lams_user_organisation_role VALUES (26, 10,5);
+
+-- test users: author, learner, staff
+INSERT INTO lams_user_organisation_role VALUES (27, 11, 3);
+INSERT INTO lams_user_organisation_role VALUES (28, 11, 4);
+INSERT INTO lams_user_organisation_role VALUES (29, 11, 5);
+INSERT INTO lams_user_organisation_role VALUES (30, 12, 3);
+INSERT INTO lams_user_organisation_role VALUES (31, 12, 4);
+INSERT INTO lams_user_organisation_role VALUES (32, 12, 5);
+INSERT INTO lams_user_organisation_role VALUES (33, 13, 3);
+INSERT INTO lams_user_organisation_role VALUES (34, 13, 4);
+INSERT INTO lams_user_organisation_role VALUES (35, 13, 5);
+INSERT INTO lams_user_organisation_role VALUES (36, 14, 3);
+INSERT INTO lams_user_organisation_role VALUES (37, 14, 4);
+INSERT INTO lams_user_organisation_role VALUES (38, 14, 5);
 
 -- themes and styles 
 INSERT INTO lams_css_style (style_id, theme_ve_id) VALUES (1,1);
@@ -90,6 +112,50 @@ values
 '0299999999','mmmmmmm@xx.os',
 0,'20041223',1,6,4);
 
+insert into lams_user (user_id,login,password,title,first_name,last_name,address_line_1,address_line_2,address_line_3,
+city,state,country,day_phone,evening_phone,mobile_phone,
+fax,email,
+disabled_flag,create_date,authentication_method_id,workspace_id,
+base_organisation_id)
+values
+(5,'test1','test1','Dr','One','Test','1','Test Ave',null,
+'Nowhere','NSW','Australia','0211111111','0211111112','0411111111',
+'0211111113','test1@xx.os',
+0,'20041223',1,7,4);
+
+insert into lams_user (user_id,login,password,title,first_name,last_name,address_line_1,address_line_2,address_line_3,
+city,state,country,day_phone,evening_phone,mobile_phone,
+fax,email,
+disabled_flag,create_date,authentication_method_id,workspace_id,
+base_organisation_id)
+values
+(6,'test2','test2','Dr','Two','Test','2','Test Ave',null,
+'Nowhere','NSW','Australia','0211111111','0211111112','0411111111',
+'0211111113','test2@xx.os',
+0,'20041223',1,8,4);
+
+insert into lams_user (user_id,login,password,title,first_name,last_name,address_line_1,address_line_2,address_line_3,
+city,state,country,day_phone,evening_phone,mobile_phone,
+fax,email,
+disabled_flag,create_date,authentication_method_id,workspace_id,
+base_organisation_id)
+values
+(7,'test3','test3','Dr','Three','Test','3','Test Ave',null,
+'Nowhere','NSW','Australia','0211111111','0211111112','0411111111',
+'0211111113','test3@xx.os',
+0,'20041223',1,9,4);
+
+insert into lams_user (user_id,login,password,title,first_name,last_name,address_line_1,address_line_2,address_line_3,
+city,state,country,day_phone,evening_phone,mobile_phone,
+fax,email,
+disabled_flag,create_date,authentication_method_id,workspace_id,
+base_organisation_id)
+values
+(8,'test4','test4','Dr','One','Test','4','Test Ave',null,
+'Nowhere','NSW','Australia','0211111111','0211111112','0411111111',
+'0211111113','test4@xx.os',
+0,'20041223',1,10,4);
+
 insert into lams_workspace_folder (workspace_folder_id,parent_folder_id,name,workspace_id,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id)
 values(1,null,'ROOT',1,1,'20041223','20041223',1);
 
@@ -106,10 +172,10 @@ insert into lams_workspace_folder (workspace_folder_id,parent_folder_id,name,wor
 values(5,1,'MAMS',5,1,'20041223','20041223',1);
 
 insert into lams_workspace_folder (workspace_folder_id,parent_folder_id,name,workspace_id,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id)
-values(6,4,'Manpreet Minhas Workspace',6,4,'20041223','20041223',1);
+values(6,4,'Mary Morgan Workspace',6,4,'20041223','20041223',1);
 
 insert into lams_workspace_folder (workspace_folder_id,parent_folder_id,name,workspace_id,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id)
-values(7,6,'Manpreet Minhas Run Sequences Folder',6,4,'20041223','20041223',2);
+values(7,6,'Mary Morgan Run Sequences Folder',6,4,'20041223','20041223',2);
 
 insert into lams_workspace_folder (workspace_folder_id,parent_folder_id,name,workspace_id,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id)
 values(8,6,'Documents',6,4,'20041223','20041223',1);
@@ -126,6 +192,29 @@ values(11,10,'Songs',6,4,'20041223','20041223',1);
 insert into lams_workspace_folder (workspace_folder_id,parent_folder_id,name,workspace_id,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id)
 values(12,10,'Movies',6,4,'20041223','20041223',1);
 
+insert into lams_workspace_folder (workspace_folder_id,parent_folder_id,name,workspace_id,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id)
+values(13,4,'One Test Workspace',6,4,'20041223','20041223',1);
+
+insert into lams_workspace_folder (workspace_folder_id,parent_folder_id,name,workspace_id,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id)
+values(14,13,'One Text Run Sequences Folder',6,4,'20041223','20041223',2);
+
+insert into lams_workspace_folder (workspace_folder_id,parent_folder_id,name,workspace_id,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id)
+values(15,4,'Two Test Workspace',6,4,'20041223','20041223',1);
+
+insert into lams_workspace_folder (workspace_folder_id,parent_folder_id,name,workspace_id,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id)
+values(16,15,'Two Text Run Sequences Folder',6,4,'20041223','20041223',2);
+
+insert into lams_workspace_folder (workspace_folder_id,parent_folder_id,name,workspace_id,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id)
+values(17,4,'Three Test Workspace',6,4,'20041223','20041223',1);
+
+insert into lams_workspace_folder (workspace_folder_id,parent_folder_id,name,workspace_id,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id)
+values(18,17,'Three Text Run Sequences Folder',6,4,'20041223','20041223',2);
+
+insert into lams_workspace_folder (workspace_folder_id,parent_folder_id,name,workspace_id,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id)
+values(19,4,'Four Test Workspace',6,4,'20041223','20041223',1);
+
+insert into lams_workspace_folder (workspace_folder_id,parent_folder_id,name,workspace_id,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id)
+values(20,19,'Four Text Run Sequences Folder',6,4,'20041223','20041223',2);
 
 update lams_workspace set root_folder_id = 1 where workspace_id = 1;
 update lams_workspace set root_folder_id = 2 where workspace_id = 2;
@@ -133,5 +222,9 @@ update lams_workspace set root_folder_id = 3 where workspace_id = 3;
 update lams_workspace set root_folder_id = 4 where workspace_id = 4;
 update lams_workspace set root_folder_id = 5 where workspace_id = 5;
 update lams_workspace set root_folder_id = 6 where workspace_id = 6;
+update lams_workspace set root_folder_id = 13 where workspace_id = 7;
+update lams_workspace set root_folder_id = 15 where workspace_id = 8;
+update lams_workspace set root_folder_id = 17 where workspace_id = 9;
+update lams_workspace set root_folder_id = 19 where workspace_id = 10;
 
 SET FOREIGN_KEY_CHECKS=1;
