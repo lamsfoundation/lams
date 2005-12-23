14196df25a45788be85b96856bba8e6b173a0fc8
mtruong <>
2005-12-23 01:55:02 +0000

    inserts test data to run nb not required for junit tests tho

---
+++ lams_tool_nb/db/sql/insert_test_data.sql
@@ -0,0 +1,25 @@
+INSERT INTO tl_lanb11_session ( uid,
+								nb_session_id,
+								nb_content_uid,
+								session_start_date,
+								session_end_date,
+								session_status)
+VALUES (null,
+		'455',
+		'1',
+		now(),
+		now(),
+		'INCOMPLETE');
+		
+INSERT INTO tl_lanb11_user ( 	uid,
+								user_id,
+								nb_session_uid,
+								username,
+								fullname,
+								user_status)
+VALUES (null,
+		'555',
+		LAST_INSERT_ID(),
+		'test',
+		'test',
+		'NOT_ATTEMPTED');
