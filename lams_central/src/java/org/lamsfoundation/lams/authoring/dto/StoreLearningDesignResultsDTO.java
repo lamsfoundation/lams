92616cbb09f7fd4e5cf7469f611a353e3c053526
mtruong <>
2006-01-24 23:03:53 +0000

    added learningDesignID

--- lams_central/src/java/org/lamsfoundation/lams/authoring/dto/StoreLearningDesignResultsDTO.java
+++ lams_central/src/java/org/lamsfoundation/lams/authoring/dto/StoreLearningDesignResultsDTO.java
@@ -13,6 +13,8 @@ public class StoreLearningDesignResultsDTO {
 	
 	private boolean valid; //indicates whether learning design is valid or not
 	
+	private Long learningDesignID;
+	
 	/*
 	 * If the design is valid, then the learningDesignID will be returned.
 	 * Otherwise will return a list of ValidationErrorDTOs
@@ -22,21 +24,24 @@ public class StoreLearningDesignResultsDTO {
 	public StoreLearningDesignResultsDTO()
 	{}
 	
-	public StoreLearningDesignResultsDTO(boolean valid, Vector messages)
+	public StoreLearningDesignResultsDTO(boolean valid, Vector messages, Long learningDesignID)
 	{
 		this.valid = valid;
 		this.messages = messages;
+		this.learningDesignID = learningDesignID;
 	}
 
 	
 	public StoreLearningDesignResultsDTO(boolean valid, Long learningDesignID)
 	{
 		this.valid = valid;
-		this.messages = new Vector();
-		messages.add(learningDesignID);
+		this.learningDesignID = learningDesignID;
 	}
 	
-	
+	public Long getLearningDesignID()
+	{
+		return learningDesignID;
+	}
 
 	public boolean isValid() {
 		return valid;
