291d564dcefeaa129b756f00cfaf2ab610a406e9
steven <>
2006-02-14 03:43:11 +0000

    Add more overload getMessage methods.

--- lams_common/src/java/org/lamsfoundation/lams/util/MessageService.java
+++ lams_common/src/java/org/lamsfoundation/lams/util/MessageService.java
@@ -24,27 +24,82 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.NoSuchMessageException;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.context.support.MessageSourceAccessor;
-
+/**
+ * Service class to help Service bean get i18n value quickly. The locale information will get from <code>LAMS_USER</code>
+ * table. For more detail see <code>org.lamsfoundation.lams.web.filter.LocaleFilter</code>.
+ * 
+ * @author Steve.Ni
+ * @version $Revision$
+ * @see org.springframework.context.support.MessageSourceAccessor
+ */
 public class MessageService {
     private MessageSourceAccessor messageAccessor;
     
-    private MessageSource messageSource;
     /**
+     * Set <code>MessageSource</code> from spring IoC. 
+     * @param messageSource
+     */
+    public void setMessageSource(MessageSource messageSource){
+    	messageAccessor = new MessageSourceAccessor(messageSource);
+    }
+    
+    /**
+     * @see org.springframework.context.support.MessageSourceAccessor#getMessage(java.lang.String)
+     * @param key
      * @return
      */
-    public MessageSource getMessageSource() {
-		return messageSource;
-	}
-    public void setMessageSource(MessageSource messageSource){
-    	this.messageSource = messageSource;
-    }
     public String getMessage(String key){
     	String message;
     	try {
-    		messageAccessor = new MessageSourceAccessor(messageSource);
     		message = messageAccessor.getMessage(key,LocaleContextHolder.getLocale());
 		} catch (NoSuchMessageException e) {
 			message = "??" + key + "??";
+		}
+		return message;
+    }
+    /** 
+     * @see org.springframework.context.support.MessageSourceAccessor#getMessage(java.lang.String, java.lang.String)
+     * @param key
+     * @param defaultMessage
+     * @return
+     */
+    public String getMessage(String key, String defaultMessage){
+    	String message = defaultMessage;
+    	try {
+    		message = messageAccessor.getMessage(key,defaultMessage,LocaleContextHolder.getLocale());
+		} catch (NoSuchMessageException e) {
+			message = defaultMessage;
+		}
+		return message;
+    }
+    /**
+     * @see org.springframework.context.support.MessageSourceAccessor#getMessage(java.lang.String, java.lang.Object[])
+     * @param key
+     * @param args
+     * @return
+     */
+    public String getMessage(String key, Object[] args){
+    	String message;
+    	try {
+    		message = messageAccessor.getMessage(key,args,LocaleContextHolder.getLocale());
+		} catch (NoSuchMessageException e) {
+			message = "??" + key + "??";
+		}
+		return message;
+    }
+    /**
+     * @see org.springframework.context.support.MessageSourceAccessor#getMessage(java.lang.String, java.lang.Object[], java.lang.String)
+     * @param key
+     * @param args
+     * @param defaultMessage
+     * @return
+     */
+    public String getMessage(String key, Object[] args, String defaultMessage){
+    	String message = defaultMessage;
+    	try {
+    		message = messageAccessor.getMessage(key,args,LocaleContextHolder.getLocale());
+		} catch (NoSuchMessageException e) {
+			message = defaultMessage;
 		}
 		return message;
     }
