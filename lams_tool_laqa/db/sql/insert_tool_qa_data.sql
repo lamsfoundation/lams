deb2242a85499ecbcf6c772d3ab1dbbbbdd1ffd0
ozgurd <>
2006-02-19 14:10:20 +0000

    changes for the proper work of tool deploy.

--- lams_tool_laqa/db/sql/insert_tool_qa_data.sql
+++ lams_tool_laqa/db/sql/insert_tool_qa_data.sql
@@ -1,21 +1,20 @@
-SET FOREIGN_KEY_CHECKS=0;
+
 -- test data for content table
 -- test data for content table
 INSERT INTO tl_laqa11_content (qa_content_id, 
-								creation_date,
-								questions_sequenced
+								creation_date
 								) 
 VALUES (10,
-		NOW(),1);
+		NOW());
 
 
 
 -- test data for content questions table
-INSERT INTO tl_laqa11_que_content (uid, 
+INSERT INTO tl_laqa11_que_content (qa_que_content_id, 
 									question, 
 									display_order, 
-									qa_content_id) VALUES (20,'What is the name of this activity?',1,1);
+									qa_content_id) VALUES (20,'What is the name of this activity?',1,10);
 
 
-SET FOREIGN_KEY_CHECKS=1;
+
 
