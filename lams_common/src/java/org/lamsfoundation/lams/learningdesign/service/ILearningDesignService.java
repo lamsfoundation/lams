ea6a26719b8e4de328d4e0d8bd4fd31456465027
mseaton <>
2006-02-21 06:00:14 +0000

    Interface for LearningDesignService

---
+++ lams_common/src/java/org/lamsfoundation/lams/learningdesign/service/ILearningDesignService.java
@@ -0,0 +1,44 @@
+/***************************************************************************
+ * Copyright (C) 2005 LAMS Foundation (http://lamsfoundation.org)
+ * =============================================================
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ * 
+ * http://www.gnu.org/licenses/gpl.txt
+ * ************************************************************************
+ */
+
+package org.lamsfoundation.lams.learningdesign.service;
+
+import java.util.Vector;
+import org.lamsfoundation.lams.learningdesign.LearningDesign;
+
+/**
+ * @author Mitchell Seaton
+ */
+
+public interface ILearningDesignService{
+	
+	/**
+	 * This method calls other validation methods which apply the validation 
+	 * rules to determine whether or not the learning design is valid.
+	 *
+	 * @param learningDesign
+	 * @return list of validation errors
+	 */
+	public Vector validateLearningDesign(LearningDesign learningDesign);
+	
+}
