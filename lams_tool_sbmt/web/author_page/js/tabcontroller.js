3d9fd1bc9dca4d0f903ed464cdefc7b9746a3bff
mseaton <>
2006-03-07 05:11:56 +0000

    Updated new tabcontroller to handle new display method for tabs (was underorder list, now uses tables)

--- lams_tool_sbmt/web/author_page/js/tabcontroller.js
+++ lams_tool_sbmt/web/author_page/js/tabcontroller.js
@@ -15,10 +15,23 @@ function selectTab(tabID) {
         selectedTabID = tabID;
         
     //change the old tab's class
-    document.getElementById("tab" + selectedTabID).className = "tabitem";
+    document.getElementById("tab" + selectedTabID).className = "tab tabcentre";
+    //swap images of side parts
+	var tl = document.getElementById("tableft_"+selectedTabID);
+	tl.src= imgRoot + themeName + "_tab_left.gif";
+	tl.height = 22;
+	var tr = document.getElementById("tabright_"+selectedTabID);
+	tr.src= imgRoot + themeName + "_tab_right.gif";
+	tr.height = 22;
     
     //change the new tab's class
-    document.getElementById("tab" + tabID).className = "tabitem_selected";    
+    document.getElementById("tab" + tabID).className = "tab tabcentre_selected";
+    var tl = document.getElementById("tableft_"+tabID);
+	tl.src= imgRoot + themeName + "_tab_s_left.gif";
+	tl.height = 25;
+	var tr = document.getElementById("tabright_"+tabID);
+	tr.src= imgRoot + themeName + "_tab_s_right.gif";
+	tr.height = 25;
     
     //save tabID as selectedTabID
     selectedTabID = tabID;
