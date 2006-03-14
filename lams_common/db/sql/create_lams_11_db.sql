fba4690045e6c2de4688618f0a7eb17575068bf3
mseaton <>
2006-03-14 06:02:27 +0000

    Updated for use in rebuild-db task.

--- lams_common/db/sql/create_lams_11_db.sql
+++ lams_common/db/sql/create_lams_11_db.sql
@@ -9,33 +9,33 @@ DROP DATABASE IF EXISTS lams;
 CREATE DATABASE lams DEFAULT CHARACTER SET utf8;
 SET FOREIGN_KEY_CHECKS=1;
 
-USE mysql;
+--USE mysql;
 
-DELETE FROM user WHERE user='';
+--DELETE FROM user WHERE user='';
 -- DELETE FROM user WHERE user IS NULL;
 
-DELETE FROM db WHERE user='';
+--DELETE FROM db WHERE user='';
 -- DELETE FROM db WHERE user IS NULL;
 
-GRANT ALL PRIVILEGES ON *.* TO lams@localhost IDENTIFIED BY 'lamsdemo';
-REVOKE PROCESS,SUPER ON *.* from lams@localhost;
+--GRANT ALL PRIVILEGES ON *.* TO lams@localhost IDENTIFIED BY 'lamsdemo';
+--REVOKE PROCESS,SUPER ON *.* from lams@localhost;
 
 --The hostname below should be replaced with the actual host name
-GRANT ALL PRIVILEGES ON *.* TO lams@'mams-yoichi' IDENTIFIED BY 'lamsdemo';
-REVOKE PROCESS,SUPER ON *.* from lams@'mams-yoichi';
+--GRANT ALL PRIVILEGES ON *.* TO lams@'mams-yoichi' IDENTIFIED BY 'lamsdemo';
+--REVOKE PROCESS,SUPER ON *.* from lams@'mams-yoichi';
 
-GRANT ALL PRIVILEGES ON *.* TO lams@'%' IDENTIFIED BY 'lamsdemo';
-REVOKE PROCESS,SUPER ON *.* from lams@'%';
+--GRANT ALL PRIVILEGES ON *.* TO lams@'%' IDENTIFIED BY 'lamsdemo';
+--REVOKE PROCESS,SUPER ON *.* from lams@'%';
 
-FLUSH PRIVILEGES;
+--FLUSH PRIVILEGES;
 
-GRANT ALL PRIVILEGES ON lams.* TO lams@localhost IDENTIFIED BY 'lamsdemo' WITH GRANT OPTION;
+--GRANT ALL PRIVILEGES ON lams.* TO lams@localhost IDENTIFIED BY 'lamsdemo' WITH GRANT OPTION;
 --The hostname below should be replaced with the actual host name
-GRANT ALL PRIVILEGES ON lams.* TO lams@'mams-yoichi' IDENTIFIED BY 'lamsdemo' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON lams.* TO lams@'%' IDENTIFIED BY 'lamsdemo' WITH GRANT OPTION;
+--GRANT ALL PRIVILEGES ON lams.* TO lams@'mams-yoichi' IDENTIFIED BY 'lamsdemo' WITH GRANT OPTION;
+--GRANT ALL PRIVILEGES ON lams.* TO lams@'%' IDENTIFIED BY 'lamsdemo' WITH GRANT OPTION;
 	
-FLUSH PRIVILEGES;
+--FLUSH PRIVILEGES;
 
-
+USE lams;
 
 SET FOREIGN_KEY_CHECKS=1;
