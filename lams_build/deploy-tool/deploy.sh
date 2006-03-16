f8355425628e9962bafaea9c14df2d36886f1935
fmalikoff <>
2006-03-16 22:44:56 +0000

    Updated mysql-connector, to go with MySQL 5.0

--- lams_build/deploy-tool/deploy.sh
+++ lams_build/deploy-tool/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 
-java -classpath lib/lams-tool-deploy.jar:lib/commons-configuration-1.1.jar:lib/commons-lang-2.0.jar:lib/commons-collections.jar:lib/commons-logging.jar:lib/commons-io-1.0.jar:lib/commons-dbutils-1.0.jar:lib/mysql-connector-java-3.1.7-bin.jar org.lamsfoundation.lams.tool.deploy.Deploy ./deploy.properties
+java -classpath lib/lams-tool-deploy.jar:lib/commons-configuration-1.1.jar:lib/commons-lang-2.0.jar:lib/commons-collections.jar:lib/commons-logging.jar:lib/commons-io-1.0.jar:lib/commons-dbutils-1.0.jar:lib/mysql-connector-java-3.1.12-bin.jar org.lamsfoundation.lams.tool.deploy.Deploy ./deploy.properties
