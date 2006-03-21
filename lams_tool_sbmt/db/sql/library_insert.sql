cf0462718e704009612fe0c8ecec16d517603a39
fmalikoff <>
2006-03-21 06:56:39 +0000

    Trying to convert binary to text.

---
+++ lams_tool_sbmt/db/sql/library_insert.sql
@@ -0,0 +1,16 @@
+-- $Id$
+
+INSERT INTO lams_learning_library
+(
+description,
+title,
+valid_flag,
+create_date_time
+)
+VALUES
+(
+'Uploading of files by learners, for review by teachers.',
+'Submit file',
+0,
+NOW()
+)
