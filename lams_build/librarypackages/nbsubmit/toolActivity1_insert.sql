5a19a3587723f3e78a4f2cb3bd3810fb51a57ec2
fmalikoff <>
2006-03-28 07:53:00 +0000

    Updated Tool Deployer so that that parallel activities, installed using the tool deployer, can also be I18N.

--- lams_build/librarypackages/nbsubmit/toolActivity1_insert.sql
+++ lams_build/librarypackages/nbsubmit/toolActivity1_insert.sql
@@ -4,11 +4,13 @@ learning_activity_type_id,grouping_id,order_id,define_later_flag,
 learning_design_id,learning_library_id,create_date_time,run_offline_flag,
 max_number_of_options,min_number_of_options,tool_id,tool_content_id,
 gate_activity_level_id,gate_start_time_offset,gate_end_time_offset,library_activity_ui_image,
-grouping_support_type_id, apply_grouping_flag,activity_category_id)
+grouping_support_type_id, apply_grouping_flag,activity_category_id,
+language_file)
 values
 (null,null,'Parallel NB Activity','Parallel NB Activity',null,null,${parent_activity_id},'Help Text for Activity',
 1,null,null,0,
 null,null,now(),0,
 null,null,${tool_id},null,
 null,null,null,'tool/lanb11/images/icon_noticeboard.swf',
-2,0,4);
+2,0,4,
+'org.lamsfoundation.lams.tool.noticeboard.ApplicationResources');
