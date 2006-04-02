48cbfb560fbb7e9865f8f21b2787f1dba32d7a6f
ozgurd <>
2006-04-02 20:30:22 +0000

    Varchar fck editor fields made TEXT

--- lams_tool_laqa/db/sql/drop_lams_tool_qa.sql
+++ lams_tool_laqa/db/sql/drop_lams_tool_qa.sql
@@ -1,10 +1,15 @@
--- $Id$  
 
 SET FOREIGN_KEY_CHECKS=0;
-DROP TABLE IF EXISTS tl_laqa11_usr_resp;
-DROP TABLE IF EXISTS tl_laqa11_que_usr;
-DROP TABLE IF EXISTS tl_laqa11_que_content;
-DROP TABLE IF EXISTS tl_laqa11_session;
-DROP TABLE IF EXISTS tl_laqa11_uploadedFile;
-DROP TABLE IF EXISTS tl_laqa11_content;
+DROP TABLE tl_laqa11_uploadedfile;
+DROP TABLE tl_laqa11_usr_resp;
+DROP TABLE tl_laqa11_que_content;
+DROP TABLE tl_laqa11_que_usr;
+DROP TABLE tl_laqa11_session;
+DROP TABLE tl_laqa11_content;
 SET FOREIGN_KEY_CHECKS=1;
+
+
+
+
+
+
