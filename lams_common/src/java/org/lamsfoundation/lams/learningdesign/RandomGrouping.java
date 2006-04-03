34b959260a0f8f8285793a4481a95ca3580eabc5
fmalikoff <>
2006-04-03 23:36:58 +0000

    Copyright statement change - only support GPL v2, not future versions. Missed the "version 2.0" the previous check in

--- lams_common/src/java/org/lamsfoundation/lams/learningdesign/RandomGrouping.java
+++ lams_common/src/java/org/lamsfoundation/lams/learningdesign/RandomGrouping.java
@@ -4,8 +4,8 @@
  * License Information: http://lamsfoundation.org/licensing/lams/2.0/
  * 
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation.
+ * it under the terms of the GNU General Public License version 2.0 
+ * as published by the Free Software Foundation.
  * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
