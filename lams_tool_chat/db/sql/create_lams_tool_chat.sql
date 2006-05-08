bbb7be61818ecfad3b0287dd3161ab98fa3b3ebb
asukkar <>
2006-05-08 04:38:34 +0000

    Added new fields in session and user to store the jabber room and jabber id.

--- lams_tool_chat/db/sql/create_lams_tool_chat.sql
+++ lams_tool_chat/db/sql/create_lams_tool_chat.sql
@@ -2,68 +2,60 @@
  
 SET FOREIGN_KEY_CHECKS=0;
 
-DROP TABLE IF EXISTS tl_lachat11_user;
-DROP TABLE IF EXISTS tl_lachat11_attachment;
-DROP TABLE IF EXISTS tl_lachat11_session;
-DROP TABLE IF EXISTS tl_lachat11_chat;
-
-CREATE TABLE tl_lachat11_chat (
-       uid BIGINT(20) NOT NULL AUTO_INCREMENT
-     , create_date DATETIME
-     , update_date DATETIME
-     , create_by BIGINT(20)
-     , title VARCHAR(255)
-     , instructions TEXT
-     , run_offline TINYINT(1)
-     , lock_on_finished TINYINT(1)
-     , online_instructions TEXT
-     , offline_instructions TEXT
-     , content_in_use TINYINT(1)
-     , define_later TINYINT(1)
-     , tool_content_id BIGINT(20)
-     , PRIMARY KEY (uid)
-)TYPE=InnoDB;
-CREATE INDEX content_id ON tl_lachat11_chat (tool_content_id ASC);
-
-CREATE TABLE tl_lachat11_session (
-       uid BIGINT(20) NOT NULL AUTO_INCREMENT
-     , session_end_date DATETIME
-     , session_start_date DATETIME
-     , status INT(11)
-     , chat_uid BIGINT(20)
-     , session_id BIGINT(20)
-     , session_name VARCHAR(250)
-     , PRIMARY KEY (uid)
-     , INDEX (chat_uid)
-     , CONSTRAINT FK_SESSION FOREIGN KEY (chat_uid)
-                  REFERENCES tl_lachat11_chat (uid) ON DELETE NO ACTION ON UPDATE NO ACTION
-)TYPE=InnoDB;
-
-CREATE TABLE tl_lachat11_attachment (
-       uid BIGINT(20) NOT NULL AUTO_INCREMENT
-     , file_version_id BIGINT(20)
-     , file_type VARCHAR(255)
-     , file_name VARCHAR(255)
-     , file_uuid BIGINT(20)
-     , create_date DATETIME
-     , chat_uid BIGINT(20)
-     , PRIMARY KEY (uid)
-     , INDEX (chat_uid)
-     , CONSTRAINT FK_ATTACHMENT FOREIGN KEY (chat_uid)
-                  REFERENCES tl_lachat11_chat (uid) ON DELETE NO ACTION ON UPDATE NO ACTION
-)TYPE=InnoDB;
-
-CREATE TABLE tl_lachat11_user (
-       uid BIGINT(20) NOT NULL AUTO_INCREMENT
-     , user_id BIGINT(20)
-     , last_name VARCHAR(255)
-     , first_name VARCHAR(255)
-     , session_id BIGINT(20)
-     , PRIMARY KEY (uid)
-     , INDEX (session_id)
-     , CONSTRAINT FK_SESSION_USER FOREIGN KEY (session_id)
-                  REFERENCES tl_lachat11_session (uid) ON DELETE NO ACTION ON UPDATE NO ACTION
-)TYPE=InnoDB;
+drop table if exists tl_lachat11_attachment;
+drop table if exists tl_lachat11_chat;
+drop table if exists tl_lachat11_session;
+drop table if exists tl_lachat11_user;
+create table tl_lachat11_attachment (
+   uid bigint not null auto_increment,
+   file_version_id bigint,
+   file_type varchar(255),
+   file_name varchar(255),
+   file_uuid bigint,
+   create_date datetime,
+   chat_uid bigint,
+   primary key (uid)
+);
+create table tl_lachat11_chat (
+   uid bigint not null auto_increment,
+   create_date datetime,
+   update_date datetime,
+   create_by bigint,
+   title varchar(255),
+   instructions text,
+   run_offline bit,
+   lock_on_finished bit,
+   online_instructions text,
+   offline_instructions text,
+   content_in_use bit,
+   define_later bit,
+   tool_content_id bigint,
+   primary key (uid)
+);
+create table tl_lachat11_session (
+   uid bigint not null auto_increment,
+   session_end_date datetime,
+   session_start_date datetime,
+   status integer,
+   session_id bigint,
+   session_name varchar(250),
+   chat_uid bigint,
+   jabber_room varchar(250),
+   primary key (uid)
+);
+create table tl_lachat11_user (
+   uid bigint not null auto_increment,
+   user_id bigint,
+   last_name varchar(255),
+   login_name varchar(255),
+   first_name varchar(255),
+   jabber_id varchar(255),
+   session_id bigint,
+   primary key (uid)
+);
+alter table tl_lachat11_attachment add index FK9ED6CB2E1A3926E3 (chat_uid), add constraint FK9ED6CB2E1A3926E3 foreign key (chat_uid) references tl_lachat11_chat (uid);
+alter table tl_lachat11_session add index FK96E446B1A3926E3 (chat_uid), add constraint FK96E446B1A3926E3 foreign key (chat_uid) references tl_lachat11_chat (uid);
+alter table tl_lachat11_user add index FK4EB8216C10360A0 (session_id), add constraint FK4EB8216C10360A0 foreign key (session_id) references tl_lachat11_session (uid);
 
 INSERT INTO tl_lachat11_chat (title,instructions,online_instructions,offline_instructions,tool_content_id,run_offline,lock_on_finished,content_in_use,define_later) 
 VALUES("LAMS Chat","Chat Instruction","Online instructions","Offline instructions",${default_content_id},0,0,0,0);
