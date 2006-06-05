b2eee47014807702c9c2ec1e94efaf5b0cadd44b
fyang <>
2006-06-05 07:05:42 +0000

    LocaleDAO to retrieve countries and languages LAMS supports

---
+++ lams_common/src/java/org/lamsfoundation/lams/usermanagement/dao/ILocaleDAO.java
@@ -0,0 +1,44 @@
+/****************************************************************
+ * Copyright (C) 2005 LAMS Foundation (http://lamsfoundation.org)
+ * =============================================================
+ * License Information: http://lamsfoundation.org/licensing/lams/2.0/
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2.0 
+ * as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ * 
+ * http://www.gnu.org/licenses/gpl.txt
+ * ****************************************************************
+ */
+package org.lamsfoundation.lams.usermanagement.dao;
+
+import java.util.List;
+
+/**
+ * @version
+ *
+ * <p>
+ * <a href="ILocaleDAO.java.html"><i>View Source</i></a>
+ * </p>
+ *
+ * @author <a href="mailto:fyang@melcoe.mq.edu.au">Fei Yang</a>
+ *
+ * Created at 16:46:18 on 2006-6-5
+ */
+public interface ILocaleDAO {
+	
+	public List getAllLanguages();
+	
+	public List getAllCountries();
+
+}
