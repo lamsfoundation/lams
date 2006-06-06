a31e81428ec8e1388639e8ae56a8f8810c29b385
fyang <>
2006-06-06 07:03:23 +0000

    PrivilegeDAO interface

---
+++ lams_common/src/java/org/lamsfoundation/lams/usermanagement/dao/IPrivilegeDAO.java
@@ -0,0 +1,52 @@
+/****************************************************************
+ * Copyright (C) 2005 LAMS Foundation (http://lamsfoundation.org)
+ * =============================================================
+ * License Information: http://lamsfoundation.org/licensing/lams/2.0/
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2.0 
+ * as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ * 
+ * http://www.gnu.org/licenses/gpl.txt
+ * ****************************************************************
+ */
+package org.lamsfoundation.lams.usermanagement.dao;
+
+import java.util.List;
+
+import org.lamsfoundation.lams.dao.IBaseDAO;
+import org.lamsfoundation.lams.usermanagement.Privilege;
+
+/**
+ * @version
+ *
+ * <p>
+ * <a href="IPrivilegeDAO.java.html"><i>View Source</i></a>
+ * </p>
+ *
+ * @author <a href="mailto:fyang@melcoe.mq.edu.au">Fei Yang</a>
+ *
+ * Created at 18:57:47 on 2006-6-5
+ */
+public interface IPrivilegeDAO extends IBaseDAO {
+
+	public List getAllPrivileges();
+	
+	public Privilege getPrivilegeByCode(String code);
+	
+	public Privilege getPrivilegeById(Integer id);
+	
+	public void deletePrivilegeById(Integer id);
+	
+	public void deletePrivilegeByCode(String code);
+}
