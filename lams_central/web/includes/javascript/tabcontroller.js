fa24e6729d808ea99968a62a54c5b977e17b807c
asukkar <>
2006-06-27 03:09:23 +0000

    Updated css and tag files required to implement lams style sheet.

--- lams_central/web/includes/javascript/tabcontroller.js
+++ lams_central/web/includes/javascript/tabcontroller.js
@@ -1,37 +1,30 @@
+
+/* Anthony's version tab controller */
 var tabSize = 0;
 var selectedTabID = 0;
-
 /* Initialise the number of tabs in the page */
-function initTabSize(size){
+function initTabSize(size) {
     tabSize = size;
 }
-
 function selectTab(tabID) {
-        
-    if(selectedTabID == tabID)
+    if (selectedTabID == tabID) {
         return;
-
-    if(selectedTabID == 0)
+    }
+    if (selectedTabID == 0) {
         selectedTabID = tabID;
-        
-    //change the old tab's class
-    document.getElementById("tab" + selectedTabID).className = "tab tabcentre";
-    //swap images of side parts
-	var tl = document.getElementById("tableft_"+selectedTabID);
-	tl.src= imgRoot + themeName + "_tab_left.gif";
-	tl.height = 22;
-	var tr = document.getElementById("tabright_"+selectedTabID);
-	tr.src= imgRoot + themeName + "_tab_right.gif";
-	tr.height = 22;
-    
-    //change the new tab's class
-    document.getElementById("tab" + tabID).className = "tab tabcentre_selected";
-    var tl = document.getElementById("tableft_"+tabID);
-	tl.src= imgRoot + themeName + "_tab_s_left.gif";
-	tl.height = 25;
-	var tr = document.getElementById("tabright_"+tabID);
-	tr.src= imgRoot + themeName + "_tab_s_right.gif";
-	tr.height = 25;
+    }
+	
+	// change the old tabs class
+    document.getElementById("tab-left-" + selectedTabID).className = "tab-left";
+    document.getElementById("tab-middle-" + selectedTabID).className = "tab-middle";
+    document.getElementById("tab-right-" + selectedTabID).className = "tab-right";
+    document.getElementById("tab-middle-link-" + selectedTabID).className = "tab-middle-link";
+
+	// change the new tabs class
+    document.getElementById("tab-left-" + tabID).className = "tab-left-selected";
+    document.getElementById("tab-middle-" + tabID).className = "tab-middle-selected";
+    document.getElementById("tab-right-" + tabID).className = "tab-right-selected";
+    document.getElementById("tab-middle-link-" + tabID).className = "tab-middle-link-selected";
     
     //save tabID as selectedTabID
     selectedTabID = tabID;
@@ -40,13 +33,13 @@ function selectTab(tabID) {
     for(i = 1; i <= tabSize; i++) {
       document.getElementById("tabbody" + i).style.display = (i == tabID) ? 'block':'none';
     }
-    
-    
-    try{
+
+    try {
         //trigger the custom event listener onSelectTab()
         onSelectTab(tabID);
     }
-    catch (error){
+    catch (error) {
         //catch reference error when onSelectTab() is not defined
     }
 }
+
