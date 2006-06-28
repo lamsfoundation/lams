53da8e60f3dd7fde80923a5f850a938968140420
fmalikoff <>
2006-06-28 02:13:54 +0000

    Changed to same name as 1.0.x

--- lams_build/librarypackages/shareresourcesforum/learninglibrary1_insert.sql
+++ lams_build/librarypackages/shareresourcesforum/learninglibrary1_insert.sql
@@ -1,2 +1,2 @@
 insert into lams_learning_library (learning_library_id,description,title,valid_flag,create_date_time) 
-values (null,'Shared Resources and Forum','Discuss Shared Resources',0,now());
+values (null,'Shared Resources and Forum','Resources and Forum',0,now());
