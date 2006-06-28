d6a38dbb0ca18f505c89ad7c4bb79ffbe185ec39
fyang <>
2006-06-28 05:16:58 +0000

    dummy profile action

---
+++ lams_central/src/java/org/lamsfoundation/lams/web/ProfileAction.java
@@ -0,0 +1,57 @@
+/****************************************************************
+ * Copyright (C) 2005 LAMS Foundation (http://lamsfoundation.org)
+ * =============================================================
+ * License Information: http://lamsfoundation.org/licensing/lams/2.0/
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2.0 
+ * as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ * 
+ * http://www.gnu.org/licenses/gpl.txt
+ * ****************************************************************
+ */
+package org.lamsfoundation.lams.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+/**
+ * @version
+ *
+ * <p>
+ * <a href="ProfileAction.java.html"><i>View Source</i></a>
+ * </p>
+ *
+ * @author <a href="mailto:fyang@melcoe.mq.edu.au">Fei Yang</a>
+ *
+ * Created at 14:21:53 on 28/06/2006
+ */
+
+/**
+ * @struts.action path="/profile"
+ *                validation="false"
+ * 
+ * @struts.action-forward name="profile"
+ *                        path="/profile.jsp"
+ */
+public class ProfileAction extends Action {
+	
+	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception{
+		return mapping.findForward("profile");
+	}
+}
