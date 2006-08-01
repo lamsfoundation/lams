e01137f90b2e82b947046966c62403303a29c79d
asukkar <>
2006-08-01 03:43:41 +0000

    Added constants to be used for id_type in notebook entry.

---
+++ lams_common/src/java/org/lamsfoundation/lams/notebook/service/NotebookConstants.java
@@ -0,0 +1,6 @@
+package org.lamsfoundation.lams.notebook.service;
+
+public class NotebookConstants {
+	public static Integer SCRATCH_PAD = new Integer(1);
+	public static Integer NOTEBOOK_TOOL = new Integer(2);
+}
