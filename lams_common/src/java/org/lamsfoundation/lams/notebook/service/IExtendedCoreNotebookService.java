76d1785e08e8c608ce5ab537467194f7d85e0190
asukkar <>
2006-08-01 03:45:44 +0000

    Added extended interface for CoreNotebookService.  Interface extends the ICoreNotebookService and adds getEntry(userID) and getEntry(userID, lessonID)

---
+++ lams_common/src/java/org/lamsfoundation/lams/notebook/service/IExtendedCoreNotebookService.java
@@ -0,0 +1,10 @@
+package org.lamsfoundation.lams.notebook.service;
+
+import java.util.List;
+
+import org.lamsfoundation.lams.notebook.model.NotebookEntry;
+
+public interface IExtendedCoreNotebookService extends ICoreNotebookService {
+	List<NotebookEntry> getEntry(Integer userID);
+	List<NotebookEntry> getEntry(Integer userID, Long lessonID);
+}
