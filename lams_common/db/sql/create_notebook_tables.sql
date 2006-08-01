7c5cfc65ebbe797e6f1944239875fbc056d481e7
asukkar <>
2006-08-01 04:11:09 +0000

    Added new field create_date and update_date

--- lams_common/db/sql/create_notebook_tables.sql
+++ lams_common/db/sql/create_notebook_tables.sql
@@ -1,2 +1,2 @@
 drop table if exists lams_notebook_entry;
-create table lams_notebook_entry (uid bigint not null auto_increment, external_id bigint, external_id_type integer, external_signature varchar(255), user_id integer, title varchar(255), entry text, primary key (uid));
+create table lams_notebook_entry (uid bigint not null auto_increment, external_id bigint, external_id_type integer, external_signature varchar(255), user_id integer, title varchar(255), entry text, create_date datetime, update_date datetime, primary key (uid));
