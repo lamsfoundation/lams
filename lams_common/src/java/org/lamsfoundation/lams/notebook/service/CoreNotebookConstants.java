32a06b2b49302ae7c4479dffde8eb2b0574e343a
asukkar <>
2006-08-03 00:28:21 +0000

    Renamed NotebookConstants to CoreNotebookConstants to remove confustion with constants in Notebook tool.

--- lams_common/src/java/org/lamsfoundation/lams/notebook/service/CoreNotebookConstants.java
+++ lams_common/src/java/org/lamsfoundation/lams/notebook/service/CoreNotebookConstants.java
@@ -1,6 +1,6 @@
 package org.lamsfoundation.lams.notebook.service;
 
-public class NotebookConstants {
+public class CoreNotebookConstants {
 	public static Integer SCRATCH_PAD = new Integer(1);
 	public static Integer NOTEBOOK_TOOL = new Integer(2);
 }
