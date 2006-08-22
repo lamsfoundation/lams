5168567be6170c3fadb69a914e151bee62c8667b
fmalikoff <>
2006-08-22 04:44:51 +0000

    Fixed error in comment.

--- lams_tool_nb/src/java/org/lamsfoundation/lams/tool/noticeboard/service/NoticeboardServicePOJO.java
+++ lams_tool_nb/src/java/org/lamsfoundation/lams/tool/noticeboard/service/NoticeboardServicePOJO.java
@@ -1026,7 +1026,7 @@ public class NoticeboardServicePOJO implements INoticeboardService, ToolContentM
         removeSession(session);
     }
 	
-	/* ===============Methods implemented from ToolContentManager =============== */
+	/* ===============Methods implemented from ToolContentImport102Manager =============== */
 	
 
     /**
