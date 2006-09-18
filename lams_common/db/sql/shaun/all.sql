3670f024ea0d8e4499f9e59866f233f4c9896a09
jliew <>
2006-09-18 00:38:53 +0000

    added danish user; need to assign danish locale for it

--- lams_common/db/sql/shaun/all.sql
+++ lams_common/db/sql/shaun/all.sql
@@ -451,6 +451,30 @@ INSERT INTO lams_user_organisation_role (user_organisation_role_id, user_organis
 INSERT INTO lams_user_organisation_role (user_organisation_role_id, user_organisation_id, role_id) VALUES (178,65,5);
 INSERT INTO lams_user_organisation_role (user_organisation_role_id, user_organisation_id, role_id) VALUES (179,65,2);
 
+
+-- need a new workspace_id and the name changes for each user.
+insert into lams_workspace_folder(workspace_folder_id,parent_folder_id,name,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id)
+values(22,null,'Danish Workspace',22,now(),now(),1);
+insert into lams_workspace (workspace_id, default_fld_id, name) values (22,22,'Danish Test');
+--insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (11,11);
+
+insert into lams_user(user_id,login,password,title,first_name,last_name,address_line_1,address_line_2,address_line_3,city,state,country,day_phone,evening_phone,mobile_phone,fax,email,disabled_flag,create_date,authentication_method_id,workspace_id, flash_theme_id,html_theme_id,locale_id)
+values(22,'danish','danish','Dr','Danish','Test','22','Danish Ave',null,'Copenhagen',null,'Denmark','0211111111','0211111112','0411111111','0211111113','danish@xx.os',0,'20041223',1,22,1,3,19);
+
+-- need a new user_organisation_id for each row, user_id must match user_id created in the lams_user
+-- belongs to course Playpen, Class Everybody
+INSERT INTO lams_user_organisation (user_organisation_id, organisation_id, user_id) VALUES (66, 2, 21);
+INSERT INTO lams_user_organisation (user_organisation_id, organisation_id, user_id) VALUES (67, 3, 21);
+
+-- need a new user_organisation_role_id for each row, use the same role_ids (3,5,2) for author, learner, teacher
+INSERT INTO lams_user_organisation_role (user_organisation_role_id, user_organisation_id, role_id) VALUES (180,66,3);
+INSERT INTO lams_user_organisation_role (user_organisation_role_id, user_organisation_id, role_id) VALUES (181,66,5);
+INSERT INTO lams_user_organisation_role (user_organisation_role_id, user_organisation_id, role_id) VALUES (182,66,2);
+INSERT INTO lams_user_organisation_role (user_organisation_role_id, user_organisation_id, role_id) VALUES (183,67,4);
+INSERT INTO lams_user_organisation_role (user_organisation_role_id, user_organisation_id, role_id) VALUES (184,67,5);
+INSERT INTO lams_user_organisation_role (user_organisation_role_id, user_organisation_id, role_id) VALUES (185,67,2);
+
+
 SET FOREIGN_KEY_CHECKS=1;
 
 
