fc1eae65cfee0ee25eedff87f9934dbb187041bd
jliew <>
2006-09-25 23:58:14 +0000

    role tag to display role names in format understood by message resources' key format

---
+++ lams_central/src/java/org/lamsfoundation/lams/web/tag/RoleTag.java
@@ -0,0 +1,47 @@
+package org.lamsfoundation.lams.web.tag;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+import org.apache.commons.lang.StringUtils;
+/**
+ * JSP tag. Converts role name into form usable as message resources key.
+ * @jsp.tag name="role"
+ * 		body-content="empty"
+ * 		display-name="Converts role name into form usable as message resources key"
+ * 		description="Converts role name into form usable as message resources key"
+ * 
+ * @author jliew
+ *
+ */
+public class RoleTag extends SimpleTagSupport{
+	
+	private String role;
+	
+	@Override
+	public void doTag() throws JspException, IOException {
+		if(StringUtils.isEmpty(role)) getJspContext().getOut().write(role);
+		
+		role = role.replace(' ','.');
+		role = role.replaceFirst("COURSE", "GROUP");
+		getJspContext().getOut().write(role.toString());
+	}
+
+	/**
+	 * @jsp.attribute 
+	 * 		required="true" 
+	 * 		rtexprvalue="true"
+	 * 		description="Role name as given from database"
+	 * @return
+	 */
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+}
