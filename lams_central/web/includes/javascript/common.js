f81237e7d271691afab5b1472f2df07457292772
fmalikoff <>
2006-10-01 22:30:02 +0000

    Adding the popup() code so that it isn't duplicated across tools.

--- lams_central/web/includes/javascript/common.js
+++ lams_central/web/includes/javascript/common.js
@@ -16,6 +16,15 @@
 			instructionsWindow.window.focus();
 //		}	
 	}
+
+	function launchPopup(url,title) {
+		var wd = null;
+		if(wd && wd.open && !wd.closed){
+			wd.close();
+		}
+		wd = window.open(url,title,'resizable,width=796,height=570,scrollbars');
+		wd.window.focus();
+	}	
 	
 	function filterData(src,target){
 		if(src.value != null){
