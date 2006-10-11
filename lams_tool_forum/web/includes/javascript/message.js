4bd9dd5189274e3383b6c520c58e916b3b6b4a74
steven <>
2006-10-11 02:57:07 +0000

    Fix js error, show/hide busy missing, remove by someone.

--- lams_tool_forum/web/includes/javascript/message.js
+++ lams_tool_forum/web/includes/javascript/message.js
@@ -28,4 +28,14 @@
 	function removeItemAttachmentComplete(){
 		hideBusy(itemAttachmentTargetDiv);
 	}
+	function showBusy(targetDiv){
+		if($(targetDiv+"_Busy") != null){
+			Element.show(targetDiv+"_Busy");
+		}
+	}
+	function hideBusy(targetDiv){
+		if($(targetDiv+"_Busy") != null){
+			Element.hide(targetDiv+"_Busy");
+		}				
+	}
 
