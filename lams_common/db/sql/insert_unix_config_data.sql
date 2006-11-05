c84a691e5cd108829681c9fa2c4129905eaf2da7
jliew <>
2006-11-05 02:36:52 +0000

    synchronise with insert_windows_config_data.sql

--- lams_common/db/sql/insert_unix_config_data.sql
+++ lams_common/db/sql/insert_unix_config_data.sql
@@ -9,7 +9,6 @@ insert into lams_configuration (config_key, config_value) values ('ContentReposi
 insert into lams_configuration (config_key, config_value) values ('UploadFileMaxSize','1048576');
 insert into lams_configuration (config_key, config_value) values ('UploadLargeFileMaxSize','10485760');
 insert into lams_configuration (config_key, config_value) values ('UploadFileMaxMemorySize','4096');
-insert into lams_configuration (config_key, config_value) values ('FileManagerDir','/usr/local/jboss-4.0.2/server/default/deploy/upload.war');
 insert into lams_configuration (config_key, config_value) values ('ExecutableExtensions','.bat,.bin,.com,.cmd,.exe,.msi,.msp,.ocx,.pif,.scr,.sct,.sh,.shs,.vbs');
 insert into lams_configuration (config_key, config_value) values ('UserInactiveTimeout','86400');
 insert into lams_configuration (config_key, config_value) values ('UseCacheDebugListener','false');
@@ -18,7 +17,7 @@ insert into lams_configuration (config_key, config_value) values ('AuthoringActi
 insert into lams_configuration (config_key, config_value) values ('AuthoringClientVersion','2.0.RC2.@datetimestamp@');
 insert into lams_configuration (config_key, config_value) values ('MonitorClientVersion','2.0.RC2.@datetimestamp@');
 insert into lams_configuration (config_key, config_value) values ('LearnerClientVersion','2.0.RC2.@datetimestamp@');
-insert into lams_configuration (config_key, config_value) values ('ServerVersionNumber','2.0.RC2.@datetimestamp@');
+insert into lams_configuration (config_key, config_value) values ('ServerVersionNumber','2.0.@datetimestamp@');
 insert into lams_configuration (config_key, config_value) values ('ServerLanguage','en_AU');
 insert into lams_configuration (config_key, config_value) values ('ServerPageDirection','LTR');
 insert into lams_configuration (config_key, config_value) values ('DictionaryDateCreated','2006-01-02');
@@ -29,3 +28,4 @@ insert into lams_configuration (config_key, config_value) values ('XmppAdmin','a
 insert into lams_configuration (config_key, config_value) values ('XmppPassword','wildfire');
 insert into lams_configuration (config_key, config_value) values ('DefaultFlashTheme','default');
 insert into lams_configuration (config_key, config_value) values ('DefaultHTMLTheme','defaultHTML');
+insert into lams_configuration (config_key, config_value) values ('AllowDirectLessonLaunch','false');
