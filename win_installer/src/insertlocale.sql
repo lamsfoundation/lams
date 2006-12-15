8063641d2e87443222576129ec6712794375f03b
lfoxton <>
2006-12-15 02:31:33 +0000

    Procedure to update locales table in database, calls insertlocalses repeatedly until all languages have been covered
    To add a new language, you nee only to add a line:
    insertLocal(language_iso_code, country_iso_code, description, direction)

--- win_installer/src/insertlocale.sql
+++ win_installer/src/insertlocale.sql
@@ -1,18 +1,16 @@
-DROP PROCEDURE IF EXISTS insertLocale
-create procedure insertLocale 
-	(IN _language VARCHAR(2), 
-	 IN _locale VARCHAR(2), 
-	 IN _description VARCHAR(??), 
-	 IN _rtl  VARCHAR(3))
-begin
-	select count(*) into _numRows from lams_supported_locale 
-		where language_iso_code = '_language' and country_iso_code = '_locale';
+DELIMITER $$
 
-	if numRows = 0 then
+DROP PROCEDURE IF EXISTS insertlocale $$
+CREATE PROCEDURE insertlocale (IN _language VARCHAR(2), IN _locale VARCHAR(2), IN _description VARCHAR(255), IN _rtl  VARCHAR(3))
+BEGIN
+
+	if (select count(*) from lams_supported_locale where language_iso_code = '_language' and country_iso_code = '_locale') = 0 then
 		insert lams_supported_locale 
-			(language_iso_code, country_iso_code, description, direction)
-			values (_language,'_locale','_description','_rtl');
+		(language_iso_code, country_iso_code, description, direction)
+		values (_language,'_locale','_description','_rtl');
 	end if;
-end
+END $$
+
+DELIMITER ;
 
 
