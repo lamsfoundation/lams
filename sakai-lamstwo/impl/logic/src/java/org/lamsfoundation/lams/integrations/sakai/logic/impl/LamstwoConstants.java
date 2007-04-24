c144f53e688b530044b19ff807e502a41d33039a
asukkar <>
2007-04-24 01:23:19 +0000

    Added to public CVS

---
+++ sakai-lamstwo/impl/logic/src/java/org/lamsfoundation/lams/integrations/sakai/logic/impl/LamstwoConstants.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+ * LamstwoConstants.java
+ * 
+ * Copyright (c) 2007 LAMS Foundation
+ * Licensed under the Educational Community License version 1.0
+ * 
+ * A copy of the Educational Community License has been included in this 
+ * distribution and is available at: http://www.opensource.org/licenses/ecl1.php
+ * 
+ *****************************************************************************/
+
+package org.lamsfoundation.lams.integrations.sakai.logic.impl;
+
+public class LamstwoConstants {
+
+	public static final String CONF_SERVER_ADDRRESS = "lamstwo.serverAddr";
+	public static final String CONF_SERVER_ID		= "lamstwo.serverId";
+	public static final String CONF_SERVER_KEY		= "lamstwo.serverKey";
+	public static final String CONF_REQUEST_SOURCE	= "lamstwo.requestSrc";
+	
+}
