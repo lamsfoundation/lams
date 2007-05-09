62294e2b2dba048e4669c1ed51d900b260138d55
fmalikoff <>
2007-05-09 11:47:37 +0000

    Fixed version string - missing "Beta"

--- lams_common/db/sql/insert_rams_unix_config_data.sql
+++ lams_common/db/sql/insert_rams_unix_config_data.sql
@@ -1,6 +1,6 @@
 insert into lams_configuration (config_key, config_value) values ('ServerURL','http://shaun.melcoe.mq.edu.au/rams/');
 insert into lams_configuration (config_key, config_value) values ('ServerURLContextPath','rams/');
-insert into lams_configuration (config_key, config_value) values ('Version','1.0');
+insert into lams_configuration (config_key, config_value) values ('Version','1.0 Beta');
 insert into lams_configuration (config_key, config_value) values ('TempDir','/var/opt/rams/temp');
 insert into lams_configuration (config_key, config_value) values ('DumpDir','/var/opt/rams/dump');
 insert into lams_configuration (config_key, config_value) values ('EARDir','/usr/local/jboss-4.0.2/server/default/deploy/rams.ear');
