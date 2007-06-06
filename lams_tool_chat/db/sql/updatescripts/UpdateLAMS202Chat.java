e29c78bdbbc19b18ab271fea21497a6197120016
lfoxton <>
2007-06-06 00:34:45 +0000

    made changes so this java class xcan communicate with the installer/updater

--- lams_tool_chat/db/sql/updatescripts/UpdateLAMS202Chat.java
+++ lams_tool_chat/db/sql/updatescripts/UpdateLAMS202Chat.java
@@ -3,6 +3,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 
 import java.util.Properties;
 
@@ -13,7 +14,8 @@ import org.hibernate.id.UUIDHexGenerator;
 
 public class UpdateLAMS202Chat {
 
-    private String dbDriverClass;
+    private String XMPPConference;
+	private String dbDriverClass;
     private String dbDriverUrl;
     private String dbUsername;
     private String dbPassword;
@@ -25,20 +27,38 @@ public class UpdateLAMS202Chat {
 	 */
 	public static void main(String[] args) throws Exception {
 		UpdateLAMS202Chat me = new UpdateLAMS202Chat();
-		String XMPPConference = args[0];
-		me.execute(XMPPConference);
+		
+		if (args.length < 4)
+		{
+			System.out.println("Usage: Java XMPPConference dbDriverUrl dbUsername dbPassword crPath");
+			System.exit(1);
+		}
+		
+		String dbDriverUrl = args[0];
+		String dbUsername = args[1];
+		String dbPassword = args[2];
+		String crPath = args[3];
+		me.execute(dbDriverUrl, dbUsername, dbPassword, crPath);
 	}
 
-	public void execute(String XMPPConference) throws Exception {
-
+	public void execute(String dbDriverUrl, String dbUsername, String dbPassword, String crPath) throws Exception {
+		
 		dbDriverClass = "com.mysql.jdbc.Driver";
+		/*
 		dbDriverUrl = "jdbc:mysql://localhost/lams_demo?characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&autoReconnect=true&useUnicode=true";
 		dbUsername = "root";
 		dbPassword = "secret4141";
 		crPath = "/var/opt/lams/repository";
-
+		*/
+		
+		this.dbDriverClass = dbDriverClass;
+		this.dbDriverUrl = dbDriverUrl;
+		this.dbUsername = dbUsername;
+		this.dbPassword = dbPassword;
+		this.crPath = crPath;
+		
         String getChatSessions = "SELECT uid, jabber_room, room_created from tl_lachat11_session";
-
+        String getXmppConference = "SELECT config_value FROM lams_configuration WHERE config_key='XmppConference'";
         String updateChatSessions = "UPDATE tl_lachat11_session set room_created=?, jabber_room=? where uid=?";
 
         Class.forName(dbDriverClass);
@@ -47,7 +67,23 @@ public class UpdateLAMS202Chat {
         PreparedStatement stmt  = conn.prepareStatement(getChatSessions);
         ResultSet results = stmt.executeQuery();
 
-    	PreparedStatement stmtUpdate = conn.prepareStatement(updateChatSessions);
+        // getting XmppConference value from db.
+        Statement xmppStmt = conn.createStatement();
+        ResultSet xmppResult = xmppStmt.executeQuery(getXmppConference);
+        
+        if (xmppResult.next()) 
+        {
+	        XMPPConference = xmppResult.getString(1);
+	        System.out.println(XMPPConference);        
+        } 
+        else
+        {
+	        conn.close();
+	        System.out.println("Could not find XMPP Conference.");
+	        System.exit(1);
+        }
+      
+        PreparedStatement stmtUpdate = conn.prepareStatement(updateChatSessions);
 
 		IdentifierGenerator idGenerator = new UUIDHexGenerator();
 				((Configurable) idGenerator).configure(Hibernate.STRING,
