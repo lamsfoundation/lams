dcba8be00c08df0eb2ac0782c92c7d06f42dc881
fmalikoff <>
2007-06-28 23:13:17 +0000

    Update done the day after the initial release - the db was being created locally on a fresh machine as user grants were failing. Referred to a non-existent parameter SQL_GRANT, which has been replaced by SQL_HOST.

--- unix_installer/installer-package/sql-scripts/create-database.sql
+++ unix_installer/installer-package/sql-scripts/create-database.sql
@@ -1,7 +1,9 @@
+-- This is used for a local database only (so @SQL_HOST@) always ends up as localhost. For separate MySQL servers, the user and the db must be created manually.
+
 SET FOREIGN_KEY_CHECKS=0;
 DROP DATABASE IF EXISTS @DB_NAME@;
 CREATE DATABASE @DB_NAME@ DEFAULT CHARACTER SET utf8;
 SET FOREIGN_KEY_CHECKS=1;
-GRANT ALL PRIVILEGES ON *.* TO @DB_USER@@@SQL_GRANT@ IDENTIFIED BY '@DB_PASS@';
-REVOKE PROCESS,SUPER ON *.* from @DB_USER@@@SQL_GRANT@;
+GRANT ALL PRIVILEGES ON *.* TO @DB_USER@@@SQL_HOST@ IDENTIFIED BY '@DB_PASS@';
+REVOKE PROCESS,SUPER ON *.* from @DB_USER@@@SQL_HOST@;
 USE @DB_NAME@;
