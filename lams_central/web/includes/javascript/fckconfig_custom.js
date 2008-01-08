e9fcffb42a9d98ae81b8bc85cdf4c3bf4804f311
erniegx <ernieg@gmail.com>
2008-01-08 03:08:13 +0000

    LDEV-1368: Support for firefox spellchecked turned on, Added templates, and default popups on _blank

--- lams_central/web/includes/javascript/fckconfig_custom.js
+++ lams_central/web/includes/javascript/fckconfig_custom.js
@@ -1,6 +1,6 @@
 FCKConfig.ToolbarSets["Default"] = [
 	['Source','-','FitWindow', 'Preview','PasteWord','Undo','Redo','Bold','Italic','Underline', '-','Subscript','Superscript','OrderedList','UnorderedList','-','Outdent','Indent','JustifyLeft','JustifyCenter','JustifyRight','JustifyFull','TextColor','BGColor'], 
-	['Image','Link','Table','Rule','Smiley','SpecialChar','FontFormat','FontName','FontSize','About']
+	['Image','Link','Table','Rule','Smiley','SpecialChar','Templates','FontFormat','FontName','FontSize','About']
 ] ;
 
 FCKConfig.ToolbarSets["Default-Learner"] = [
@@ -17,3 +17,7 @@ FCKConfig.ToolbarSets["Default-Learner"] = [
 
 FCKConfig.EditorAreaCSS = FCKConfig.BasePath + '../../css/defaultHTML_learner.css' ;
 FCKConfig.SkinPath = FCKConfig.BasePath + 'skins/office2003/' ;
+FCKConfig.FirefoxSpellChecker = true;
+FCKConfig.BrowserContextMenuOnCtrl = true;
+FCKConfig.DefaultLinkTarget = "_blank";
+FCKConfig.TemplatesXmlPath = FCKConfig.BasePath + '../../www/htmltemplates.xml'
