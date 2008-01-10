4ef62f42d4bd4bcdbe98bac97cafcd3c0c7a4ab2
fmalikoff <>
2008-01-10 23:16:21 +0000

    LDEV-1493: Default the update script's set tool_version line to commented out - we assume the normal behaviour is that this is called by the tool updater.

--- lams_tool_laqa/db/sql/updatescripts/updateTo20080108.sql
+++ lams_tool_laqa/db/sql/updatescripts/updateTo20080108.sql
@@ -13,4 +13,4 @@ UPDATE lams_tool SET modified_date_time = NOW() WHERE tool_signature = 'laqa11';
 
 -- update the tool version - special code that should only be executed if the upgrade is being done manually.
 -- if it is being done via the tool deployer then it will update the version automatically.
-UPDATE lams_tool SET tool_version = "20080108" WHERE tool_signature = "laqa11";
+-- UPDATE lams_tool SET tool_version = "20080108" WHERE tool_signature = "laqa11";
