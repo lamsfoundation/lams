32679966238b3e510496c940f0708ec2a41a3d8d
fmalikoff <>
2008-01-14 05:05:32 +0000

    LDEV-1488: Demo was missing the system_tool_id for 90 grouping activities. This script fixes this problelm - how it arose is unknown.

---
+++ lams_common/db/sql/updatescripts/alter_21_fixgroupings.sql
@@ -0,0 +1,6 @@
+-- FOR LAMS 2.1 Release: See LDEV-1488
+-- We found after upgrading demo to LAMS 2.1 that some of the existing designs had the group activities missing
+-- their system tool id. This script will fix this. 
+
+update lams_learning_activity set system_tool_id = 1 where learning_activity_type_id = '2' and system_tool_id is null;
+
