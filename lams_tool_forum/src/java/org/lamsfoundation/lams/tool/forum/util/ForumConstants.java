f3fdd03cd679fc8aad8b5149692b63997aa09f06
erniegx <ernieg@gmail.com>
2008-05-01 05:45:43 +0000

    LDEV-1679: new constant for locked when finished

--- lams_tool_forum/src/java/org/lamsfoundation/lams/tool/forum/util/ForumConstants.java
+++ lams_tool_forum/src/java/org/lamsfoundation/lams/tool/forum/util/ForumConstants.java
@@ -64,6 +64,7 @@ public interface ForumConstants {
 	public static final String ATTR_ALLOW_NEW_TOPICS = "allowNewTopics";
 	public static final String ATTR_ALLOW_RICH_EDITOR = "allowRichEditor";
 	public static final String ATTR_LIMITED_CHARS = "limitedChars";
+        public static final String ATTR_LOCK_WHEN_FINISHED = "lockedWhenFinished";
 
 	public static final String ONLINE_ATTACHMENT = "online_att";
 	public static final String OFFLINE_ATTACHMENT = "offline_att";
