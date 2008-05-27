26d14d3cd9e555bde9d99b13527e29f88787b7b4
jliew <>
2008-05-27 07:14:08 +0000

    LDEV-1707 fixing broken sql statement

--- lams_tool_sbmt/db/sql/updatescripts/updateTo20080509.sql
+++ lams_tool_sbmt/db/sql/updatescripts/updateTo20080509.sql
@@ -1,4 +1,5 @@
 -- Update the File Submit tables to 20080509
 -- This is for the LAMS 2.1 release.
 
-UPDATE TABLE tl_lasbmt11_report MODIFY COLUMN marks float;
+ALTER TABLE tl_lasbmt11_report MODIFY COLUMN marks float;
+--UPDATE lams_tool SET tool_version = "20080509" WHERE tool_signature = "lasbmt11";
