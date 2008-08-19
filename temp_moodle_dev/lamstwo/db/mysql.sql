10a8a9d91b8a91cf597fa766680c3d61b7e01e7a
jliew <>
2008-08-19 07:29:31 +0000

    fix current version of lamstwo tables

--- temp_moodle_dev/lamstwo/db/mysql.sql
+++ temp_moodle_dev/lamstwo/db/mysql.sql
@@ -8,11 +8,29 @@ CREATE TABLE prefix_lamstwo (
   id int(10) unsigned NOT NULL auto_increment,
   course int(10) unsigned NOT NULL default '0',
   name varchar(255) NOT NULL default '',
-  introduction text NOT NULL default '',
-  sequence_id bigint(20) default '0',
-  lesson_id bigint(20) default '0',
+  intro text NOT NULL default '',
   timemodified int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (id),
   KEY course (course)
 )COMMENT='LAMS2 activity';  
 
+CREATE TABLE prefix_lamstwo_lesson (
+  id int(10) unsigned NOT NULL auto_increment,
+  course int(10) unsigned NOT NULL default '0',
+  lamstwo int(10) unsigned NOT NULL default '0',
+  name varchar(255) NOT NULL default '',
+  intro text NOT NULL default '',
+  groupid bigint(10) NOT NULL default '-1',
+  sequence_id bigint(20) default '0',
+  lesson_id bigint(20) default '0',
+  timemodified int(10) unsigned NOT NULL default '0',
+  PRIMARY KEY  (id)
+); 
+
+CREATE TABLE mdl_lamstwo_grade (
+  id int(10) unsigned NOT NULL auto_increment,
+  lamstwolesson int(10) unsigned NOT NULL default '0',
+  user int(10) unsigned NOT NULL default '0',
+  completed int(1) unsigned NOT NULL default '0',
+  PRIMARY KEY (id)
+);
