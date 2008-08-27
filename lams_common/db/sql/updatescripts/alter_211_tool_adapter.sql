ab921f79702e13885a0c2326d9d521991289d853
lfoxton <>
2008-08-27 06:03:27 +0000

    Fixing up the alter_211_tool_adapter_sql update script to have the correct update statement.

--- lams_common/db/sql/updatescripts/alter_211_tool_adapter.sql
+++ lams_common/db/sql/updatescripts/alter_211_tool_adapter.sql
@@ -1,2 +1,2 @@
 -- Adding a new tool type for tool adapters
-ALTER TABLE lams_tool ADD COLUMN tool_adapter TINYINT(1) DEFAULT 0;
+ALTER TABLE lams_tool ADD COLUMN ext_lms_id VARCAHR(255) DEFAULT null;
