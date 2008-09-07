cf141360f6d83b7f8e611cf80b4d6eb590ee3083
lfoxton <>
2008-09-07 23:53:53 +0000

    Build dir on patch project should be empty in the CVS

--- unix_installer/patch-package/build/lams-unix-patch-2.1.1/sql-scripts/update-script.sql
+++
@@ -1,4 +0,0 @@
-UPDATE lams_tool SET tool_version='20080813' WHERE tool_signature='lachat11' AND tool_version='20080229';
-
-UPDATE lams_configuration SET config_value='2.1.1' WHERE config_key='Version' AND config_value='2.1';
-
