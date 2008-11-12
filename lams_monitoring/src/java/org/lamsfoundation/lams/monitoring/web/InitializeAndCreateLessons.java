c8689e47cc0c92f05664c9840bc038d7e42a8be1
jliew <>
2008-11-12 08:40:11 +0000

    LDEV-1968 should probably rename servlet if its mapping is going to be renamed

--- lams_monitoring/src/java/org/lamsfoundation/lams/monitoring/web/InitializeAndCreateLessons.java
+++ lams_monitoring/src/java/org/lamsfoundation/lams/monitoring/web/InitializeAndCreateLessons.java
@@ -42,23 +42,23 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 /**
- * Servlet for flash to call in order to initialise and create lesson classes for
- *  multiple lessons.
+ * Servlet for flash to call in order to initialise and create lesson classes
+ * for multiple lessons.
  * 
  * @web:servlet name="initializeAndCreateLessons"
  * @web:servlet-mapping url-pattern="/initializeAndCreateLessons"
  */
-public class StartLessonsServlet extends AbstractStoreWDDXPacketServlet {
+public class InitializeAndCreateLessons extends AbstractStoreWDDXPacketServlet {
     // ---------------------------------------------------------------------
     // Instance variables
     // ---------------------------------------------------------------------
-    private static Logger log = Logger.getLogger(StartLessonsServlet.class);
+    private static Logger log = Logger.getLogger(InitializeAndCreateLessons.class);
 
     private static final long serialVersionUID = 2349582345234543680L;
 
     private static IAuditService auditService;
 
-    private static String messageKey = "startLessons";
+    private static String messageKey = "initializeAndCreateLessons";
 
     protected String process(String lessonPackage, HttpServletRequest request) throws Exception {
 	FlashMessage flashMessage;
@@ -84,7 +84,7 @@ public class StartLessonsServlet extends AbstractStoreWDDXPacketServlet {
 	    List<Long> lessonIds = monitoringService.initializeAndCreateLessons(userID, lessonPackage);
 	    if (lessonIds != null && lessonIds.size() > 0) {
 		String lessonIdsCSV = "";
-		for (Long lessonId: lessonIds) {
+		for (Long lessonId : lessonIds) {
 		    lessonIdsCSV += lessonId + ",";
 		}
 		flashMessage = new FlashMessage(messageKey, lessonIdsCSV);
@@ -94,7 +94,7 @@ public class StartLessonsServlet extends AbstractStoreWDDXPacketServlet {
 	} catch (Exception e) {
 	    log.error("Exception thrown while starting lessons.", e);
 	    flashMessage = FlashMessage.getExceptionOccured(messageKey, e.getMessage());
-	    auditService.log(StartLessonsServlet.class.getName(), e.getMessage());
+	    auditService.log(InitializeAndCreateLessons.class.getName(), e.getMessage());
 	}
 
 	return flashMessage.serializeMessage();
