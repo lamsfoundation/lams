00279222325eba336ca7a008a8713e5bdf85b201
marinara <>
2008-11-18 03:24:16 +0000

    "Add" variable changes so we can use lamsframes.php with several Moodle tools

--- temp_moodle_dev/moodle/course/lamsframes.php
+++ temp_moodle_dev/moodle/course/lamsframes.php
@@ -4,7 +4,8 @@
  <script language="JavaScript">  
 <?php
 	require_once("../config.php");
-	require_once($CFG->dirroot.'/mod/quiz/lib.php');
+	//require_once($CFG->dirroot.'/mod/quiz/lib.php');
+	//require_once($CFG->dirroot.'/mod/choice/lib.php');
 	$id   = optional_param('id',0, PARAM_INT);
 	$lamsupdateurl = urlencode(optional_param('lamsUpdateURL', PARAM_TEXT));
 	$returnurl = urlencode(optional_param('returnUrl', PARAM_TEXT));
@@ -12,12 +13,13 @@
     $section = optional_param('section', PARAM_TEXT);
     $courseid = optional_param('course', PARAM_TEXT);
     $is_learner  = optional_param('is_learner',0, PARAM_INT);
+    $add = optional_param('add', PARAM_TEXT);
     if($id==0){
     	?>
         /*Lams navigation buttons frame */
     	document.write('<frameset rows="7%,*" >');
 		document.write('<frame name="button" src="lamsbuttons.php?<?php echo('&amp;lamsUpdateURL='.$lamsupdateurl.'&amp;returnUrl='.$returnurl)?>"  scrolling="no" noresize>'); 
-	 	document.write('<frame name="contenido" src="<?php echo($CFG->wwwroot.'/'.$dest.'?add=quiz&course='.$courseid.'&section='.$section.'&is_learner='.$is_learner)?>" scrolling="yes" noresize>');
+	 	document.write('<frame name="contenido" src="<?php echo($CFG->wwwroot.'/'.$dest.'?add='.$add.'&course='.$courseid.'&section='.$section.'&is_learner='.$is_learner)?>" scrolling="yes" noresize>');
 		document.write('</frameset>');
     	
     	<?php 
