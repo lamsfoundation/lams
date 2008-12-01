de355f2f9e24e128a14b3bf995c815841b9120e4
lfoxton <>
2008-12-01 05:30:31 +0000

    LI-179 Make sure quatz tables are set as capitals

--- win_installer/updater/src/sql/removeConfigs.sql
+++ win_installer/updater/src/sql/removeConfigs.sql
@@ -16,3 +16,18 @@ update lams_learning_library set valid_flag=0 where title="Dimdim";
 delete from lams_qtz_SIMPLE_TRIGGERS;
 delete from lams_qtz_TRIGGERS;
 delete from lams_qtz_JOB_DETAILS;
+
+-- From 2.2 onwards. WINDOWS ONLY. Make sure quatz tables are set as capitals
+Replace lams_qtz_blob_triggers with lams_qtz_BLOB_TRIGGERS
+Replace lams_qtz_calendars with lams_qtz_CALENDARS
+Replace lams_qtz_cron_triggers with lams_qtz_CRON_TRIGGERS
+Replace lams_qtz_fired_triggers with lams_qtz_FIRED_TRIGGERS
+Replace lams_qtz_job_details with lams_qtz_JOB_DETAILS
+Replace lams_qtz_job_listeners with lams_qtz_JOB_LISTENERS
+Replace lams_qtz_locks with ams_qtz_LOCKS
+Replace lams_qtz_paused_trigger_grps with lams_qtz_PAUSED_TRIGGER_GRPS
+Replace lams_qtz_scheduler_state with lams_qtz_SCHEDULER_STATE
+Replace lams_qtz_simple_triggers with lams_qtz_SIMPLE_TRIGGERS
+Replace lams_qtz_triggers with lams_qtz_TRIGGERS
+Replace lams_qtz_trigger_listeners with lams_qtz_TRIGGER_LISTENERS
+
