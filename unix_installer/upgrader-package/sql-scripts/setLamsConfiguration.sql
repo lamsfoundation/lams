601d646c6e38dd6ab3328498ca5ef15198240981
lfoxton <>
2008-12-01 03:29:42 +0000

    LI-177 adding dimdim as hidden for 2.2 updater

--- unix_installer/upgrader-package/sql-scripts/setLamsConfiguration.sql
+++ unix_installer/upgrader-package/sql-scripts/setLamsConfiguration.sql
@@ -1,3 +1,6 @@
 update lams_configuration set config_value='${LAMS_VERSION}' where config_key='Version';
 update lams_configuration set config_value='${LAMS_SERVER_VERSION}' where config_key='AuthoringClientVersion' or config_key='MonitorClientVersion' or config_key="LearnerClientVersion" or config_key='ServerVersionNumber';
 update lams_configuration set config_value='${LAMS_LANGUAGE_VERSION}' where config_key='DictionaryDateCreated';
+
+-- 2.2 only 
+update lams_learning_library set valid_flag=0 where title='Dimdim';
