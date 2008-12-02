76f5c8a9544e63ba4b245cf3ad76080f0a1e8d9b
lfoxton <>
2008-12-02 00:50:03 +0000

    LDEV-1893 - Event notification service db stuff added to autopatch script

--- lams_common/src/java/org/lamsfoundation/lams/dbupdates/patch0013_updateFrom21.sql
+++ lams_common/src/java/org/lamsfoundation/lams/dbupdates/patch0013_updateFrom21.sql
@@ -5,6 +5,38 @@ SET AUTOCOMMIT = 0;
 
 ----------------------Put all sql statements below here-------------------------
 
+-- LDEV1893 - Create notifications/events service ------------------------------
+CREATE TABLE lams_events (
+       uid BIGINT NOT NULL UNIQUE auto_increment
+     , scope VARCHAR(255) NOT NULL
+     , name VARCHAR(255) NOT NULL
+     , event_session_id BIGINT
+     , triggered TINYINT
+     , default_subject VARCHAR(255)
+     , default_message TEXT
+	 , subject VARCHAR(255)
+	 , message TEXT
+	 , fail_time DATETIME
+	 , INDEX (scope,name,event_session_id)
+     , PRIMARY KEY (uid)
+)TYPE=InnoDB;
+
+CREATE TABLE lams_event_subscriptions (
+       uid BIGINT NOT NULL UNIQUE auto_increment
+     , user_id BIGINT
+     , event_uid BIGINT
+     , delivery_method_id TINYINT UNSIGNED
+     , periodicity BIGINT
+     , last_operation_time DATETIME
+     , last_operation_message TEXT
+     , PRIMARY KEY (uid)
+     , CONSTRAINT EventSubscriptionsToUsers FOREIGN KEY (user_id)
+                  REFERENCES lams_user (user_id) ON DELETE CASCADE ON UPDATE CASCADE
+     , INDEX (event_uid)
+     , CONSTRAINT EventSubscriptionsToEvent FOREIGN KEY (event_uid)
+                  REFERENCES lams_events (uid) ON DELETE CASCADE ON UPDATE CASCADE
+)TYPE=InnoDB;
+
 -- LDEV1909 - Competence Editor Update Scripts ---------------------------------
 CREATE TABLE lams_competence (
        competence_id BIGINT NOT NULL UNIQUE auto_increment
