f39b3fba094ef50236c931928a7ce72ed3dcae35
marinara <>
2008-12-17 23:44:10 +0000

    New pages containing the navigation buttons used in LAMS for Wiki

---
+++ temp_moodle_dev/moodle/mod/wiki/showlamsnext.php
@@ -0,0 +1,15 @@
+       <html >
+		<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Untitled Document</title>
+		<script type="text/javascript">
+		/* Functoin that passa parameter to the buttons frame and display the button */
+        function appearbutton(){
+						parent.button.toggle("next");  /* makes button visible*/
+						
+		}
+		</script>
+		</head>
+		<body  onLoad="appearbutton()">			  
+		</body>
+		</html>
