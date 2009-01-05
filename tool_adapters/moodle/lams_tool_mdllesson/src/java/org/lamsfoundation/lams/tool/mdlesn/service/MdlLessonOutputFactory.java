7ad431bc970af29173c7b095c79fe7b6afb38765
marinara <>
2009-01-05 23:13:57 +0000

    Java changes for output finction purposes

--- tool_adapters/moodle/lams_tool_mdllesson/src/java/org/lamsfoundation/lams/tool/mdlesn/service/MdlLessonOutputFactory.java
+++ tool_adapters/moodle/lams_tool_mdllesson/src/java/org/lamsfoundation/lams/tool/mdlesn/service/MdlLessonOutputFactory.java
@@ -30,14 +30,14 @@ import org.lamsfoundation.lams.tool.ToolOutput;
 import org.lamsfoundation.lams.tool.ToolOutputDefinition;
 import org.lamsfoundation.lams.tool.exception.ToolException;
 import org.lamsfoundation.lams.tool.mdlesn.model.MdlLesson;
+import org.lamsfoundation.lams.tool.mdlesn.service.IMdlLessonService;
 
 /**
  * Creates the output definitions for mdllesson tool Adapter tool.
  */
 public class MdlLessonOutputFactory extends OutputFactory {
 
-    protected final static String OUTPUT_NAME_LEARNER_NUM_POSTS = "learner.number.of.comments";
-    protected final static String OUTPUT_NAME_LEARNER_NUM_ENTRIES = "learner.number.of.entries";
+    protected final static String OUTPUT_NAME_LEARNER_GRADE = "learner.percentage.grade";
 
     public MdlLessonOutputFactory() {
     }
@@ -47,48 +47,39 @@ public class MdlLessonOutputFactory extends OutputFactory {
      */
     public SortedMap<String, ToolOutputDefinition> getToolOutputDefinitions(Object toolContentObject)
 	    throws ToolException {
-	TreeMap<String, ToolOutputDefinition> definitionMap = new TreeMap<String, ToolOutputDefinition>();
-
-	ToolOutputDefinition definition = buildRangeDefinition(OUTPUT_NAME_LEARNER_NUM_POSTS, new Long(0), null);
-	definitionMap.put(OUTPUT_NAME_LEARNER_NUM_POSTS, definition);
-
-	ToolOutputDefinition definition2 = buildRangeDefinition(OUTPUT_NAME_LEARNER_NUM_ENTRIES, new Long(0), null);
-	definitionMap.put(OUTPUT_NAME_LEARNER_NUM_ENTRIES, definition2);
-
-	return definitionMap;
+		TreeMap<String, ToolOutputDefinition> definitionMap = new TreeMap<String, ToolOutputDefinition>();
+	
+		ToolOutputDefinition definition = buildRangeDefinition(OUTPUT_NAME_LEARNER_GRADE, new Long(0), null);
+		definitionMap.put(OUTPUT_NAME_LEARNER_GRADE, definition);
+	
+		return definitionMap;
     }
 
     public SortedMap<String, ToolOutput> getToolOutput(List<String> names, IMdlLessonService dlLessonService,
 	    Long toolSessionId, Long learnerId, MdlLesson mdlLesson, Long extSessionId) {
 
-	TreeMap<String, ToolOutput> map = new TreeMap<String, ToolOutput>();
-	if (names == null || names.contains(OUTPUT_NAME_LEARNER_NUM_POSTS)) {
-	    map.put(OUTPUT_NAME_LEARNER_NUM_POSTS, getExtToolOutput(OUTPUT_NAME_LEARNER_NUM_POSTS, dlLessonService,
-		    mdlLesson, learnerId, extSessionId.toString(), toolSessionId));
-	}
-	if (names.contains(OUTPUT_NAME_LEARNER_NUM_ENTRIES)) {
-	    map.put(OUTPUT_NAME_LEARNER_NUM_ENTRIES, getExtToolOutput(OUTPUT_NAME_LEARNER_NUM_ENTRIES, dlLessonService,
-		    mdlLesson, learnerId, extSessionId.toString(), toolSessionId));
-	}
-	return map;
-
+		TreeMap<String, ToolOutput> map = new TreeMap<String, ToolOutput>();
+		if (names == null || names.contains(OUTPUT_NAME_LEARNER_GRADE)) {
+		    map.put(OUTPUT_NAME_LEARNER_GRADE, getExtToolOutput(OUTPUT_NAME_LEARNER_GRADE, dlLessonService,
+			    mdlLesson, learnerId, extSessionId.toString(), toolSessionId));
+		}
+		return map;
     }
 
     public ToolOutput getToolOutput(String name, IMdlLessonService dlLessonService, Long toolSessionId, Long learnerId,
 	    MdlLesson getToolOutput, Long extSessionId) {
-	if (name != null) {
-	    return getExtToolOutput(name, dlLessonService, getToolOutput, learnerId, extSessionId.toString(),
-		    toolSessionId);
-	}
-	return null;
-
+		if (name != null) {
+		    return getExtToolOutput(name, dlLessonService, getToolOutput, learnerId, extSessionId.toString(),
+			    toolSessionId);
+		}
+		return null;
     }
 
-    public ToolOutput getExtToolOutput(String outputName, IMdlLessonService mdlLessonService, MdlLesson mdlLesson,
-	    Long userId, String extToolContentId, Long toolSessionId) {
-	int number = mdlLessonService.getExternalToolOutputInt(outputName, mdlLesson, userId, extToolContentId,
-		toolSessionId);
-	return new ToolOutput(outputName, getI18NText(outputName, true), new Long(number));
+    public ToolOutput getExtToolOutput(String outputName, IMdlLessonService mdlLessonService, MdlLesson mdlLesson, Long userId,
+	    String extToolContentId, Long toolSessionId) {
+		int number = mdlLessonService.getExternalToolOutputInt(outputName, mdlLesson, userId, extToolContentId,
+			toolSessionId);
+		return new ToolOutput(outputName, getI18NText(outputName, true), new Long(number));
     }
 
 }
