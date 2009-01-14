db32635cf9a8949f8e2f25f6afcb0be362689ca9
marinara <>
2009-01-14 02:47:37 +0000

    pass a parameter to the footer function in aim to not to display the Moodle's footer

--- temp_moodle_dev/moodle/mod/scorm/index.php
+++ temp_moodle_dev/moodle/mod/scorm/index.php
@@ -95,6 +95,7 @@
 
     print_table($table);
 
-    print_footer($course);
+    //we pass a new parameter to the function so it won't we printed if is_lams=1
+    print_footer($course,null, false,$scorm->is_lams);
 
 ?>
