ea56de4a81a972e364f6f8870c44d39e52687581
ruslan <>
2009-02-02 03:27:29 +0000

    LDEV-963: adding new posting feature

--- lams_tool_forum/src/java/org/lamsfoundation/lams/tool/forum/persistence/TimestampDao.java
+++ lams_tool_forum/src/java/org/lamsfoundation/lams/tool/forum/persistence/TimestampDao.java
@@ -42,46 +42,10 @@ public class TimestampDao extends HibernateDaoSupport {
     private static final String SQL_QUERY_TIMESTAMP_BY_MESSAGE_USER = 
 	" FROM " + Timestamp.class.getName() + " ts WHERE ts.message.uid = ? AND ts.forumUser.uid = ? ";
     
-    /*private static final String SQL_QUERY_ROOT_MESSAGE_NUMBER_BY_SESSION_MESSAGE_USER = 
-	"SELECT count(*) FROM " + Message.class.getName() + " AS p3 WHERE p3.uid = ? " +
-    	"AND p3.updated > ( SELECT ts.timestamp FROM " + Timestamp.class.getName() + " ts WHERE ts.message.uid = ? AND ts.forumUser.uid = ?)";
-    */
-    
     private static final String SQL_QUERY_MESSAGE_NUMBER_BY_MESSAGE_USER = 
 	"SELECT count(*) FROM " + Message.class.getName() + " mes WHERE mes.uid IN (SELECT seq.message.uid FROM " + 
 	MessageSeq.class.getName() + " seq WHERE seq.rootMessage.uid = ?) " + " AND mes.updated > (SELECT ts.timestamp FROM " + 
 	Timestamp.class.getName() + " ts WHERE ts.message.uid = ? AND ts.forumUser.uid = ?)";
-	
-    /*private static final String SQL_QUERY_ROOT_MESSAGE_CHANGED =
-	" SELECT count(*) FROM " + Message.class.getName() + " mes1, " + Message.class.getName() + " mes2" +
-	" WHERE mes1.updated = mes2.updated AND mes1.uid = ? AND mes1.parent.uid IS NULL " +
-	" AND mes1.updated > (SELECT ts.timestamp FROM " + Timestamp.class.getName() + 
-	" ts WHERE ts.message.uid = ? AND ts.forumUser.uid = ?)";
-    */
-    /*private static final String SQL_QUERY_ROOT_MESSAGE_CHANGED = 
-	"SELECT count(*) FROM " + Message.class.getName() + " AS mes LEFT JOIN mes.parent " +
-	" WHERE  ";
-	/*" WHERE p1.uid = p2.parent AND (p1.uid = ? OR p1.parent = ?) " +
-	" AND p2.toolSession.sessionId = ? AND p2.updated > ( SELECT ts.timestamp FROM " + Timestamp.class.getName() + 
-	" ts WHERE ts.message.uid = ? AND ts.forumUser.uid = ?)";
-    */
-    /* Recursive query for 10 levels down  
-    private static final String SQL_QUERY_MESSAGE_NUMBER_BY_SESSION_MESSAGE_USER = 
-	"SELECT count(*) FROM " + Message.class.getName() + " AS p1 LEFT JOIN p1.parent " + 
-	" WHERE p1.toolSession.sessionId = ? " +
-	" AND p1.updated > (SELECT ts.timestamp FROM " + Timestamp.class.getName() + " ts WHERE ts.message.uid = ? AND ts.forumUser.uid = ?)";
-    */
-    /*private static final String SQL_QUERY_MESSAGE_NUMBER_BY_SESSION_MESSAGE_USER = 
-	"SELECT count(*) FROM " + Message.class.getName() + " as p1 LEFT JOIN " + 
-	" p1.parent as p2 LEFT JOIN p2.parent as p3 LEFT JOIN p3.parent as p4 LEFT JOIN p4.parent as p5 " +
-	" WHERE p1.uid = ? OR p2.uid = ? OR p3.uid = ? OR p4.uid = ? OR p5.uid = ? AND p1.toolSession.sessionId = ? " +
-	" AND p1.updated > (SELECT ts.timestamp FROM " + Timestamp.class.getName() + " ts WHERE ts.message.uid = ? AND ts.forumUser.uid = ?)";
-    */
-    /*private static final String SQL_QUERY_MESSAGE_NUMBER_BY_SESSION_MESSAGE_USER = 
-	"SELECT count(*) FROM " + Message.class.getName() + " AS p1, " + Message.class.getName() + " AS p2 " +
-	" WHERE p1.uid = p2.parent AND (p1.uid = ? OR p1.parent = ?) " +
-	" AND p2.toolSession.sessionId = ? AND p2.updated > ( SELECT ts.timestamp FROM " + Timestamp.class.getName() + 
-	" ts WHERE ts.message.uid = ? AND ts.forumUser.uid = ?)";*/
     
     public void delete(Timestamp timestamp) {
 	this.getHibernateTemplate().delete(timestamp);
@@ -115,7 +79,6 @@ public class TimestampDao extends HibernateDaoSupport {
     /**
      * Get number of new postings.
      * 
-     * @param sessionId
      * @param messageId
      * @param userId
      * @return 
@@ -126,31 +89,11 @@ public class TimestampDao extends HibernateDaoSupport {
 	{
 		if (((Number)firstTimeList.get(0)).intValue() > 0) // if not first time
 		{
-		    //List rootPostingsList = this.getHibernateTemplate().find(SQL_QUERY_ROOT_MESSAGE_NUMBER_BY_SESSION_MESSAGE_USER, 
-			//    new Object[]{messageId, messageId, userId});
 		    List postingsList = this.getHibernateTemplate().find(SQL_QUERY_MESSAGE_NUMBER_BY_MESSAGE_USER, 
 			    new Object[]{messageId, messageId, userId});
-		    //List rootUpdatedList = this.getHibernateTemplate().find(SQL_QUERY_ROOT_MESSAGE_CHANGED, 
-			//    new Object[]{messageId, messageId, userId});
 				
 		    if(postingsList != null && postingsList.size() > 0)
-		    {
-			//int rootMesNum = ((Number)rootPostingsList.get(0)).intValue();
-			//int postingsNum = ((Number)postingsList.get(0)).intValue();
-			//int rootUpdated = ((Number)rootUpdatedList.get(0)).intValue();
-			
-			/*if (mesNum > 0) // if there are new messages
-			{   
-			    if (rootUpdated == 1) // if root posting was updated
-				return mesNum + 1;
-			    else
-				return mesNum;
-			}
-			else if (mesNum == 0) // if there are no messages
-			    return rootMesNum;*/
-			
 			return ((Number)postingsList.get(0)).intValue();
-		    }
 		    else
 			    return 0;
 		}
