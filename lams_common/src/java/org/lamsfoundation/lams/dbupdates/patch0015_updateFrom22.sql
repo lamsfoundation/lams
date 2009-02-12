a88f61d1992e0842c274ee3023b25e6421b569a1
lfoxton <>
2009-02-12 04:07:04 +0000

    LDEV-2099 - Forgot to commit db changes for server registration and lamscommunity tokens in the update scripts

--- lams_common/src/java/org/lamsfoundation/lams/dbupdates/patch0015_updateFrom22.sql
+++ lams_common/src/java/org/lamsfoundation/lams/dbupdates/patch0015_updateFrom22.sql
@@ -21,6 +21,24 @@ values ('SMTPUser','', 'config.smtp.user', 'config.header.email', 'STRING', 0);
 insert into lams_configuration (config_key, config_value, description_key, header_name, format, required) 
 values ('SMTPPassword','', 'config.smtp.password', 'config.header.email', 'STRING', 0);
 
+-- LDEV-2099 --------------
+ALTER TABLE lams_user ADD COLUMN lams_community_token VARCHAR(255);
+ALTER TABLE lams_user ADD COLUMN lams_community_username VARCHAR(255);
+
+CREATE TABLE lams_registration (
+	   uid BIGINT NOT NULL auto_increment
+     , site_name VARCHAR(255) NOT NULL
+     , organisation VARCHAR(255) NOT NULL
+     , name VARCHAR(255) NOT NULL
+     , email VARCHAR(255) NOT NULL
+	 , server_country VARCHAR(2) NOT NULL
+	 , public_directory TINYINT(1) DEFAULT 1
+	 , enable_lams_community TINYINT(1) DEFAULT 0
+	 , server_key VARCHAR(255) NOT NULL
+	 , server_id VARCHAR(255) NOT NULL
+     , PRIMARY KEY (uid)
+)TYPE=InnoDB;
+
 -- LDEV-2107 --------------
 INSERT into lams_workspace_folder_type VALUES (3, 'PUBLIC SEQUENCES');
 
