f39e5e344972c1d8226770c1f3b237d9e9d2a3d5
jliew <>
2009-03-10 15:33:47 +0000

    fix problems with foreign key references referencing wrong tables

--- tool_adapters/moodle/lams_tool_mdlchat/db/sql/create_lams_tool_mdlChat.sql
+++ tool_adapters/moodle/lams_tool_mdlchat/db/sql/create_lams_tool_mdlChat.sql
@@ -9,7 +9,7 @@ create table tl_mdchat10_configuration (uid bigint not null auto_increment, conf
 create table tl_mdchat10_mdlchat (uid bigint not null auto_increment, create_date datetime, update_date datetime, define_later bit, content_in_use bit, run_offline bit, tool_content_id bigint, ext_tool_content_id bigint, ext_user_name varchar(255), ext_course_id varchar(255), ext_section varchar(255), primary key (uid))TYPE=InnoDB;
 create table tl_mdchat10_session (uid bigint not null auto_increment, session_end_date datetime, session_start_date datetime, status integer, session_id bigint, ext_session_id bigint, session_name varchar(250), mdlchat_uid bigint, primary key (uid))TYPE=InnoDB;
 create table tl_mdchat10_user (uid bigint not null auto_increment, user_id bigint, last_name varchar(255), login_name varchar(255), first_name varchar(255), finishedActivity bit, mdlchat_session_uid bigint, entry_uid bigint, primary key (uid))TYPE=InnoDB;
-alter table tl_mdchat10_session add index FKmdchat10_session (mdlChat_uid), add constraint FKmdchat10_session foreign key (mdlChat_uid) references tl_mdchat10_user (uid);
+alter table tl_mdchat10_session add index FKmdchat10_session (mdlchat_uid), add constraint FKmdchat10_session foreign key (mdlchat_uid) references tl_mdchat10_mdlchat (uid);
 alter table tl_mdchat10_user add index FKmdchat10_user (mdlchat_session_uid), add constraint FKmdchat10_user foreign key (mdlchat_session_uid) references tl_mdchat10_session (uid);
 
 INSERT INTO tl_mdchat10_configuration (
--- tool_adapters/moodle/lams_tool_mdlforum/db/sql/create_lams_tool_mdlForum.sql
+++ tool_adapters/moodle/lams_tool_mdlforum/db/sql/create_lams_tool_mdlForum.sql
@@ -9,7 +9,7 @@ create table tl_mdfrum10_configuration (uid bigint not null auto_increment, conf
 create table tl_mdfrum10_mdlforum (uid bigint not null auto_increment, create_date datetime, update_date datetime, define_later bit, content_in_use bit, run_offline bit, tool_content_id bigint, ext_tool_content_id bigint, ext_user_name varchar(255), ext_course_id varchar(255), ext_section varchar(255), primary key (uid))TYPE=InnoDB;
 create table tl_mdfrum10_session (uid bigint not null auto_increment, session_end_date datetime, session_start_date datetime, status integer, session_id bigint, ext_session_id bigint, session_name varchar(250), mdlforum_uid bigint, primary key (uid))TYPE=InnoDB;
 create table tl_mdfrum10_user (uid bigint not null auto_increment, user_id bigint, last_name varchar(255), login_name varchar(255), first_name varchar(255), finishedActivity bit, mdlforum_session_uid bigint, entry_uid bigint, primary key (uid))TYPE=InnoDB;
-alter table tl_mdfrum10_session add index FKmdfrum10_session (mdlForum_uid), add constraint FKmdfrum10_session foreign key (mdlForum_uid) references tl_mdmdfrum10_user (uid);
+alter table tl_mdfrum10_session add index FKmdfrum10_session (mdlForum_uid), add constraint FKmdfrum10_session foreign key (mdlforum_uid) references tl_mdfrum10_mdlforum (uid);
 alter table tl_mdfrum10_user add index FKmdfrum10_user (mdlforum_session_uid), add constraint FK_mdfrum10_blah foreign key (mdlforum_session_uid) references tl_mdfrum10_session (uid);
 
 INSERT INTO tl_mdfrum10_configuration (
