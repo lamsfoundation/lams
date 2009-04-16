3cae36fc3c73c4ce595adc48b7ef2f8ac166daaf
erniegx <ernieg@gmail.com>
2009-04-16 08:02:34 +0000

    LDEV-2239: Upgrading mysql connector

--- lams_build/deploy-tool/deploy.bat
+++ lams_build/deploy-tool/deploy.bat
@@ -1,2 +1,2 @@
-java -classpath lib/lams-tool-deploy.jar;lib/commons-configuration-1.1.jar;lib/commons-lang-2.0.jar;lib/commons-collections.jar;lib/commons-logging.jar;lib/commons-io-1.0.jar;lib/commons-dbutils-1.0.jar;lib/mysql-connector-java-3.1.12-bin.jar;lib/xstream-1.1.2.ja
+java -classpath lib/lams-tool-deploy.jar;lib/commons-configuration-1.1.jar;lib/commons-lang-2.0.jar;lib/commons-collections.jar;lib/commons-logging.jar;lib/commons-io-1.0.jar;lib/commons-dbutils-1.0.jar;lib/mysql-connector-java-5.0.8-bin.jar;lib/xstream-1.1.2.ja
 r org.lamsfoundation.lams.tool.deploy.Deploy ./deploy.properties True
