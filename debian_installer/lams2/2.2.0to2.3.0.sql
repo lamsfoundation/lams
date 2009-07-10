673423347b56e7120356a92ca5e7c8ec2f057134
jliew <>
2009-07-10 08:05:11 +0000

    remove accidental newline char

--- debian_installer/lams2/2.2.0to2.3.0.sql
+++ debian_installer/lams2/2.2.0to2.3.0.sql
@@ -3,6 +3,5 @@ ALTER TABLE lams_tool ADD COLUMN pedagogical_planner_url TEXT;
 
 -- Version strings
 update lams_configuration set config_value='2.3' where config_key='Version';
-update lams_configuration set config_value='2.3.0.200905110000' where config_key='AuthoringClientVersion' or config_key='Mo
-nitorClientVersion' or config_key="LearnerClientVersion" or config_key='ServerVersionNumber';
+update lams_configuration set config_value='2.3.0.200905110000' where config_key='AuthoringClientVersion' or config_key='MonitorClientVersion' or config_key="LearnerClientVersion" or config_key='ServerVersionNumber';
 update lams_configuration set config_value='2009-05-11' where config_key='DictionaryDateCreated';
