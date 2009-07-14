72325784375aa89f6a36aca006333bdc77ecc752
jliew <>
2009-07-14 01:49:43 +0000

    LDEV-2379 improvements

--- temp_moodle_dev/moodle/blocks/lamstwo_status/block_lamstwo_status.php
+++ temp_moodle_dev/moodle/blocks/lamstwo_status/block_lamstwo_status.php
@@ -14,9 +14,12 @@ class block_lamstwo_status extends block_base {
     const STATUS_COMPLETED = 'status_completed';
     const STATUS_UNKNOWN = 'status_unknown';
     
+    // when there is a problem contacting lams server, set true
+    public static $error = false;
+    
     function init() {
         $this->title = get_string('block_title', 'lamstwo');
-        $this->version = 2009070800;
+        $this->version = 2009071400;
     }
     
     function get_content() {
@@ -26,19 +29,23 @@ class block_lamstwo_status extends block_base {
         
         $this->content = new stdClass;
         $this->content->text = $this->print_all_lesson_status();
-        $this->content->footer = '';
+        $this->content->footer = '&nbsp;';
         
         return $this->content;
     }
     
     function print_all_lesson_status() {
-        $return_str = '<table width="100%">';
+        $return_str = '<table width="100%" style="margin-top: 6px; border-bottom: thin solid grey;">';
         $array = $this->get_all_lesson_status();
-        foreach ($array as $lamstwo_id => $status_obj_array) {
-            $return_str .= '<tr><th colspan="2" align="centre">'.$status_obj_array[0]->lamstwo_name.'</th></tr>';
-            foreach ($status_obj_array as $status_obj) {
-                $status_str =  get_string($status_obj->status_i18n_key, 'lamstwo');
-                $return_str .= '<tr><td>'.$status_obj->lesson_name.'</td><td align="right">'.$status_str.'</td></tr>';
+        if (self::$error) {
+            $return_str .= '<tr><td>'.get_string($array, 'lamstwo').'</td></tr>';
+        } else {
+            foreach ($array as $lamstwo_id => $status_obj_array) {
+                $return_str .= '<tr><th style="border-top: thin solid grey;" colspan="2" align="left">'.$status_obj_array[0]->lamstwo_name.'</th></tr>';
+                foreach ($status_obj_array as $status_obj) {
+                    $status_str =  get_string($status_obj->status_i18n_key, 'lamstwo');
+                    $return_str .= '<tr><td>'.$status_obj->lesson_name.'</td><td align="right">'.$status_str.'</td></tr>';
+                }
             }
         }
         $return_str .= '</table>';
@@ -51,7 +58,7 @@ class block_lamstwo_status extends block_base {
         
         // get course module ids
         $cms = get_records_sql("SELECT cm.* FROM {$CFG->prefix}course_modules cm, {$CFG->prefix}modules m 
-            WHERE cm.module=m.id AND m.name='lamstwo' AND cm.course={$COURSE->id}");
+            WHERE cm.module=m.id AND m.name='lamstwo' AND cm.course={$COURSE->id} AND cm.visible=1 ORDER BY cm.section");
         
         foreach ($cms as $cmid => $cm) {
             $context = get_context_instance(CONTEXT_MODULE, $cmid);
@@ -63,12 +70,18 @@ class block_lamstwo_status extends block_base {
                 // get status for all lessons
                 foreach ($lessons as $obj) {
                     $xml_array = $this->get_single_lesson_status($USER->username, $COURSE->id, $obj->lesson_id);
-                    $status_obj = new stdClass();
-                    $status_obj->lamstwo_name = $obj->lamstwo_name;
-                    $status_obj->lesson_id = $obj->lesson_id;
-                    $status_obj->lesson_name = $obj->lesson_name;
-                    $status_obj->status_i18n_key = $this->process_xml($xml_array);
-                    $status_array[$obj->lamstwo_id][] = $status_obj;
+                    if (is_array($xml_array)) {
+                        $status_obj = new stdClass();
+                        $status_obj->lamstwo_name = $obj->lamstwo_name;
+                        $status_obj->lesson_id = $obj->lesson_id;
+                        $status_obj->lesson_name = $obj->lesson_name;
+                        $status_obj->status_i18n_key = $this->process_xml($xml_array);
+                        $status_array[$obj->lamstwo_id][] = $status_obj;
+                    } else {
+                        // error contacting lams server, $xml_array is an error string
+                        self::$error = true;
+                        return $xml_array;
+                    }
                 }
             }
         }
@@ -96,7 +109,7 @@ class block_lamstwo_status extends block_base {
         $request = "$CFG->lamstwo_serverurl$service?serverId=$CFG->lamstwo_serverid&datetime=$datetime_encoded&hashValue=$hashvalue&method=singleStudentProgress&username=$username&courseId=$courseid&lsId=$lsId&progressUser=$username";
     
         // GET call to LAMS
-        $xml = file_get_contents($request);
+        $xml = @file_get_contents($request);
         
         if(!empty($http_response_header[0])) {
         	// Retrieve HTTP status code
@@ -107,16 +120,22 @@ class block_lamstwo_status extends block_base {
         		case 200:
         			break;
         		case 503:
+        		    return 'restcall503';
         			break;
         		case 403:
+        		    return 'restcall403';
         			break;
         		case 400:
+        		    return 'restcall400';
         			break;
+        		case 500:
+        		    return 'restcall500';
+        		    break;
         		default:
+        		    return 'restcalldefault';
         	}
         } else {
-        	//return get_string('restcallfail', 'lamstwo');
-        	//print_error('restcallfail', 'lamstwo', $CFG->wwwroot.'/course/view.php?id='.$courseid);
+        	return 'restcallfail';
         }
         
     	$xml_array = xmlize($xml);
