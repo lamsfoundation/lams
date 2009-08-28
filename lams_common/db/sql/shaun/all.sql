8c15f5286498028ff789094bf80d082afdb8186c
erniegx <ernieg@gmail.com>
2009-08-28 05:04:04 +0000

    Fixing issues that MySQL 5.1 seems to be picky about now

--- lams_common/db/sql/shaun/all.sql
+++ lams_common/db/sql/shaun/all.sql
@@ -4,7 +4,7 @@ SET FOREIGN_KEY_CHECKS=0;
 insert into lams_workspace_folder(workspace_folder_id,parent_folder_id,name,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id) 
 values(11,null,'Bulgarian Workspace',11,now(),now(),1);
 insert into lams_workspace (workspace_id, default_fld_id, name) values (11,11,'Bulgarian Test');
---insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (11,11);
+-- insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (11,11);
 
 insert into lams_user(user_id,login,password,title,first_name,last_name,address_line_1,address_line_2,address_line_3,city,state,country,day_phone,evening_phone,mobile_phone,fax,email,disabled_flag,create_date,authentication_method_id,workspace_id, flash_theme_id,html_theme_id,locale_id) 
 values(11,'bulgarian','bulgarian','Dr','Bulgarian','Test','11','Bulgarian Ave',null,'Sofia',null,'Bulgaria','0211111111','0211111112','0411111111','0211111113','bulgarian@xx.os',0,'20041223',1,11,1,2,14);
@@ -29,7 +29,7 @@ SET FOREIGN_KEY_CHECKS=0;
 insert into lams_workspace_folder(workspace_folder_id,parent_folder_id,name,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id) 
 values(12,null,'Chinese Workspace',12,now(),now(),1);
 insert into lams_workspace (workspace_id, default_fld_id, name) values (12,12,'Chinese Test');
---insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (12,12);
+-- insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (12,12);
 
 insert into lams_user(user_id,login,password,title,first_name,last_name,address_line_1,address_line_2,address_line_3,city,state,country,day_phone,evening_phone,mobile_phone,fax,email,disabled_flag,create_date,authentication_method_id,workspace_id, flash_theme_id,html_theme_id,locale_id) 
 values(12,'chinese','chinese','Dr','Chinese','Test','12','Chinese Ave',null,'Beijing',null,'China','0211111111','0211111112','0411111111','0211111113','chinese@xx.os',0,'20041223',1,12,1,2,5);
@@ -55,7 +55,7 @@ SET FOREIGN_KEY_CHECKS=0;
 insert into lams_workspace_folder(workspace_folder_id,parent_folder_id,name,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id) 
 values(10,null,'French Workspace',10,now(),now(),1);
 insert into lams_workspace (workspace_id, default_fld_id, name) values (10,10,'French Test');
---insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (10,10);
+-- insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (10,10);
 
 insert into lams_user(user_id,login,password,title,first_name,last_name,address_line_1,address_line_2,address_line_3,city,state,country,day_phone,evening_phone,mobile_phone,fax,email,disabled_flag,create_date,authentication_method_id,workspace_id, flash_theme_id,html_theme_id,locale_id) 
 values(10,'french','french','Dr','French','Test','10','French Ave',null,'Paris',null,'France','0211111111','0211111112','0411111111','0211111113','french@xx.os',0,'20041223',1,10,1,2,6);
@@ -80,7 +80,7 @@ SET FOREIGN_KEY_CHECKS=0;
 insert into lams_workspace_folder(workspace_folder_id,parent_folder_id,name,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id) 
 values(13,null,'German Workspace',13,now(),now(),1);
 insert into lams_workspace (workspace_id, default_fld_id, name) values (13,13,'German Test');
---insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (13,13);
+-- insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (13,13);
 
 insert into lams_user(user_id,login,password,title,first_name,last_name,address_line_1,address_line_2,address_line_3,city,state,country,day_phone,evening_phone,mobile_phone,fax,email,disabled_flag,create_date,authentication_method_id,workspace_id, flash_theme_id,html_theme_id,locale_id) 
 values(13,'german','german','Dr','German','Test','13', 'German Ave',null,'Berlin',null,'Germany','0211111111','0211111112','0411111111','0211111113','german@xx.os',0,'20041223',1,13,1,2,4);
@@ -105,7 +105,7 @@ SET FOREIGN_KEY_CHECKS=0;
 insert into lams_workspace_folder(workspace_folder_id,parent_folder_id,name,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id) 
 values(21,null,'Hungarian Workspace',21,now(),now(),1);
 insert into lams_workspace (workspace_id, default_fld_id, name) values (21,21,'Hungarian Test');
---insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (21,21);
+-- insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (21,21);
 
 insert into lams_user(user_id,login,password,title,first_name,last_name,address_line_1,address_line_2,address_line_3,city,state,country,day_phone,evening_phone,mobile_phone,fax,email,disabled_flag,create_date,authentication_method_id,workspace_id, flash_theme_id,html_theme_id,locale_id) 
 values(21,'hungarian','hungarian','Dr','Hungarian','Test','21', 'Hungarian Ave',null,'Bukarest',null,'Hungry','0211111111','0211111112','0411111111','0211111113','hungarian@xx.os',0,'20041223',1,21,1,2,13);
@@ -130,7 +130,7 @@ SET FOREIGN_KEY_CHECKS=0;
 insert into lams_workspace_folder(workspace_folder_id,parent_folder_id,name,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id) 
 values(16,null,'Italian Workspace',16,now(),now(),1);
 insert into lams_workspace (workspace_id, default_fld_id, name) values (16,16,'Italian Test');
---insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (16,16);
+-- insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (16,16);
 
 insert into lams_user(user_id,login,password,title,first_name,last_name,address_line_1,address_line_2,address_line_3,city,state,country,day_phone,evening_phone,mobile_phone,fax,email,disabled_flag,create_date,authentication_method_id,workspace_id, flash_theme_id,html_theme_id,locale_id) 
 values(16,'italian','italian','Dr','Italian','Test','16', 'Italian Ave',null,'Rome',null,'Italy','0211111111','0211111112','0411111111','0211111113','italian@xx.os',0,'20041223',1,16,1,2,7);
@@ -156,7 +156,7 @@ SET FOREIGN_KEY_CHECKS=0;
 insert into lams_workspace_folder(workspace_folder_id,parent_folder_id,name,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id) 
 values(18,null,'Korean Workspace',18, now(),now(),1);
 insert into lams_workspace (workspace_id, default_fld_id, name) values (18,18,'Korean Test');
---insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (18,18);
+-- insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (18,18);
 
 insert into lams_user(user_id,login,password,title,first_name,last_name,address_line_1,address_line_2,address_line_3,city,state,country,day_phone,evening_phone,mobile_phone,fax,email,disabled_flag,create_date,authentication_method_id,workspace_id, flash_theme_id,html_theme_id,locale_id) 
 values(18,'korean','korean','Dr','Korean','Test','18', 'Korean Ave',null,'Seoul',null,'Korea','0211111111','0211111112','0411111111','0211111113','korean@xx.os',0,'20041223',1,18,1,2,10);
@@ -182,7 +182,7 @@ SET FOREIGN_KEY_CHECKS=0;
 insert into lams_workspace_folder(workspace_folder_id,parent_folder_id,name,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id) 
 values(20,null,'Maori Workspace',20,now(),now(),1);
 insert into lams_workspace (workspace_id, default_fld_id, name) values (20,20,'Maori Test');
---insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (20,20);
+-- insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (20,20);
 
 insert into lams_user(user_id,login,password,title,first_name,last_name,address_line_1,address_line_2,address_line_3,city,state,country,day_phone,evening_phone,mobile_phone,fax,email,disabled_flag,create_date,authentication_method_id,workspace_id, flash_theme_id,html_theme_id,locale_id) 
 values(20,'maori','maori','Dr','Maori','Test','20', 'Maori Ave',null,'Auckland',null,'New Zealand','0211111111','0211111112','0411111111','0211111113','maori@xx.os',0,'20041223',1,20,1,2,3);
@@ -207,7 +207,7 @@ SET FOREIGN_KEY_CHECKS=0;
 insert into lams_workspace_folder(workspace_folder_id,parent_folder_id,name,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id) 
 values(14,null,'Norwegian Workspace',14,now(),now(),1);
 insert into lams_workspace (workspace_id, default_fld_id, name) values (14,14,'Norwegian Test');
---insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (14,14);
+-- insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (14,14);
 
 insert into lams_user(user_id,login,password,title,first_name,last_name,address_line_1,address_line_2,address_line_3,city,state,country,day_phone,evening_phone,mobile_phone,fax,email,disabled_flag,create_date,authentication_method_id,workspace_id, flash_theme_id,html_theme_id,locale_id) 
 values(14,'norwegian','norwegian','Dr','Norwegian','Test','14', 'Norwegian Ave',null,'Oslo',null,'Norway','0211111111','0211111112','0411111111','0211111113','norwegian@xx.os',0,'20041223',1,14,1,2,8);
@@ -232,7 +232,7 @@ SET FOREIGN_KEY_CHECKS=0;
 insert into lams_workspace_folder(workspace_folder_id,parent_folder_id,name,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id) 
 values(17,null,'Polish Workspace',17,now(),now(),1);
 insert into lams_workspace (workspace_id, default_fld_id, name) values (17,17,'Polish Test');
---insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (17,17);
+-- insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (17,17);
 
 insert into lams_user(user_id,login,password,title,first_name,last_name,address_line_1,address_line_2,address_line_3,city,state,country,day_phone,evening_phone,mobile_phone,fax,email,disabled_flag,create_date,authentication_method_id,workspace_id, flash_theme_id,html_theme_id,locale_id) 
 values(17,'polish','polish','Dr','Polish','Test','17', 'Polish Ave',null,'Warsaw',null,'Poland','0211111111','0211111112','0411111111','0211111113','polish@xx.os',0,'20041223',1,17,1,2,11);
@@ -258,7 +258,7 @@ SET FOREIGN_KEY_CHECKS=0;
 insert into lams_workspace_folder(workspace_folder_id,parent_folder_id,name,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id) 
 values(15,null,'Portuguese Workspace',15,now(),now(),1);
 insert into lams_workspace (workspace_id, default_fld_id, name) values (15,15,'Portuguese Test');
---insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (15,15);
+-- insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (15,15);
 
 insert into lams_user(user_id,login,password,title,first_name,last_name,address_line_1,address_line_2,address_line_3,city,state,country,day_phone,evening_phone,mobile_phone,fax,email,disabled_flag,create_date,authentication_method_id,workspace_id, flash_theme_id,html_theme_id,locale_id) 
 values(15,'portuguese','portuguese','Dr','Portuguese','Test','15', 'Portuguese Ave',null,'Brasilia',null,'Brazil','0211111111','0211111112','0411111111','0211111113','portuguese@xx.os',0,'20041223',1,15,1,2,12);
@@ -283,7 +283,7 @@ SET FOREIGN_KEY_CHECKS=0;
 insert into lams_workspace_folder(workspace_folder_id,parent_folder_id,name,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id) 
 values(19,null,'Spanish Workspace',19,now(),now(),1);
 insert into lams_workspace (workspace_id, default_fld_id, name) values (19,19,'Spanish Test');
---insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (19,19);
+-- insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (19,19);
 
 insert into lams_user(user_id,login,password,title,first_name,last_name,address_line_1,address_line_2,address_line_3,city,state,country,day_phone,evening_phone,mobile_phone,fax,email,disabled_flag,create_date,authentication_method_id,workspace_id, flash_theme_id,html_theme_id,locale_id) 
 values(19,'spanish','spanish','Dr','Spanish','Test','19', 'Spanish Ave',null,'Madrid',null,'Spain','0211111111','0211111112','0411111111','0211111113','spanish@xx.os',0,'20041223',1,19,1,2,2);
@@ -309,7 +309,7 @@ SET FOREIGN_KEY_CHECKS=0;
 insert into lams_workspace_folder(workspace_folder_id,parent_folder_id,name,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id) 
 values(9,null,'Swedish Workspace',9,now(),now(),1);
 insert into lams_workspace (workspace_id, default_fld_id, name) values (9,9,'Swedish Test');
---insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (9,9);
+-- insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (9,9);
 
 insert into lams_user(user_id,login,password,title,first_name,last_name,address_line_1,address_line_2,address_line_3,city,state,country,day_phone,evening_phone,mobile_phone,fax,email,disabled_flag,create_date,authentication_method_id,workspace_id, flash_theme_id,html_theme_id,locale_id) 
 values(9,'swedish','swedish','Dr','Sweden','Test','9','Swedish Ave',null,'Stockholm',null,'Sweden','0211111111','0211111112','0411111111','0211111113','swedish@xx.os',0,'20041223',1,9,1,2,9);
@@ -335,7 +335,7 @@ SET FOREIGN_KEY_CHECKS=0;
 insert into lams_workspace_folder(workspace_folder_id,parent_folder_id,name,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id) 
 values(24,null,'Thai Workspace',24,now(),now(),1);
 insert into lams_workspace (workspace_id, default_fld_id, name) values (24,24,'Thai Test');
---insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (24,24);
+-- insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (24,24);
 
 insert into lams_user(user_id,login,password,title,first_name,last_name,address_line_1,address_line_2,address_line_3,city,state,country,day_phone,evening_phone,mobile_phone,fax,email,disabled_flag,create_date,authentication_method_id,workspace_id, flash_theme_id,html_theme_id,locale_id) 
 values(24,'thai','thai','Dr','Thai','Test','24', 'Thai Ave',null,'Bangkok',null,'Thailand','0211111111','0211111112','0411111111','0211111113','thai@xx.os',0,'20041223',1,24,1,2,16);
@@ -361,7 +361,7 @@ SET FOREIGN_KEY_CHECKS=0;
 insert into lams_workspace_folder(workspace_folder_id,parent_folder_id,name,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id) 
 values(25,null,'Greek Workspace',25,now(),now(),1);
 insert into lams_workspace (workspace_id, default_fld_id, name) values (25,25,'Greek Test');
---insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (11,11);
+-- insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (11,11);
 
 insert into lams_user(user_id,login,password,title,first_name,last_name,address_line_1,address_line_2,address_line_3,city,state,country,day_phone,evening_phone,mobile_phone,fax,email,disabled_flag,create_date,authentication_method_id,workspace_id, flash_theme_id,html_theme_id,locale_id) 
 values(25,'greek','greek','Dr','Greek','Test','25','Greek Ave',null,'Athens',null,'Greece','0211111111','0211111112','0411111111','0211111113','greek@xx.os',0,'20041223',1,25,1,2,17);
@@ -387,7 +387,7 @@ SET FOREIGN_KEY_CHECKS=0;
 insert into lams_workspace_folder(workspace_folder_id,parent_folder_id,name,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id) 
 values(26,null,'Welsh Workspace',26,now(),now(),1);
 insert into lams_workspace (workspace_id, default_fld_id, name) values (26,26,'Welsh Test');
---insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (11,11);
+-- insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (11,11);
 
 insert into lams_user(user_id,login,password,title,first_name,last_name,address_line_1,address_line_2,address_line_3,city,state,country,day_phone,evening_phone,mobile_phone,fax,email,disabled_flag,create_date,authentication_method_id,workspace_id, flash_theme_id,html_theme_id,locale_id) 
 values(26,'welsh','welsh','Dr','Welsh','Test','26','Welsh Ave',null,'Cardiff',null,'Wales','0211111111','0211111112','0411111111','0211111113','welsh@xx.os',0,'20041223',1,26,1,2,15);
@@ -409,7 +409,7 @@ INSERT INTO lams_user_organisation_role (user_organisation_role_id, user_organis
 insert into lams_workspace_folder(workspace_folder_id,parent_folder_id,name,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id)
 values(27,null,'Dutch Workspace',27,now(),now(),1);
 insert into lams_workspace (workspace_id, default_fld_id, name) values (27,27,'Dutch Test');
---insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (11,11);
+-- insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (11,11);
 
 insert into lams_user(user_id,login,password,title,first_name,last_name,address_line_1,address_line_2,address_line_3,city,state,country,day_phone,evening_phone,mobile_phone,fax,email,disabled_flag,create_date,authentication_method_id,workspace_id, flash_theme_id,html_theme_id,locale_id)
 values(27,'dutch','dutch','Dr','Dutch','Test','26','Dutch Ave',null,'Amsterdam',null,'Netherlands','0211111111','0211111112','0411111111','0211111113','dutch@xx.os',0,'20041223',1,27,1,2,18);
@@ -433,7 +433,7 @@ INSERT INTO lams_user_organisation_role (user_organisation_role_id, user_organis
 insert into lams_workspace_folder(workspace_folder_id,parent_folder_id,name,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id)
 values(28,null,'Arabic Workspace',28,now(),now(),1);
 insert into lams_workspace (workspace_id, default_fld_id, name) values (28,28,'Arabic Test');
---insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (11,11);
+-- insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (11,11);
 
 insert into lams_user(user_id,login,password,title,first_name,last_name,address_line_1,address_line_2,address_line_3,city,state,country,day_phone,evening_phone,mobile_phone,fax,email,disabled_flag,create_date,authentication_method_id,workspace_id, flash_theme_id,html_theme_id,locale_id)
 values(28,'arabic','arabic','Dr','Arabic','Test','28','Arabic Ave',null,'Amman',null,'Jordan','0211111111','0211111112','0411111111','0211111113','arabic@xx.os',0,'20041223',1,28,1,2,19);
@@ -456,7 +456,7 @@ INSERT INTO lams_user_organisation_role (user_organisation_role_id, user_organis
 insert into lams_workspace_folder(workspace_folder_id,parent_folder_id,name,user_id,create_date_time,last_modified_date_time,lams_workspace_folder_type_id)
 values(29,null,'Danish Workspace',29,now(),now(),1);
 insert into lams_workspace (workspace_id, default_fld_id, name) values (29,29,'Danish Test');
---insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (11,11);
+-- insert into lams_workspace_folder (workspace_id, workspace_folder_id) values (11,11);
 
 insert into lams_user(user_id,login,password,title,first_name,last_name,address_line_1,address_line_2,address_line_3,city,state,country,day_phone,evening_phone,mobile_phone,fax,email,disabled_flag,create_date,authentication_method_id,workspace_id, flash_theme_id,html_theme_id,locale_id)
 values(29,'danish','danish','Dr','Danish','Test','29','Danish Ave',null,'Copenhagen',null,'Denmark','0211111111','0211111112','0411111111','0211111113','danish@xx.os',0,'20041223',1,29,1,2,20);
