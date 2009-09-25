5d49eeaf81f1d43d9e9cc233a695ec24c6bee1f1
erniegx <ernieg@gmail.com>
2009-09-25 03:55:06 +0000

    Updating version number on HEAD to 2.4

--- lams_common/db/sql/insert_lams_windows_config_data.sql
+++ lams_common/db/sql/insert_lams_windows_config_data.sql
@@ -5,7 +5,7 @@ insert into lams_configuration (config_key, config_value, description_key, heade
 values ('ServerURLContextPath','lams/', 'config.server.url.context.path', 'config.header.system', 'STRING', 1);
 
 insert into lams_configuration (config_key, config_value, description_key, header_name, format, required) 
-values ('Version','2.3.2', 'config.version', 'config.header.system', 'STRING', 1);
+values ('Version','2.4', 'config.version', 'config.header.system', 'STRING', 1);
 
 insert into lams_configuration (config_key, config_value, description_key, header_name, format, required) 
 values ('TempDir','C:/lams/temp', 'config.temp.dir', 'config.header.system', 'STRING', 1);
@@ -50,16 +50,16 @@ insert into lams_configuration (config_key, config_value, description_key, heade
 values ('AuthoringActivitiesColour', 'true', 'config.authoring.activities.colour', 'config.header.look.feel', 'BOOLEAN', 1);
 
 insert into lams_configuration (config_key, config_value, description_key, header_name, format, required) 
-values ('AuthoringClientVersion','2.3.2.@datetimestamp@', 'config.authoring.client.version', 'config.header.versions', 'STRING', 1);
+values ('AuthoringClientVersion','2.4.@datetimestamp@', 'config.authoring.client.version', 'config.header.versions', 'STRING', 1);
 
 insert into lams_configuration (config_key, config_value, description_key, header_name, format, required) 
-values ('MonitorClientVersion','2.3.2.@datetimestamp@', 'config.monitor.client.version', 'config.header.versions', 'STRING', 1);
+values ('MonitorClientVersion','2.4.@datetimestamp@', 'config.monitor.client.version', 'config.header.versions', 'STRING', 1);
 
 insert into lams_configuration (config_key, config_value, description_key, header_name, format, required) 
-values ('LearnerClientVersion','2.3.2.@datetimestamp@', 'config.learner.client.version', 'config.header.versions', 'STRING', 1);
+values ('LearnerClientVersion','2.4.@datetimestamp@', 'config.learner.client.version', 'config.header.versions', 'STRING', 1);
 
 insert into lams_configuration (config_key, config_value, description_key, header_name, format, required) 
-values ('ServerVersionNumber','2.3.2.@datetimestamp@', 'config.server.version.number', 'config.header.versions', 'STRING', 1);
+values ('ServerVersionNumber','2.4.@datetimestamp@', 'config.server.version.number', 'config.header.versions', 'STRING', 1);
 
 insert into lams_configuration (config_key, config_value, description_key, header_name, format, required) 
 values ('ServerLanguage','en_AU', 'config.server.language', 'config.header.look.feel', 'STRING', 1);
