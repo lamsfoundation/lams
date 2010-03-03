dc2f32797a5648447b8db4e368664b7b838130d9
jliew <>
2010-03-03 05:08:15 +0000

    LDEV-2476 forgot to add table for this feature

--- lams_common/db/sql/create_lams_11_tables.sql
+++ lams_common/db/sql/create_lams_11_tables.sql
@@ -1149,6 +1149,18 @@ CREATE TABLE lams_planner_recent_learning_designs (
    , PRIMARY KEY (user_id,learning_design_id)
 )TYPE=InnoDB;
 
+CREATE TABLE lams_planner_node_role (
+       uid BIGINT(20) NOT NULL AUTO_INCREMENT
+     , node_uid BIGINT(20) NOT NULL
+     , user_id BIGINT(20) NOT NULL
+     , role_id INT(6) NOT NULL
+     , PRIMARY KEY (uid)
+)TYPE=InnoDB;
+
+ALTER TABLE lams_planner_node_role ADD CONSTRAINT FK_planner_node_role_user FOREIGN KEY (user_id) REFERENCES lams_user (user_id) ON DELETE CASCADE ON UPDATE NO ACTION;
+ALTER TABLE lams_planner_node_role ADD CONSTRAINT FK_planner_node_role_node FOREIGN KEY (node_uid) REFERENCES lams_planner_nodes (uid) ON DELETE CASCADE ON UPDATE NO ACTION;
+ALTER TABLE lams_planner_node_role ADD CONSTRAINT FK_planner_node_role_role FOREIGN KEY (role_id) REFERENCES lams_role (role_id) ON DELETE CASCADE ON UPDATE NO ACTION;
+
 CREATE TABLE lams_openid_config (
      config_key VARCHAR(20) NOT NULL UNIQUE
    , config_value VARCHAR(255) NOT NULL
