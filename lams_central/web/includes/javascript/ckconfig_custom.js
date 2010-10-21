46d8c21c543aec67ce72acb1ce41cf72d5e1b908
MarcinCieslak <mgr.inz.marcin.cieslak@gmail.com>
2010-10-21 18:57:21 +0000

    LDEV-2577, LDEV-2588: Fixed names of buttons that have changed from FCKEditor. CKEditor are hidden until they are completely initialized.

--- lams_central/web/includes/javascript/ckconfig_custom.js
+++ lams_central/web/includes/javascript/ckconfig_custom.js
@@ -1,36 +1,36 @@
 CKEDITOR.config.toolbar_Default = [
-	['Source','-','FitWindow', 'Preview','PasteWord','Undo','Redo','Bold','Italic','Underline', '-','Subscript','Superscript','OrderedList','UnorderedList','-','Outdent','Indent','JustifyLeft','JustifyCenter','JustifyRight','JustifyFull','TextColor','BGColor','Equation'], 
-	['Paint_Button','MoviePlayer','VideoRecorder','Image','Link','Table','Rule','Smiley','SpecialChar','Templates','FontFormat','FontName','FontSize','About']
+	['Source','-','Maximize', 'Preview','PasteFromWord','Undo','Redo','Bold','Italic','Underline', '-','Subscript','Superscript','NumberedList','BulletedList','-','Outdent','Indent','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','TextColor','BGColor','Equation'], 
+	['Paint_Button','MoviePlayer','VideoRecorder','Image','Link','Table','HorizontalRule','Smiley','SpecialChar','Templates','Format','Font','FontSize','About']
 ] ;
 
 CKEDITOR.config.toolbar_DefaultLearner = [
-	['Preview','PasteWord'],
+	['Preview','PasteFromWord'],
 	['Undo','Redo'],
 	['Bold','Italic','Underline', '-','Subscript','Superscript'],
-	['OrderedList','UnorderedList','-','Outdent','Indent'],
-	['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
+	['NumberedList','BulletedList','-','Outdent','Indent'],
+	['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
 	['Equation','About'],
 	['TextColor','BGColor'],
-	['Table','Rule','Smiley','SpecialChar'],
-	['FontFormat','FontName','FontSize']
+	['Table','HorizontalRule','Smiley','SpecialChar'],
+	['Format','Font','FontSize']
 ] ;
 
 CKEDITOR.config.toolbar_CustomWiki = [
-	['Preview','PasteWord'],
+	['Preview','PasteFromWord'],
 	['Undo','Redo'],
 	['Bold','Italic','Underline', '-','Subscript','Superscript'],
-	['OrderedList','UnorderedList','-','Outdent','Indent'],
-	['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
+	['NumberedList','BulletedList','-','Outdent','Indent'],
+	['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
 	['WikiLink','Link','Image'],
 	['Equation','About'],
 	['TextColor','BGColor'],
-	['Table','Rule','Smiley','SpecialChar'],
-	['FontFormat','FontName','FontSize']
+	['Table','HorizontalRule','Smiley','SpecialChar'],
+	['Format','Font','FontSize']
 ] ;
 
 CKEDITOR.config.toolbar_CustomPedplanner = [
-					      ['Source','-','FitWindow','Preview','PasteWord','Bold','Italic','Underline', '-','OrderedList','UnorderedList','-','Outdent','Indent','JustifyLeft','JustifyCenter','JustifyRight','JustifyFull','TextColor','BGColor','Equation'], 
-	['Image','Link','Table','Smiley','FontName','FontSize','WikiLink']
+	['Source','-','Maximize','Preview','PasteFromWord','Bold','Italic','Underline', '-','NumberedList','BulletedList','-','Outdent','Indent','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','TextColor','BGColor','Equation'], 
+	['Image','Link','Table','Smiley','Font','FontSize']
 ] ;
 
 
@@ -49,3 +49,14 @@ CKEDITOR.config.enterMode = 'div' ;
 CKEDITOR.plugins.addExternal('WikiLink', CKEDITOR.basePath + '../tool/lawiki10/wikilink/', 'plugin.js') ;
 //CKEDITOR.config.extraPlugins = 'equation,movieplayer,videorecorder,paint,wikilink' ;
 CKEDITOR.config.extraPlugins = 'WikiLink' ;
+
+// ---- Additional scripts -----
+// Hides editor instaces until they are fully initialized
+
+CKEDITOR.on('instanceCreated', function(e){
+	e.editor.element.$.style.display = 'none';
+});
+
+CKEDITOR.on('instanceReady', function(e){
+	e.editor.element.$.style.display = '';
+});
