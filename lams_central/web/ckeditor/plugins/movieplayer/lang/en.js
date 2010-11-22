63caec7d86903e659e906042390eee95cba3a801
MarcinCieslak <mgr.inz.marcin.cieslak@gmail.com>
2010-11-22 07:14:38 +0000

    LDEV-2577: Readded movieplayer plugin for CKEditor 3.x

---
+++ lams_central/web/ckeditor/plugins/movieplayer/lang/en.js
@@ -0,0 +1,13 @@
+CKEDITOR.plugins.setLang( 'movieplayer', 'en',	    {			movieplayer :	        {				// Toolbar button				
+				MoviePlayerBtn : 'Media Player',				
+				MoviePlayerTooltip : 'Insert/Edit Movies or Audio',				
+				
+				// Dialog				
+				DlgMoviePlayerTitle : 'Media Player Properties',				
+				MoviePlayerURL : 'URL:',				
+				MoviePlayerURLDesc : 'Select a .avi, .flv, .mov, .mp3, .mp4, .mpv, .wma, .wmv file or youtube URL',				
+				MoviePlayerWidth : 'Width:',				
+				MoviePlayerHeight : 'Height:',				
+				MoviePlayerAutoplay : 'Auto Play:',								MoviePlayerBrowseServer : 'Browse Server',				
+				// Dialog errors				
+				MoviePlayerNoUrl : 'Please specify a movie URL.'	        }	    });
---
+++ lams_central/web/ckeditor/plugins/movieplayer/plugin.js
@@ -0,0 +1,5 @@
+/* 
+ *   CKEditior plugin.js for Movie Player
+ *  ------------
+ */CKEDITOR.plugins.add('movieplayer',	       {	          requires : ['iframedialog'],			  lang : [ 'en' ],	          init : function(editor) {	             var pluginPath = CKEDITOR.plugins.getPath('movieplayer');	             	             editor.ui.addButton(	                'MoviePlayer',	                {		                   label : editor.lang.movieplayer.MoviePlayerBtn,		                   command : 'MoviePlayer',		                   icon : pluginPath + 'filmreel.gif',		                   title : editor.lang.movieplayer.MoviePlayerTooltip	                }	             );	             	             editor.addCommand('MoviePlayer', {exec:showDialogPlugin});	             	             CKEDITOR.dialog.addIframe(	            	'MoviePlayer',	            	editor.lang.movieplayer.DlgMoviePlayerTitle,	                pluginPath + 'movieplayer.html',	                450,	                260	             );	          }	       }	    );
+function showDialogPlugin(e){	e.openDialog('MoviePlayer');}
