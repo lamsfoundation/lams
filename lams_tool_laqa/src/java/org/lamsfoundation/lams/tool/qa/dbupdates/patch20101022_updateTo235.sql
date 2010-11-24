216bca26cc0f08ef546c116c394f7953c6d418a9
wkiaora <balandrey@gmail.com>
2010-11-24 10:37:37 +0000

    LDEV-2524 adding autopatch script
    *merge from lams2_3_release

---
+++ lams_tool_laqa/src/java/org/lamsfoundation/lams/tool/qa/dbupdates/patch20101022_updateTo235.sql
@@ -0,0 +1,14 @@
+-- SQL statements to update to LAMS 2.3.5
+
+-- Turn off autocommit, so nothing is committed if there is an error
+SET AUTOCOMMIT = 0;
+
+----------------------Put all sql statements below here-------------------------
+
+ALTER TABLE tl_laqa11_que_content ADD COLUMN answer_required TINYINT(1) NOT NULL DEFAULT 0;
+
+----------------------Put all sql statements above here-------------------------
+
+-- If there were no errors, commit and restore autocommit to on
+COMMIT;
+SET AUTOCOMMIT = 1;
