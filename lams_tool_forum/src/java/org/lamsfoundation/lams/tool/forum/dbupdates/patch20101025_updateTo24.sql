9f68cca69460a326b5b85a4543bda04b9efc845c
wkiaora <balandrey@gmail.com>
2010-11-29 16:15:16 +0000

    LDEV-2612 fixed Forum problems on upgrade
    *merge from lams2_3_release

--- lams_tool_forum/src/java/org/lamsfoundation/lams/tool/forum/dbupdates/patch20101025_updateTo24.sql
+++ lams_tool_forum/src/java/org/lamsfoundation/lams/tool/forum/dbupdates/patch20101025_updateTo24.sql
@@ -4,8 +4,8 @@ SET AUTOCOMMIT = 0;
 ----------------------Put all sql statements below here-------------------------
 
 -- LDEV-2545 Deliver email messages to students/teachers on forum postings
-alter table tl_lafrum11_forum add column notify_learners_on_forum_posting tinyint;
-alter table tl_lafrum11_forum add column notify_teachers_on_forum_posting tinyint;
+alter table tl_lafrum11_forum add column notify_learners_on_forum_posting tinyint DEFAULT 0;
+alter table tl_lafrum11_forum add column notify_teachers_on_forum_posting tinyint DEFAULT 0;
 
 ----------------------Put all sql statements above here-------------------------
 
