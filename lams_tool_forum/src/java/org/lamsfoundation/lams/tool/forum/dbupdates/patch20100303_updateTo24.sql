00db1ab75dc0a354a430762538201ba6d2b5ddca
wkiaora <balandrey@gmail.com>
2010-12-31 13:08:59 +0000

    LDEV-1743 fixing the problem with messages having the same sequence_id
    *merge from lams2_3_release

--- lams_tool_forum/src/java/org/lamsfoundation/lams/tool/forum/dbupdates/patch20100303_updateTo24.sql
+++ lams_tool_forum/src/java/org/lamsfoundation/lams/tool/forum/dbupdates/patch20100303_updateTo24.sql
@@ -1,4 +1,4 @@
 
 -- LDEV-1743 Be able to remove/update and change order of threads
 alter table tl_lafrum11_message add column sequence_id integer;
-update tl_lafrum11_message set sequence_id='0' where sequence_id IS NULL;
+update tl_lafrum11_message set sequence_id=uid where sequence_id IS NULL;
