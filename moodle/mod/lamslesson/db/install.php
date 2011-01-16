82b05bec226a5e8a2e57f35727aefe8b8a3d2fad
erniegx <ernieg@gmail.com>
2011-01-16 23:29:52 +0000

    Initial import LAMS - Moodle 2 integration

---
+++ moodle/mod/lamslesson/db/install.php
@@ -0,0 +1,34 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file replaces the legacy STATEMENTS section in db/install.xml,
+ * lib.php/modulename_install() post installation hook and partially defaults.php
+ *
+ * @package   mod_lamslesson
+ * @copyright 2011 LAMS Foundation - Ernie Ghiglione (ernieg@lamsfoundation.org)  <your@email.adress>
+ * @license  http://www.gnu.org/licenses/gpl-2.0.html GNU GPL v2
+ */
+
+/**
+ * Post installation procedure
+ */
+function xmldb_lamslesson_install() {
+
+    /// insert here code to perform some actions
+    
+}
---
+++ moodle/mod/lamslesson/db/log.php
@@ -0,0 +1,38 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Definition of log events
+ * NOTE: this is an example how to insert log event during installation/update.
+ * It is not really essential to know about it, but these logs were created as example
+ * in the previous 1.9 NEWMODULE.
+ *
+ * @package   mod_lamslesson
+ * @copyright 2011 LAMS Foundation - Ernie Ghiglione (ernieg@lamsfoundation.org)  <your@email.adress>
+ * @license  http://www.gnu.org/licenses/gpl-2.0.html GNU GPL v2
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+global $DB;
+
+$logs = array(
+    array('module'=>'lamslesson', 'action'=>'add', 'mtable'=>'lamslesson', 'field'=>'name'),
+    array('module'=>'lamslesson', 'action'=>'update', 'mtable'=>'lamslesson', 'field'=>'name'),
+    array('module'=>'lamslesson', 'action'=>'view', 'mtable'=>'lamslesson', 'field'=>'name'),
+    array('module'=>'lamslesson', 'action'=>'view all', 'mtable'=>'lamslesson', 'field'=>'name')
+);
