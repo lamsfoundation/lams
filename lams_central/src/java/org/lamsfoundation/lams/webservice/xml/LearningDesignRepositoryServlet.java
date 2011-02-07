6c622fc902f451ad79501061202fc107c7771133
wkiaora <balandrey@gmail.com>
2011-02-07 17:55:41 +0000

    LDEV-2637 Show public folder when creating a lesson
    *merge from lams2_3_release

--- lams_central/src/java/org/lamsfoundation/lams/webservice/xml/LearningDesignRepositoryServlet.java
+++ lams_central/src/java/org/lamsfoundation/lams/webservice/xml/LearningDesignRepositoryServlet.java
@@ -220,6 +220,7 @@ public class LearningDesignRepositoryServlet extends HttpServlet {
 	ContentTreeNode root = new ContentTreeNode(rootFolder);
 	FolderContentDTO userFolder = service.getUserWorkspaceFolder(userId);
 	root.addChild(buildContentTreeNode(userFolder, userId, mode));
+	
 	FolderContentDTO dummyOrgFolder = new FolderContentDTO(msgService.getMessage("organisations"), msgService
 		.getMessage("folder"), null, null, FolderContentDTO.FOLDER, new Long(WorkspaceAction.ORG_FOLDER_ID
 		.longValue()), WorkspaceFolder.READ_ACCESS, null);
@@ -236,6 +237,11 @@ public class LearningDesignRepositoryServlet extends HttpServlet {
 	    dummyOrgNode.addChild(buildContentTreeNode(orgFolder, userId, mode));
 	}
 	root.addChild(dummyOrgNode);
+	
+	FolderContentDTO publicFolder = service.getPublicWorkspaceFolder(userId);
+	if (publicFolder != null) {
+	    root.addChild(buildContentTreeNode(publicFolder, userId, mode));
+	}
 	return root;
     }
 
