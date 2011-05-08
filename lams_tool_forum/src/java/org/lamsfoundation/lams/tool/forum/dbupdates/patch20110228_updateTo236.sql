8d6cf04ec6c1da33edd6e1299ad87dad81eb3b8d
erniegx <ernieg@gmail.com>
2011-05-08 11:08:57 +0000

    LDEV-2677: upgrade script fix

--- lams_tool_forum/src/java/org/lamsfoundation/lams/tool/forum/dbupdates/patch20110228_updateTo236.sql
+++ lams_tool_forum/src/java/org/lamsfoundation/lams/tool/forum/dbupdates/patch20110228_updateTo236.sql
@@ -5,25 +5,30 @@ SET AUTOCOMMIT = 0;
 
 ----------------------Put all sql statements below here-------------------------
 
-ALTER TABLE tl_lafrum11_forum ADD COLUMN allow_rate_messages smallint;
+ALTER TABLE tl_lafrum11_forum ADD COLUMN allow_rate_messages smallint not null default 0;
 
 -- LDEV-2651 Forum option to rate other student's postings
 CREATE TABLE tl_lafrum11_message_rating (
        uid BIGINT(20) NOT NULL AUTO_INCREMENT
      , rating float
      , user_id BIGINT(20) NOT NULL
-     , response_id BIGINT(20) NOT NULL
+     , message_id BIGINT(20) NOT NULL
      , PRIMARY KEY (uid)
      , INDEX (user_id)
      , CONSTRAINT FK_tl_lafrum11_message_rating_1 FOREIGN KEY (user_id)
                   REFERENCES tl_lafrum11_forum_user (uid)
-     , INDEX (response_id)
-     , CONSTRAINT FK_tl_lafrum11_message_rating_2 FOREIGN KEY (response_id)
+     , INDEX (message_id)
+     , CONSTRAINT FK_tl_lafrum11_message_rating_2 FOREIGN KEY (message_id)
                   REFERENCES tl_lafrum11_message (uid)
 )TYPE=InnoDB;
 
 
 UPDATE lams_tool SET tool_version = "20110228" WHERE tool_signature = "lafrum11";
+
+-- LDEV-2657
+alter table tl_lafrum11_forum add column submission_deadline datetime default null;
+
+UPDATE lams_tool SET tool_version = "20110408" WHERE tool_signature = "lafrum11";
 
 ----------------------Put all sql statements above here-------------------------
 
