a748f9ff3448b1d21febcb6f3e226a7860667651
wkiaora <balandrey@gmail.com>
2011-05-18 16:45:07 +0000

    LDEV-2657 Date and time restriction in tools

--- lams_central/web/includes/javascript/monitorToolSummaryAdvanced.js
+++ lams_central/web/includes/javascript/monitorToolSummaryAdvanced.js
@@ -15,3 +15,74 @@ function toggleAdvancedOptionsVisibility(div, img, imageUrl)
 		img.src = treeOpenIcon;
 	}
 }
+
+//check if jquery is loaded
+if ((typeof jQuery != 'undefined') && (typeof settings != 'undefined')) {
+	//support for setting up submission deadline
+	$(function(){
+		$("#datetime").datetimepicker();
+		
+		if (settings.submissionDeadline != "") {
+			var date = new Date(eval(settings.submissionDeadline));
+			$("#dateInfo").html( formatDate2(date) );
+			
+			//open up date restriction area
+			toggleAdvancedOptionsVisibility(document.getElementById('restrictUsageDiv'), document.getElementById('restrictUsageTreeIcon'),settings.lams);
+		}
+	});	
+	
+	function formatDate2(date) {
+		var currHour = "" + date.getHours();
+		if (currHour.length == 1) {
+			currHour = "0" + currHour;
+		}			
+		var currMin = "" + date.getMinutes();
+		if (currMin.length == 1) {
+			currMin = "0" + currMin;
+		}
+		return $.datepicker.formatDate( 'mm/dd/yy', date ) + " " + currHour + ":" + currMin;
+	}
+
+	function setSubmissionDeadline() {
+		//get the timestamp in milliseconds since midnight Jan 1, 1970
+		var date = $("#datetime").datetimepicker('getDate');
+		if (date == null) {
+			return;
+		}
+
+		var reqIDVar = new Date();
+		var parameterDelimiter = (settings.setSubmissionDeadlineUrl.indexOf("?") == -1) ? "?" : "&"; 
+		
+		var url = settings.setSubmissionDeadlineUrl + parameterDelimiter + "toolContentID=" + settings.toolContentID + "&submissionDeadline=" +
+					+ date.getTime() + "&reqID=" + reqIDVar.getTime();
+
+		$.ajax({
+			url : url,
+			success : function() {
+				$.growlUI(settings.messageNotification, settings.messageRestrictionSet);
+				$("#datetimeDiv").hide();
+				$("#dateInfo").html(formatDate2(date) );
+				$("#dateInfoDiv").show();
+			}
+		});
+	}
+	function removeSubmissionDeadline() {
+		var reqIDVar = new Date();
+		var parameterDelimiter = (settings.setSubmissionDeadlineUrl.indexOf("?") == -1) ? "?" : "&"; 
+		
+		var url = settings.setSubmissionDeadlineUrl + parameterDelimiter + "toolContentID=" + settings.toolContentID + "&submissionDeadline=" +
+				"&reqID=" + reqIDVar.getTime();
+
+		$.ajax({
+			url : url,
+			success : function() {
+				$.growlUI(settings.messageNotification, settings.messageRestrictionRemoved);
+				$("#dateInfoDiv").hide();
+				
+				$("#datetimeDiv").show();
+				$("#datetime").val("");
+			}
+		});
+	}
+}
+
