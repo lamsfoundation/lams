ce763cdf18290083b22b6ad41a9c0cb386322e8d
MarcinCieslak <mgr.inz.marcin.cieslak@gmail.com>
2011-12-08 08:33:24 +0000

    LDEV-2733: Removing patched 01, 03 and 05 as mentioned in previous JIRA comments. Removing only some parts of 06 script.

--- lams_common/src/java/org/lamsfoundation/lams/dbupdates/patch02040005.sql
+++
@@ -1,8 +0,0 @@
-SET AUTOCOMMIT = 0;
-
--- LDEV-2509 Adding configuration setting for Embedded SMTP server
-insert into lams_configuration (config_key, config_value, description_key, header_name, format, required) 
-values ('InternalSMTPServer','true', 'config.use.internal.smtp.server', 'config.header.email', 'BOOLEAN', 0);
-
-COMMIT;
-SET AUTOCOMMIT = 1;
