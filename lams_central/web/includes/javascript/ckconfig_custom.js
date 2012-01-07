a27d5b96e306c9fc153684069b353cd5ec9814f2
erniegx <ernieg@gmail.com>
2012-01-07 12:29:18 +0000

    LDEV-2775: Use <div> rather than <p> as default entermode for CKEditor

--- lams_central/web/includes/javascript/ckconfig_custom.js
+++ lams_central/web/includes/javascript/ckconfig_custom.js
@@ -44,6 +44,7 @@ CKEDITOR.config.format_tags	= 'div;h1;h2;h3;h4;h5;h6;pre;address;p' ;
 CKEDITOR.config.enterMode = 'div' ;
 CKEDITOR.plugins.addExternal('WikiLink', CKEDITOR.basePath + '../tool/lawiki10/wikilink/', 'plugin.js') ;
 CKEDITOR.config.extraPlugins = 'videorecorder,kaltura,WikiLink,equation,paint,movieplayer' ;
+CKEDITOR.config.enterMode = CKEDITOR.ENTER_DIV; 
 
 // ---- Additional scripts -----
 // Hides editor instaces until they are fully initialized
