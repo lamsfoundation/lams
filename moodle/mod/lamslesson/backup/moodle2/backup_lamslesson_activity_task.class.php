941ad535edb314ff3bcb89504edab085f6538e31
erniegx <ernieg@gmail.com>
2012-01-15 18:10:33 +0000

    LDEV-2650: implementing lamslesson moodle2 backup based on choice example

---
+++ moodle/mod/lamslesson/backup/moodle2/backup_lamslesson_activity_task.class.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * @package moodlecore
+ * @subpackage backup-moodle2
+ * @copyright 2010 onwards Eloy Lafuente (stronk7) {@link http://stronk7.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once($CFG->dirroot . '/mod/lamslesson/backup/moodle2/backup_lamslesson_stepslib.php'); // Because it exists (must)
+require_once($CFG->dirroot . '/mod/lamslesson/backup/moodle2/backup_lamslesson_settingslib.php'); // Because it exists (optional)
+
+/**
+ * lamslesson backup task that provides all the settings and steps to perform one
+ * complete backup of the activity
+ */
+class backup_lamslesson_activity_task extends backup_activity_task {
+
+    /**
+     * Define (add) particular settings this activity can have
+     */
+    protected function define_my_settings() {
+        // No particular settings for this activity
+    }
+
+    /**
+     * Define (add) particular steps this activity can have
+     */
+    protected function define_my_steps() {
+        // Choice only has one structure step
+        $this->add_step(new backup_lamslesson_activity_structure_step('lamslesson_structure', 'lamslesson.xml'));
+    }
+
+    /**
+     * Code the transformations to perform in the activity in
+     * order to get transportable (encoded) links
+     */
+    static public function encode_content_links($content) {
+        global $CFG;
+
+        $base = preg_quote($CFG->wwwroot,"/");
+
+        // Link to the list of lamslessons
+        $search="/(".$base."\/mod\/lamslesson\/index.php\?id\=)([0-9]+)/";
+        $content= preg_replace($search, '$@CHOICEINDEX*$2@$', $content);
+
+        // Link to lamslesson view by moduleid
+        $search="/(".$base."\/mod\/lamslesson\/view.php\?id\=)([0-9]+)/";
+        $content= preg_replace($search, '$@CHOICEVIEWBYID*$2@$', $content);
+
+        return $content;
+    }
+}
--- moodle/mod/lamslesson/lib.php
+++ moodle/mod/lamslesson/lib.php
@@ -810,7 +810,7 @@ function lamslesson_supports($feature) {
         case FEATURE_GRADE_HAS_GRADE:         return true;
         case FEATURE_GRADE_OUTCOMES:          return true;
         case FEATURE_RATE:                    return false;
-        case FEATURE_BACKUP_MOODLE2:          return false;
+        case FEATURE_BACKUP_MOODLE2:          return true;
 
         default: return null;
     }
