d8cad0ad05b4177fc1b94cdfd4808b357cc2457d
MarcinCieslak <mgr.inz.marcin.cieslak@gmail.com>
2012-02-20 09:56:59 +0000

    LDEV-2806: Fill Activtiy table with missing information about tranistions.

---
+++ lams_common/src/java/org/lamsfoundation/lams/dbupdates/patch02040008.sql
@@ -0,0 +1,15 @@
+-- Turn off autocommit, so nothing is committed if there is an error
+SET AUTOCOMMIT = 0;
+
+-- LDEV-2806 Fill Activities with missing transition data
+UPDATE lams_learning_activity AS act
+ JOIN  lams_learning_transition AS trans_to
+  ON trans_to.to_activity_id = act.activity_id
+ JOIN lams_learning_transition AS trans_from
+  ON trans_from.from_activity_id = act.activity_id
+ SET act.transition_from_id = trans_from.transition_id,
+     act.transition_to_id = trans_to.transition_id;
+
+-- If there were no errors, commit and restore autocommit to on
+COMMIT;
+SET AUTOCOMMIT = 1;
