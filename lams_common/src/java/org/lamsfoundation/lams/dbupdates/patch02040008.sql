d571cfc33ee30ba94651afdedb68b84ad3b1a3b5
MarcinCieslak <mgr.inz.marcin.cieslak@gmail.com>
2012-02-20 20:31:31 +0000

    LDEV-2806: Update border (start and end) activities as well by changing SQL statement selecting transitions for them.

--- lams_common/src/java/org/lamsfoundation/lams/dbupdates/patch02040008.sql
+++ lams_common/src/java/org/lamsfoundation/lams/dbupdates/patch02040008.sql
@@ -3,15 +3,16 @@ SET AUTOCOMMIT = 0;
 
 -- LDEV-2806 Fill Activities with missing transition data
 UPDATE lams_learning_activity AS act
- JOIN  lams_learning_transition AS trans_to
+ LEFT JOIN  lams_learning_transition AS trans_to
   ON trans_to.to_activity_id = act.activity_id
   AND trans_to.transition_type = 0
- JOIN lams_learning_transition AS trans_from
+ LEFT JOIN lams_learning_transition AS trans_from
   ON trans_from.from_activity_id = act.activity_id
   AND trans_from.transition_type = 0
  SET act.transition_from_id = trans_from.transition_id,
      act.transition_to_id = trans_to.transition_id;
 
+     
 -- If there were no errors, commit and restore autocommit to on
 COMMIT;
 SET AUTOCOMMIT = 1;
