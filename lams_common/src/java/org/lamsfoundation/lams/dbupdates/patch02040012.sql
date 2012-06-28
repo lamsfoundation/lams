35917e1e3c7882f83d762c24ec5ee0ae963e94b4
MarcinCieslak <mgr.inz.marcin.cieslak@gmail.com>
2012-06-28 18:29:56 +0000

    LDEV-2890, CNG-26: Recommit suspicious SQL entries.

--- lams_common/src/java/org/lamsfoundation/lams/dbupdates/patch02040012.sql
+++ lams_common/src/java/org/lamsfoundation/lams/dbupdates/patch02040012.sql
@@ -1,7 +1,7 @@
 -- Turn off autocommit, so nothing is committed if there is an error
 SET AUTOCOMMIT = 0;
 
--- LDEV-2890 When importing a sequence make the suffix string optional; turned off by default
+-- LDEV-2890 When importing a sequence make the suffix string optional, turned off by default
 insert into lams_configuration (config_key, config_value, description_key, header_name, format, required) 
 values ('SuffixImportedLD','false', 'config.authoring.suffix', 'config.header.features', 'BOOLEAN', 0);
 
