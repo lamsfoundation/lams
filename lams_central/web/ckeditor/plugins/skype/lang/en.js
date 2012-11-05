e582cf3845a4ca84e6ea709c249d475876c0227d
erniegx <ernieg@gmail.com>
2012-11-05 19:08:42 +0000

    LDEV-2880: Upgrade Skype plugin for CKEditor

---
+++ lams_central/web/ckeditor/plugins/skype/lang/en.js
@@ -0,0 +1,11 @@
+CKEDITOR.plugins.setLang( 'skype', 'en',{
+	skype : {
+		// Toolbar button
+		SkypeBtn : 'Insert Skype button',
+		SkypeTitle: 'Skype Properties',
+		SkypeTxt: 'Please type the skype contact name.',
+		SkypeHelp: 'How to use this?',
+		SkypeContactName: 'Skype contact name:',
+		SkypeValidationError: 'You must enter an Skype contact name or Cancel to abort.'
+	}
+});
