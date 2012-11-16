4e6c33625056d164b43e5ea8f47cd2c7a88fe83a
erniegx <ernieg@gmail.com>
2012-11-16 19:24:52 +0000

    LDEV-2903: remove upgrade sql scripts as now they are done by autopatch

--- lams_common/db/sql/updatescripts/alter_211_gmap.sql
+++
@@ -1,4 +0,0 @@
--- FOR LAMS 2.1.1 
--- Adding a configuration entry into for the google map api key
-
-insert into lams_configuration (config_key, config_value, description_key, header_name, format, required)  values ('GmapKey','', 'config.gmap.gmapkey', 'config.gmap.section.title', 'STRING', 0);
--- lams_common/db/sql/updatescripts/alter_211_tool_adapter.sql
+++
@@ -1,2 +0,0 @@
--- Adding a new tool type for tool adapters
-ALTER TABLE lams_tool ADD COLUMN ext_lms_id VARCHAR(255) DEFAULT null;
