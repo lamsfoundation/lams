7b021f32e5158b52664060e256fffbee09f2494f
erniegx <ernieg@gmail.com>
2013-09-07 14:07:09 +0000

    LDEV-3118 turning single activity lessons ON by default

--- lams_common/src/java/org/lamsfoundation/lams/dbupdates/patch02040017.sql
+++ lams_common/src/java/org/lamsfoundation/lams/dbupdates/patch02040017.sql
@@ -4,7 +4,7 @@ SET AUTOCOMMIT = 0;
 SET FOREIGN_KEY_CHECKS=0;
 
 -- 	LDEV-3070 Allow single activity lesson creation on redesigned index page
-ALTER TABLE lams_organisation ADD COLUMN enable_single_activity_lessons TINYINT(1) NOT NULL DEFAULT 0
+ALTER TABLE lams_organisation ADD COLUMN enable_single_activity_lessons TINYINT(1) NOT NULL DEFAULT 1
 	  AFTER enable_learner_gradebook; 
 
 -- If there were no errors, commit and restore autocommit to on
