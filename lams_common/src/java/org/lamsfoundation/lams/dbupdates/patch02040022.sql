a0d3cb0f7ddd7e6cb7fa2eb853ebf79becba1726
wkiaora <balandrey@gmail.com>
2014-01-20 15:39:39 +0000

    LDEV-3147 temporarily suspend removing DB columns

--- lams_common/src/java/org/lamsfoundation/lams/dbupdates/patch02040022.sql
+++ lams_common/src/java/org/lamsfoundation/lams/dbupdates/patch02040022.sql
@@ -4,12 +4,13 @@ SET AUTOCOMMIT = 0;
 SET FOREIGN_KEY_CHECKS=0;
 
 -- LDEV-3147 Simplify tools: get rid of instructions tab, define in monitor and run offline activity options
+/* Comment out this part until we will be able to modify tools' tool_insert.sql and tool_insert.sql activity_insert.sql to exclude inserting according columns   
 ALTER TABLE lams_learning_activity DROP COLUMN define_later_flag;
 ALTER TABLE lams_learning_activity DROP COLUMN run_offline_flag;
 
 ALTER TABLE lams_tool DROP COLUMN supports_run_offline_flag;
 ALTER TABLE lams_tool DROP COLUMN define_later_url;
-
+*/
 ALTER TABLE lams_learning_design DROP COLUMN online_instructions;
 ALTER TABLE lams_learning_design DROP COLUMN offline_instructions;
 
