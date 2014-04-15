38ec15237ab2ce03f901019934aa20517de8f5e0
MarcinCieslak <mgr.inz.marcin.cieslak@gmail.com>
2014-04-15 08:58:37 +0000

    LDEV-3208: Complie patterns just once and reuse multiple times afterwards.

--- lams_common/src/java/org/lamsfoundation/lams/util/ValidationUtil.java
+++ lams_common/src/java/org/lamsfoundation/lams/util/ValidationUtil.java
@@ -31,77 +31,60 @@ import java.util.regex.Pattern;
  */
 public class ValidationUtil {
 
-    private final static String REGEX_USER_NAME = "^[^<>^!#&()/\\|'\"?,.:{}= ~`*@%$]*$";
-    
-    private final static String REGEX_FIRST_LAST_NAME = "^[\\p{L}]++(?:[' -][\\p{L}]++)*+\\.?$";
-    
-    private final static String REGEX_EMAIL = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-    
-    private final static String REGEX_ORGANISATION_NAME = "^[^<>^*@%$]*$";
-    
+    private final static Pattern REGEX_USER_NAME = Pattern.compile("^[^<>^!#&()/\\|'\"?,.:{}= ~`*@%$]*$");
+
+    private final static Pattern REGEX_FIRST_LAST_NAME = Pattern.compile("^[\\p{L}]++(?:[' -][\\p{L}]++)*+\\.?$");
+
+    private final static Pattern REGEX_EMAIL = Pattern
+	    .compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+
+    private final static Pattern REGEX_ORGANISATION_NAME = Pattern.compile("^[^<>^*@%$]*$");
+
     /**
      * Checks whether supplied username is valid. Username can only contain alphanumeric characters and no spaces.
-     * 
-     * @param Username
-     * @return
      */
     public static boolean isUserNameValid(String userName) {
-	
-	boolean isUserNameValid = ValidationUtil.isRegexMatches(REGEX_USER_NAME,userName);
+	boolean isUserNameValid = ValidationUtil.isRegexMatches(ValidationUtil.REGEX_USER_NAME, userName);
 	return isUserNameValid;
     }
 
     /**
-     * Checks whether supplied name is valid, which should only contain letters and the following characters ''',' ','-' 
+     * Checks whether supplied name is valid, which should only contain letters and the following characters ''',' ','-'
      * 
      * @param name
      * @return
      */
     public static boolean isFirstLastNameValid(String name) {
-	
-	boolean isNameValid = ValidationUtil.isRegexMatches(REGEX_FIRST_LAST_NAME, name);
+
+	boolean isNameValid = ValidationUtil.isRegexMatches(ValidationUtil.REGEX_FIRST_LAST_NAME, name);
 	return isNameValid;
     }
 
     /**
      * Checks whether supplied email address is valid.
-     * 
-     * @param email
-     * @return
      */
     public static boolean isEmailValid(String email) {
-	
-	boolean isEmailValid = ValidationUtil.isRegexMatches(REGEX_EMAIL, email);
+	boolean isEmailValid = ValidationUtil.isRegexMatches(ValidationUtil.REGEX_EMAIL, email);
 	return isEmailValid;
     }
-    
+
     /**
      * Checks whether supplied string is valid. Name cannot contain any of these characters < > ^ * @ % $
-     * 
-     * @param orgName
-     * @return
      */
     public static boolean isOrgNameValid(String orgName) {
-	
-	boolean isOrgNameValid = ValidationUtil.isRegexMatches(REGEX_ORGANISATION_NAME, orgName);
+	boolean isOrgNameValid = ValidationUtil.isRegexMatches(ValidationUtil.REGEX_ORGANISATION_NAME, orgName);
 	return isOrgNameValid;
     }
-    
+
     /**
      * Checks whether regex matches the input string.
-     * 
-     * @param regex
-     * @param input
-     * @return
      */
-    private static boolean isRegexMatches(final String regex, final String input) {
-	
+    private static boolean isRegexMatches(Pattern pattern, String input) {
 	if (input == null) {
 	    return true;
 	}
-	
-	Pattern p = Pattern.compile(regex);
-	Matcher m = p.matcher(input.trim());
+
+	Matcher m = pattern.matcher(input.trim());
 	return m.matches();
     }
 }
