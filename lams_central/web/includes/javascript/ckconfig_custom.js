52b4e75dd7b5a56c0b4842d1716fc20f87aa356a
wkiaora <balandrey@gmail.com>
2014-08-23 01:40:49 +0000

    LDEV-3301 set autoGrow_minHeight to prevent it from increasing its size on accessing it the first time.

--- lams_central/web/includes/javascript/ckconfig_custom.js
+++ lams_central/web/includes/javascript/ckconfig_custom.js
@@ -70,6 +70,7 @@ CKEDITOR.config.enterMode = CKEDITOR.ENTER_DIV;
 CKEDITOR.config.removePlugins = 'elementspath';
 CKEDITOR.config.allowedContent = true;
 CKEDITOR.config.toolbarCanCollapse = true;
+CKEDITOR.config.autoGrow_minHeight = 100;
 
 // ---- Additional scripts -----
 // Hides editor instaces until they are fully initialized
