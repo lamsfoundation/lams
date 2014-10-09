a57c1ddf55a49a64b6b6269f8710d8cfbab7609e
MarcinCieslak <mgr.inz.marcin.cieslak@gmail.com>
2014-10-09 09:55:41 +0000

    LDEV-3319: Synchronize patch execution with an external monitor. Modules in WildFly can be loaded in parallel. AutoPatch is not ready for this as it uses static methods with unsynchronized collections. Each AutoPatchService instance should be run sequentially.

---
+++ lams_common/src/java/org/lamsfoundation/lams/dbupdates/AutoPatchServiceSynchronizer.java
@@ -0,0 +1,30 @@
+package org.lamsfoundation.lams.dbupdates;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import com.tacitknowledge.util.migration.MigrationException;
+import com.tacitknowledge.util.migration.jdbc.AutoPatchService;
+
+/**
+ * Synchronizes patching across modules, which in vanilla version can be run in parallel.
+ * WildFly loads modules simultaneously and AutoPatch uses static methods which cause errors
+ * when they are entered at the same time.
+ * 
+ * @author Marcin Cieslak, Marek Bubala
+ */
+public class AutoPatchServiceSynchronizer extends AutoPatchService {
+
+    protected static final Lock lock = new ReentrantLock();
+
+    @Override
+    public void patch() throws MigrationException {
+	AutoPatchServiceSynchronizer.lock.lock();
+
+	try {
+	    super.patch();
+	} finally {
+	    AutoPatchServiceSynchronizer.lock.unlock();
+	}
+    }
+}
---
+++ lams_common/src/java/org/lamsfoundation/lams/dbupdates/WebAppJNDIMigrationLauncherSynchronizer.java
@@ -0,0 +1,26 @@
+package org.lamsfoundation.lams.dbupdates;
+
+import javax.servlet.ServletContextEvent;
+
+import com.tacitknowledge.util.migration.jdbc.WebAppJNDIMigrationLauncher;
+
+/**
+ * Synchronizes patching across modules, which in vanilla version can be run in parallel.
+ * WildFly loads modules simultaneously and AutoPatch uses static methods which cause errors
+ * when they are entered at the same time.
+ * 
+ * @author Marcin Cieslak, Marek Bubala
+ */
+public class WebAppJNDIMigrationLauncherSynchronizer extends WebAppJNDIMigrationLauncher {
+    
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+	AutoPatchServiceSynchronizer.lock.lock();
+
+	try {
+	    super.contextInitialized(sce);
+	} finally {
+	    AutoPatchServiceSynchronizer.lock.unlock();
+	}
+    }
+}
