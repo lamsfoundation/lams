ea63f6db0d83cdc29f998531cfad896c5e650a71
wkiaora <balandrey@gmail.com>
2014-11-25 23:32:28 +0000

    LDEV-3382 *Add datetime parameter check to LoginRequest. *Introduced getServerTime method available for 3rd party servers

--- lams_central/src/java/org/lamsfoundation/lams/web/LoginRequestServlet.java
+++ lams_central/src/java/org/lamsfoundation/lams/web/LoginRequestServlet.java
@@ -135,17 +135,16 @@ public class LoginRequestServlet extends HttpServlet {
 	    }
 
 	    //in case of request for learner with strict authentication check cache should also contain lsid
+	    String lsId = null;
 	    if (LoginRequestDispatcher.METHOD_LEARNER_STRICT_AUTHENTICATION.equals(method)) {
-		String lsId = request.getParameter(LoginRequestDispatcher.PARAM_LESSON_ID);
+		
+		lsId = request.getParameter(LoginRequestDispatcher.PARAM_LESSON_ID);
 		if (lsId == null) {
 		    response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Login Failed - lsId parameter missing");
 		    return;
 		}
-		Authenticator.authenticate(serverMap, timestamp, extUsername, method, lsId, hash);
-		
-	    } else {
-		Authenticator.authenticate(serverMap, timestamp, extUsername, method, hash);
 	    }
+	    Authenticator.authenticateLoginRequest(serverMap, timestamp, extUsername, method, lsId, hash);
 	    
 	    User user = userMap.getUser();
 	    String login = user.getLogin();
---
+++ lams_central/src/java/org/lamsfoundation/lams/webservice/GetServerTimeServlet.java
@@ -0,0 +1,69 @@
+/**************************************************************** 
+ * Copyright (C) 2005 LAMS Foundation (http://lamsfoundation.org) 
+ * ============================================================= 
+ * License Information: http://lamsfoundation.org/licensing/lams/2.0/ 
+ * 
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License version 2.0 
+ * as published by the Free Software Foundation. 
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 * USA 
+ * 
+ * http://www.gnu.org/licenses/gpl.txt 
+ * **************************************************************** 
+ */
+
+/* $Id$ */
+package org.lamsfoundation.lams.webservice;
+
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author Andrey Balan
+ * 
+ * @web:servlet name="GetServerTimeServlet"
+ * @web:servlet-mapping url-pattern="/services/getServerTime"
+ */
+public class GetServerTimeServlet extends HttpServlet {
+
+    private static Logger logger = Logger.getLogger(GetServerTimeServlet.class);
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+
+	try {
+	    long currentTime = (new Date()).getTime();
+
+	    response.setContentType("text/html;charset=utf-8");
+	    PrintWriter out = response.getWriter();
+
+	    out.println(currentTime);
+	    logger.warn("AAAB " + currentTime);
+
+	    out.flush();
+	    out.close();
+
+	} catch (Exception e) {
+	    logger.error(e.getMessage());
+	}
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+	doPost(request, response);
+    }
+
+}
