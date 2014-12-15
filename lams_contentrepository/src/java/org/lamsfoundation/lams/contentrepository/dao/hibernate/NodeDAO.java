db2e1c5861eba794ac60ef024441bba11f753a1f
MarcinCieslak <mgr.inz.marcin.cieslak@gmail.com>
2014-12-15 12:01:41 +0000

    LDEV-3336: Remove code for unnecessary ContentRepository WAR.

--- lams_contentrepository/src/java/org/lamsfoundation/lams/contentrepository/dao/hibernate/NodeDAO.java
+++ lams_contentrepository/src/java/org/lamsfoundation/lams/contentrepository/dao/hibernate/NodeDAO.java
@@ -21,7 +21,7 @@
  * ****************************************************************
  */
 
-/* $$Id$$ */	
+/* $$Id$$ */
 package org.lamsfoundation.lams.contentrepository.dao.hibernate;
 
 import java.util.List;
@@ -33,78 +33,79 @@ import org.lamsfoundation.lams.contentrepository.dao.INodeDAO;
 import org.lamsfoundation.lams.dao.hibernate.LAMSBaseDAO;
 import org.springframework.stereotype.Repository;
 
-
-
 /**
  * Data access routines for Nodes
  * 
  * @author Fiona Malikoff
  */
 @Repository
-public class NodeDAO extends LAMSBaseDAO implements INodeDAO  {
+public class NodeDAO extends LAMSBaseDAO implements INodeDAO {
+    protected Logger log = Logger.getLogger(NodeDAO.class);
 
-	protected Logger log = Logger.getLogger(NodeDAO.class);	
+    @Override
+    public void saveOrUpdate(Object object) {
+	insertOrUpdate(object);
+    }
 
-	public void saveOrUpdate(Object object) {
-		this.saveOrUpdate(object);		
-	}
-	
-	/** Get all child nodes for a node/version.
-	 * 
-	 * The child/parent relationship set is lazy loaded to make things
-	 * more efficient. But as the repository doesn't keep the session
-	 * open, the lazy loading can't occur! So load manually.
-	 * 
-	 * @param parentNodeVersion
-	 * @return Set of CrNodes that are child nodes of this node/version 
-	 */
-	public List findChildNodes(CrNodeVersion parentNodeVersion) {
+    /**
+     * Get all child nodes for a node/version.
+     * 
+     * The child/parent relationship set is lazy loaded to make things more efficient. But as the repository doesn't
+     * keep the session open, the lazy loading can't occur! So load manually.
+     * 
+     * @param parentNodeVersion
+     * @return Set of CrNodes that are child nodes of this node/version
+     */
+    @Override
+    public List findChildNodes(CrNodeVersion parentNodeVersion) {
 
-		if ( log.isDebugEnabled() )
-			log.debug("Getting all child nodes for "+parentNodeVersion);
-		
-		String queryString = "from CrNode as n where n.parentNodeVersion = ?";
-		List nodes = doFind(queryString,parentNodeVersion);
-		
-		if(nodes.size() == 0){
-			log.debug("No nodes found");
-			return null;
-		}else{
-			if ( log.isDebugEnabled() )
-				log.debug("Returning "+nodes.size()+" nodes.");
-			return nodes;
-		}
+	if (log.isDebugEnabled()) {
+	    log.debug("Getting all child nodes for " + parentNodeVersion);
 	}
 
+	String queryString = "from CrNode as n where n.parentNodeVersion = ?";
+	List nodes = doFind(queryString, parentNodeVersion);
 
-	/** Get all child nodes for a node/version.
-	 * 
-	 * The child/parent relationship set is lazy loaded to make things
-	 * more efficient. But as the repository doesn't keep the session
-	 * open, the lazy loading can't occur! So load manually.
-	 * 
-	 * @param parentNodeVersion
-	 * @param relPath relative path of the file, based on the main page 
-	 * from the parent node.
-	 * @return Set of CrNodes that are child nodes of this node/version 
-	 */
-	public CrNode findChildNode(CrNodeVersion parentNodeVersion, String relPath) {
+	if (nodes.size() == 0) {
+	    log.debug("No nodes found");
+	    return null;
+	} else {
+	    if (log.isDebugEnabled()) {
+		log.debug("Returning " + nodes.size() + " nodes.");
+	    }
+	    return nodes;
+	}
+    }
 
-		if ( log.isDebugEnabled() )
-			log.debug("Getting child node from "+parentNodeVersion+" path "+relPath);
-		
-		String queryString = "from CrNode as n where n.parentNodeVersion = ? and n.path = ? ";
-		List nodes = doFind(queryString,new Object[] {parentNodeVersion, relPath});
-		
-		if(nodes.size() == 0){
-			log.debug("No nodes found");
-			return null;
-		}else{
-			if ( nodes.size() == 0 ) {
-				log.error(nodes.size()+" matches found for "+parentNodeVersion+" path "+relPath);
-			}
-			return (CrNode) nodes.get(0);
-		}
+    /**
+     * Get all child nodes for a node/version.
+     * 
+     * The child/parent relationship set is lazy loaded to make things more efficient. But as the repository doesn't
+     * keep the session open, the lazy loading can't occur! So load manually.
+     * 
+     * @param parentNodeVersion
+     * @param relPath
+     *            relative path of the file, based on the main page from the parent node.
+     * @return Set of CrNodes that are child nodes of this node/version
+     */
+    @Override
+    public CrNode findChildNode(CrNodeVersion parentNodeVersion, String relPath) {
+
+	if (log.isDebugEnabled()) {
+	    log.debug("Getting child node from " + parentNodeVersion + " path " + relPath);
 	}
 
+	String queryString = "from CrNode as n where n.parentNodeVersion = ? and n.path = ? ";
+	List nodes = doFind(queryString, new Object[] { parentNodeVersion, relPath });
+
+	if (nodes.size() == 0) {
+	    log.debug("No nodes found");
+	    return null;
+	} else {
+	    if (nodes.size() == 0) {
+		log.error(nodes.size() + " matches found for " + parentNodeVersion + " path " + relPath);
+	    }
+	    return (CrNode) nodes.get(0);
+	}
+    }
 }
