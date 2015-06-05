c0a09fde582bb6455959c84cac5928f93994cea0
MarcinCieslak <mgr.inz.marcin.cieslak@gmail.com>
2015-06-05 11:34:21 +0000

    LDEV-3483: Add iframe plugin to CKEditor in Authoring.

--- lams_central/web/includes/javascript/ckconfig_custom.js
+++ lams_central/web/includes/javascript/ckconfig_custom.js
@@ -1,6 +1,6 @@
 CKEDITOR.config.toolbar_Default = [
 	['Source','-','Maximize', 'Preview','PasteFromWord','Undo','Redo','Bold','Italic','Underline', '-','Subscript','Superscript','NumberedList','BulletedList','-','Outdent','Indent','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','TextColor','BGColor','equation','-','Skype'], 
-	['Paint_Button','MoviePlayer','Kaltura','Image','Link','Table','HorizontalRule','Smiley','SpecialChar','Templates','Format','Font','FontSize','About']
+	['Paint_Button','MoviePlayer','Kaltura','Image','Link','Iframe','Table','HorizontalRule','Smiley','SpecialChar','Templates','Format','Font','FontSize','About']
 ] ;
 // removing Video Recorder from default tool bar LDEV-2961
 // To include it back, just add 'VideoRecorder' in between the MoviePlayer and Kaltura
@@ -44,7 +44,7 @@ CKEDITOR.config.toolbar_CustomWiki = [
 
 CKEDITOR.config.toolbar_CustomPedplanner = [
 	['Source','-','Maximize','Preview','PasteFromWord','Bold','Italic','Underline', '-','NumberedList','BulletedList','-','Outdent','Indent','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','TextColor','BGColor','equation'], 
-	['Image','Link','Table','Smiley','Font','FontSize']
+	['Image','Link','Iframe','Table','Smiley','Font','FontSize']
 ] ;
 
 CKEDITOR.config.toolbar_LessonDescription = [
@@ -57,15 +57,15 @@ CKEDITOR.config.toolbar_LessonDescription = [
 ] ;
 
 
-CKEDITOR.config.contentsCss = CKEDITOR.basePath + '../css/defaultHTML_learner.css' ;
+CKEDITOR.config.contentsCss = CKEDITOR.basePath + '../css/defaultHTML_learner.css';
 //CKEDITOR.config.skin = 'office2013' ;
 CKEDITOR.config.disableNativeSpellChecker = false;
 CKEDITOR.config.browserContextMenuOnCtrl = true;
 CKEDITOR.config.templates = CKEDITOR.basePath + '../www/htmltemplates.xml';
 CKEDITOR.config.format_tags	= 'div;h1;h2;h3;h4;h5;h6;pre;address;p' ;
-CKEDITOR.config.enterMode = 'div' ;
-CKEDITOR.plugins.addExternal('wikilink', CKEDITOR.basePath + '../tool/lawiki10/wikilink/', 'plugin.js') ;
-CKEDITOR.config.extraPlugins = 'kaltura,wikilink,equation,paint,movieplayer,skype' ;
+CKEDITOR.config.enterMode = 'div';
+CKEDITOR.plugins.addExternal('wikilink', CKEDITOR.basePath + '../tool/lawiki10/wikilink/', 'plugin.js');
+CKEDITOR.config.extraPlugins = 'kaltura,wikilink,equation,paint,movieplayer,skype,iframe';
 CKEDITOR.config.enterMode = CKEDITOR.ENTER_DIV; 
 CKEDITOR.config.removePlugins = 'elementspath';
 CKEDITOR.config.allowedContent = true;
