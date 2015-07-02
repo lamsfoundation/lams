a3b7e180b74ccf08ee8882759518c902db4b63da
erniegx <ernieg@gmail.com>
2015-07-02 00:14:31 +0000

    LDEV-3493: Remove skype plugin as Skype no longer supports this

--- lams_central/web/ckeditor/plugins/skype/lang/en.js
+++
@@ -1,9 +0,0 @@
-CKEDITOR.plugins.setLang( 'skype', 'en', {
-	// Toolbar button
-	SkypeBtn : 'Insert Skype button',
-	SkypeTitle: 'Skype Properties',
-	SkypeTxt: 'The Skype Plugin allows you to embed a button that will allow your students to chat with you using Skype. Just insert your Skype contact name.',
-	SkypeHelp: 'How to use this?',
-	SkypeContactName: 'Skype contact name:',
-	SkypeValidationError: 'You must enter an Skype contact name or Cancel to abort.'
-});
