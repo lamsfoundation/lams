dc74b8a18a404fbc79dc6e02445e18a7c0cab6b9
MarcinCieslak <mgr.inz.marcin.cieslak@gmail.com>
2015-07-22 08:00:28 +0000

    LDEV-3514: Wrap all transactional methods with TransactionRetryInterceptor. Set number of attempts to 3. Remove previous safety mechanism for completeActivity() method.

---
+++ lams_common/src/java/org/lamsfoundation/lams/web/filter/TransactionRetryInterceptor.java
@@ -0,0 +1,66 @@
+/****************************************************************
+ * Copyright (C) 2005 LAMS Foundation (http://lamsfoundation.org)
+ * =============================================================
+ * License Information: http://lamsfoundation.org/licensing/lams/2.0/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2.0
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 * USA
+ *
+ * http://www.gnu.org/licenses/gpl.txt
+ * ****************************************************************
+ */
+
+/* $Id$ */
+package org.lamsfoundation.lams.web.filter;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.apache.log4j.Logger;
+import org.springframework.dao.DataIntegrityViolationException;
+
+/**
+ * Retries proxied method in case of an exception.
+ * Is the first interceptor in the processing chain.
+ * @author Marcin Cieslak
+ *
+ */
+public class TransactionRetryInterceptor implements MethodInterceptor {
+    
+    private static final Logger log = Logger.getLogger(TransactionRetryInterceptor.class);
+
+    private static final int MAX_ATTEMPTS = 3;
+
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+	int attempt = 1;
+	Throwable exception = null;
+	do {
+	    try {
+		return invocation.proceed();
+	    } catch (DataIntegrityViolationException e) {
+		exception = e;
+		StringBuilder message = new StringBuilder("When invoking method  ")
+			.append(invocation.getMethod().getName()).append(" caught ").append(e.getMessage())
+			.append(". Attempt #").append(attempt);
+		attempt++;
+		if (attempt <= TransactionRetryInterceptor.MAX_ATTEMPTS) {
+		    message.append(". Retrying.");
+		} else {
+		    message.append(". Giving up.");
+		}
+		TransactionRetryInterceptor.log.warn(message);
+	    }
+	} while (attempt <= TransactionRetryInterceptor.MAX_ATTEMPTS);
+	throw exception;
+    }
+}
