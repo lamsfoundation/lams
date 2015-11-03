9e6ea591342a452fcb30062bc34155e9e6f57e4d
erniegx <ernieg@gmail.com>
2015-11-03 14:43:41 +0000

    LDEV-3607: Remove old movieplayer

--- lams_central/web/ckeditor/plugins/movieplayer/lang/en.js
+++
@@ -1,3 +0,0 @@
-CKEDITOR.plugins.setLang( 'movieplayer', 'en', {	// Toolbar button	MoviePlayerBtn : 'Media Player',	MoviePlayerTooltip : 'Insert/Edit Movies or Audio',					
-	// Dialog	DlgMoviePlayerTitle : 'Media Player Properties',	MoviePlayerURL : 'URL:',	MoviePlayerURLDesc : 'Select a .avi, .flv, .mov, .mp3, .mp4, .mpv, .wma, .wmv file or youtube URL',	MoviePlayerWidth : 'Width:',	MoviePlayerHeight : 'Height:',	MoviePlayerAutoplay : 'Auto Play:',	MoviePlayerBrowseServer : 'Browse Server',					
-	// Dialog errors	MoviePlayerNoUrl : 'Please specify a movie URL.'});
--- lams_central/web/ckeditor/plugins/movieplayer/plugin.js
+++
@@ -1,13 +0,0 @@
-/* 
- *   CKEditior plugin.js for Movie Player
- *  ------------
- */CKEDITOR.plugins.add('movieplayer', {	requires : ['iframedialog'],	lang : [ 'en' ],	init : function(editor) {	             var pluginPath = CKEDITOR.plugins.getPath('movieplayer');	             	             editor.ui.addButton(	                'MoviePlayer',	                {
-	                	label   : editor.lang.movieplayer.MoviePlayerBtn,		                command : 'MoviePlayer',		                icon    : pluginPath + 'filmreel.gif',		                title   : editor.lang.movieplayer.MoviePlayerTooltip	                }	             );	             editor.addCommand(
-	            	'MoviePlayer', 
-	                 {
-	                 	exec : function(editor){
-	                 		editor.openDialog('MoviePlayer');
-	                  	}
-	                 }
-	             );
-	             	             CKEDITOR.dialog.addIframe(	            	'MoviePlayer',	            	editor.lang.movieplayer.DlgMoviePlayerTitle,	                pluginPath + 'movieplayer.html',	                450,	                260	             );	}});
