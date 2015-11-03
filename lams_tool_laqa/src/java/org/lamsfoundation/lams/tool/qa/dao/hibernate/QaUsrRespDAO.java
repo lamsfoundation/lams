f4236ff458eed45b099e05fdea21a389d9f7fbe2
FionaMalikoff <ozstitch@gmail.com>
2015-11-03 02:49:59 +0000

    LDEV-3581: Modify Q&A monitor UI to support search LDEV-3605: Modify QA to support sorting by rating.

--- lams_tool_laqa/src/java/org/lamsfoundation/lams/tool/qa/dao/hibernate/QaUsrRespDAO.java
+++ lams_tool_laqa/src/java/org/lamsfoundation/lams/tool/qa/dao/hibernate/QaUsrRespDAO.java
@@ -25,6 +25,7 @@ package org.lamsfoundation.lams.tool.qa.dao.hibernate;
 
 import java.util.List;
 
+import org.hibernate.Query;
 import org.lamsfoundation.lams.dao.hibernate.LAMSBaseDAO;
 import org.lamsfoundation.lams.tool.qa.QaAppConstants;
 import org.lamsfoundation.lams.tool.qa.QaUsrResp;
@@ -42,18 +43,38 @@ public class QaUsrRespDAO extends LAMSBaseDAO implements IQaUsrRespDAO {
     private static final String LOAD_ATTEMPT_FOR_SESSION_AND_QUESTION = "from qaUsrResp in class QaUsrResp "
 	    + " where qaUsrResp.qaQueUser.qaSession.qaSessionId=:qaSessionId and qaUsrResp.qaQuestion.uid=:questionId";
 
-    private static final String LOAD_ATTEMPT_FOR_SESSION_AND_QUESTION_LIMIT = "from qaUsrResp in class QaUsrResp "
-	    + "where qaUsrResp.qaQueUser.qaSession.qaSessionId=:qaSessionId AND qaUsrResp.qaQuestion.uid=:questionId AND qaUsrResp.qaQueUser.queUsrId!=:excludeUserId order by ";
-    
+    private static final String LOAD_ATTEMPT_FOR_SESSION_AND_QUESTION_LIMIT_WITH_NAME_SEARCH = "from qaUsrResp in class QaUsrResp "
+	    + " where qaUsrResp.qaQueUser.qaSession.qaSessionId=:qaSessionId AND qaUsrResp.qaQuestion.uid=:questionId AND qaUsrResp.qaQueUser.queUsrId!=:excludeUserId "
+	    + " AND qaUsrResp.qaQueUser.fullname LIKE CONCAT('%', :searchString, '%') "
+	    + " order by ";
+
+    private static final String SQL_LOAD_ATTEMPT_FOR_SESSION_AND_QUESTION_LIMIT_WITH_NAME_SEARCH_AVG_RATING = "SELECT resp.*, AVG(rating.rating) avg_rating"
+	    + " FROM tl_laqa11_usr_resp resp"
+	    + " JOIN tl_laqa11_que_usr usr"
+	    + " ON resp.qa_que_content_id = :questionId AND resp.que_usr_id = usr.uid "
+	    + " AND usr.que_usr_id!=:excludeUserId "
+	    + " JOIN tl_laqa11_session sess "
+	    + " ON usr.qa_session_id = sess.uid AND sess.qa_session_id = :qaSessionId "
+	    + " AND usr.fullname LIKE CONCAT('%', :searchString, '%')"
+	    + " LEFT JOIN ("
+	    + " 	SELECT rat.item_id, rat.rating FROM lams_rating rat"
+	    + "         JOIN lams_rating_criteria crit"
+	    + " 	ON rat.rating_criteria_id = crit.rating_criteria_id AND crit.tool_content_id = :toolContentId"
+	    + " 	) rating"
+	    + " ON rating.item_id = resp.response_id"
+	    + " GROUP BY response_id"
+	    + " ORDER BY ";
+	    
     private static final String LOAD_ATTEMPT_FOR_USER = "from qaUsrResp in class QaUsrResp "
 	    + "where qaUsrResp.qaQueUser.uid=:userUid order by qaUsrResp.qaQuestion.displayOrder asc";
 
     private static final String GET_COUNT_RESPONSES_BY_QACONTENT = "SELECT COUNT(*) from " + QaUsrResp.class.getName()
 	    + " as r where r.qaQuestion.qaContent.qaContentId=?";
 
-    private static final String GET_COUNT_RESPONSES_FOR_SESSION_AND_QUESTION = "SELECT COUNT(*) from "
+    private static final String GET_COUNT_RESPONSES_FOR_SESSION_AND_QUESTION_WITH_NAME_SEARCH = "SELECT COUNT(*) from "
 	    + QaUsrResp.class.getName()
-	    + " as r where r.qaQueUser.qaSession.qaSessionId=? and r.qaQuestion.uid=? AND r.qaQueUser.queUsrId!=?";
+	    + " as r where r.qaQueUser.qaSession.qaSessionId=? and r.qaQuestion.uid=? AND r.qaQueUser.queUsrId!=?"
+	    + " and r.qaQueUser.fullname LIKE CONCAT('%', ?, '%') ";
 
     public void createUserResponse(QaUsrResp qaUsrResp) {
     	getSession().save(qaUsrResp);
@@ -91,10 +112,12 @@ public class QaUsrRespDAO extends LAMSBaseDAO implements IQaUsrRespDAO {
 		.setLong("qaSessionId", qaSessionId.longValue()).setLong("questionId", questionId.longValue()).list();
     }
 
+    @SuppressWarnings("unchecked")
     @Override
-    public List<QaUsrResp> getResponsesForTablesorter(final Long qaSessionId, final Long questionId, final Long excludeUserId,
-	    int page, int size, int sorting) {
-	String sortingOrder = "";
+    public List<QaUsrResp> getResponsesForTablesorter(final Long toolContentId, final Long qaSessionId, final Long questionId, final Long excludeUserId,
+	    int page, int size, int sorting, String searchString) {
+	String sortingOrder;
+	boolean useAverageRatingSort = false;
 	switch (sorting) {
 	case QaAppConstants.SORT_BY_NO:
 	    sortingOrder = "qaUsrResp.attemptTime";
@@ -105,13 +128,46 @@ public class QaUsrRespDAO extends LAMSBaseDAO implements IQaUsrRespDAO {
 	case QaAppConstants.SORT_BY_ANSWER_DESC:
 	    sortingOrder = "answer DESC";
 	    break;
+	case QaAppConstants.SORT_BY_USERNAME_ASC:
+	    sortingOrder = "qaUsrResp.qaQueUser.fullname ASC";
+	    break;
+	case QaAppConstants.SORT_BY_USERNAME_DESC:
+	    sortingOrder = "qaUsrResp.qaQueUser.fullname DESC";
+	    break;
+	case QaAppConstants.SORT_BY_RATING_ASC:
+	    sortingOrder = " avg_rating ASC";
+	    useAverageRatingSort = true;
+	    break;
+	case QaAppConstants.SORT_BY_RATING_DESC:
+	    sortingOrder = " avg_rating DESC";
+	    useAverageRatingSort = true;
+	    break;
+	default: 
+	    sortingOrder = " resp.attempt_time"; // default if we get an unexpected sort order    
+	}
+
+	Query query = null;
+	
+	if ( useAverageRatingSort ) {
+	    query = getSessionFactory().getCurrentSession()
+		    .createSQLQuery(SQL_LOAD_ATTEMPT_FOR_SESSION_AND_QUESTION_LIMIT_WITH_NAME_SEARCH_AVG_RATING+sortingOrder).addEntity(QaUsrResp.class)
+		    .setLong("toolContentId", toolContentId.longValue());
+	} else {
+	    query = getSessionFactory().getCurrentSession()
+		    .createQuery(LOAD_ATTEMPT_FOR_SESSION_AND_QUESTION_LIMIT_WITH_NAME_SEARCH+sortingOrder);
 	}
 	
-	return getSessionFactory().getCurrentSession()
-		.createQuery(LOAD_ATTEMPT_FOR_SESSION_AND_QUESTION_LIMIT + sortingOrder)
-		.setLong("qaSessionId", qaSessionId.longValue()).setLong("questionId", questionId.longValue())
-		.setLong("excludeUserId", excludeUserId.longValue()).setFirstResult(page * size).setMaxResults(size)
-		.list();
+	query.setLong("questionId", questionId.longValue());
+	query.setLong("qaSessionId", qaSessionId.longValue());
+	query.setLong("excludeUserId", excludeUserId.longValue());
+
+	// support for custom search from a toolbar
+	searchString = searchString == null ? "" : searchString;
+	query.setString("searchString", searchString);
+
+	query.setFirstResult(page * size);
+	query.setMaxResults(size);
+	return query.list();
     }
 
     @Override
@@ -130,10 +186,11 @@ public class QaUsrRespDAO extends LAMSBaseDAO implements IQaUsrRespDAO {
 	return ((Number) list.get(0)).intValue();
     }
 
-    public int getCountResponsesBySessionAndQuestion(final Long qaSessionId, final Long questionId, final Long excludeUserId) {
+    public int getCountResponsesBySessionAndQuestion(final Long qaSessionId, final Long questionId, final Long excludeUserId, String searchString) {
 
-	List list = doFind(GET_COUNT_RESPONSES_FOR_SESSION_AND_QUESTION,
-		new Object[] { qaSessionId, questionId, excludeUserId });
+	String filter = searchString != null ? searchString.trim() : "";
+	List list = doFind(GET_COUNT_RESPONSES_FOR_SESSION_AND_QUESTION_WITH_NAME_SEARCH,
+		new Object[] { qaSessionId, questionId, excludeUserId, filter });
 	if (list == null || list.size() == 0) {
 	    return 0;
 	}
--- lams_tool_laqa/src/java/org/lamsfoundation/lams/tool/qa/service/QaServicePOJO.java
+++ lams_tool_laqa/src/java/org/lamsfoundation/lams/tool/qa/service/QaServicePOJO.java
@@ -308,15 +308,15 @@ public class QaServicePOJO implements IQaService, ToolContentManager, ToolSessio
     }
 
     @Override
-    public List<QaUsrResp> getResponsesForTablesorter(final Long qaSessionId, final Long questionId,
-	    final Long excludeUserId, int page, int size, int sorting) {
-	return qaUsrRespDAO.getResponsesForTablesorter(qaSessionId, questionId, excludeUserId, page, size, sorting);
+    public List<QaUsrResp> getResponsesForTablesorter(final Long toolContentId, final Long qaSessionId, final Long questionId,
+	    final Long excludeUserId, int page, int size, int sorting, String searchString) {
+	return qaUsrRespDAO.getResponsesForTablesorter(toolContentId, qaSessionId, questionId, excludeUserId, page, size, sorting, searchString);
     }
 
     @Override
     public int getCountResponsesBySessionAndQuestion(final Long qaSessionId, final Long questionId,
-	    final Long excludeUserId) {
-	return qaUsrRespDAO.getCountResponsesBySessionAndQuestion(qaSessionId, questionId, excludeUserId);
+	    final Long excludeUserId, String searchString) {
+	return qaUsrRespDAO.getCountResponsesBySessionAndQuestion(qaSessionId, questionId, excludeUserId, searchString);
     }
 
     @Override
