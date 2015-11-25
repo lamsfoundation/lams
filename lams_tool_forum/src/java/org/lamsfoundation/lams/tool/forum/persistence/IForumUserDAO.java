bec5d3437f63b02dcce242c16cddfe49b88daa8d
FionaMalikoff <Fiona.Malikoff@gmail.com>
2015-11-25 07:50:48 +0000

    LDEV-3586: Forum now sorts on marked and has optimised access to the Notebook entries in Monitoring.

--- lams_tool_forum/src/java/org/lamsfoundation/lams/tool/forum/persistence/IForumUserDAO.java
+++ lams_tool_forum/src/java/org/lamsfoundation/lams/tool/forum/persistence/IForumUserDAO.java
@@ -3,6 +3,7 @@ package org.lamsfoundation.lams.tool.forum.persistence;
 import java.util.List;
 
 import org.lamsfoundation.lams.dao.IBaseDAO;
+import org.lamsfoundation.lams.notebook.service.ICoreNotebookService;
 
 public interface IForumUserDAO extends IBaseDAO {
 
@@ -20,7 +21,8 @@ public interface IForumUserDAO extends IBaseDAO {
 
     int getCountUsersBySession(Long sessionId, String searchString);
 
-    List<ForumUser> getUsersForTablesorter(final Long sessionId, int page, int size, int sorting, String searchString);
+    List<Object[]> getUsersForTablesorter(final Long sessionId, int page, int size, int sorting, String searchString, 
+	    boolean getNotebookEntries, ICoreNotebookService coreNotebookService);
 
 
 }
