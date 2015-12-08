3de8af949921782c80d7077a5295b7e03fabe223
wkiaora <balandrey@gmail.com>
2015-12-08 19:44:57 +0000

    LDEV-3626 *Add a third tab to Assessment xls export * remove the first column with counter from a table on summary page

--- lams_tool_assessment/src/java/org/lamsfoundation/lams/tool/assessment/dao/hibernate/AssessmentUserDAOHibernate.java
+++ lams_tool_assessment/src/java/org/lamsfoundation/lams/tool/assessment/dao/hibernate/AssessmentUserDAOHibernate.java
@@ -100,7 +100,7 @@ public class AssessmentUserDAOHibernate extends LAMSBaseDAO implements Assessmen
 		Long userId = ((Number) element[0]).longValue();
 		String firstName = (String) element[1];
 		String lastName = (String) element[2];
-		float grade = ((Number) element[3]).floatValue();
+		float grade = element[3] == null ? 0 : ((Number) element[3]).floatValue();
 
 		AssessmentUserDTO userDto = new AssessmentUserDTO();
 		userDto.setUserId(userId);
@@ -180,7 +180,7 @@ public class AssessmentUserDAOHibernate extends LAMSBaseDAO implements Assessmen
 		Long questionResultUid = ((Number) element[0]).longValue();
 		String firstName = (String) element[1];
 		String lastName = (String) element[2];
-		float grade = ((Number) element[3]).floatValue();
+		float grade = element[3] == null ? 0 : ((Number) element[3]).floatValue();
 
 		AssessmentUserDTO userDto = new AssessmentUserDTO();
 		userDto.setQuestionResultUid(questionResultUid);;
