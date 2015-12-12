662e144da0df02dfacd0f157c29bc12012cab224
MarcinCieslak <mgr.inz.marcin.cieslak@gmail.com>
2015-12-12 18:28:52 +0000

    LDEV-3632: Add buttons to the math formula plugin. Silence Java exceptions when an user makes a mistake in a formula.

--- lams_central/src/java/org/lamsfoundation/lams/web/JlatexmathServlet.java
+++ lams_central/src/java/org/lamsfoundation/lams/web/JlatexmathServlet.java
@@ -18,9 +18,9 @@
  * 
  * http://www.gnu.org/licenses/gpl.txt 
  * **************************************************************** 
- */  
- 
-/* $Id$ */  
+ */
+
+/* $Id$ */
 package org.lamsfoundation.lams.web;
 
 import java.awt.AlphaComposite;
@@ -37,7 +37,9 @@ import javax.servlet.http.HttpServletResponse;
 import javax.swing.JLabel;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 import org.lamsfoundation.lams.util.WebUtil;
+import org.scilab.forge.jlatexmath.ParseException;
 import org.scilab.forge.jlatexmath.TeXConstants;
 import org.scilab.forge.jlatexmath.TeXFormula;
 import org.scilab.forge.jlatexmath.TeXIcon;
@@ -49,37 +51,56 @@ import org.scilab.forge.jlatexmath.TeXIcon;
  * @web:servlet-mapping url-pattern="/servlet/jlatexmath"
  */
 public class JlatexmathServlet extends HttpServlet {
-    
+
+    private static Logger log = Logger.getLogger(JlatexmathServlet.class);
+
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+
 	String formulaParam = request.getParameter("formula");
 	if (StringUtils.isBlank(formulaParam)) {
 	    return;
 	}
-	
+
 	Integer fontSize = WebUtil.readIntParam(request, "fontSize", true);
 	if (fontSize == null) {
 	    fontSize = 20;
 	}
-	
-	TeXFormula formula = new TeXFormula(formulaParam);
-	TeXIcon icon = formula.new TeXIconBuilder().setStyle(TeXConstants.STYLE_DISPLAY)
-		.setSize(fontSize)
-		.setWidth(TeXConstants.UNIT_PIXEL, 256f, TeXConstants.ALIGN_CENTER)
-		.setIsMaxWidth(true).setInterLineSpacing(TeXConstants.UNIT_PIXEL, 20f)
-		.build();
-	
-//	TeXFormula fomule = new TeXFormula(formula);
-//	TeXIcon ti = fomule.createTeXIcon(TeXConstants.STYLE_DISPLAY, 40);
-	BufferedImage b = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
-//	Color transparent = new Color(0, true);
-//	((Graphics2D)b.getGraphics()).setBackground(transparent);
-//	b.getGraphics().clearRect(0, 0, icon.getIconWidth(), icon.getIconHeight());
-	
-	((Graphics2D)b.getGraphics()).setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.5F));
+
+	TeXFormula formula = null;
+	try {
+	    formula = new TeXFormula(formulaParam);
+	} catch (ParseException e) {
+	    // don't throw a full-blown exception whenever an user makes a mistake in the formula
+	    if (JlatexmathServlet.log.isTraceEnabled()) {
+		JlatexmathServlet.log.trace(e.getMessage());
+	    }
+	    return;
+	}
+
+	TeXIcon icon = formula.new TeXIconBuilder().setStyle(TeXConstants.STYLE_DISPLAY).setSize(fontSize)
+		.setWidth(TeXConstants.UNIT_PIXEL, 256f, TeXConstants.ALIGN_CENTER).setIsMaxWidth(true)
+		.setInterLineSpacing(TeXConstants.UNIT_PIXEL, 20f).build();
+
+	BufferedImage b = null;
+	// TeXFormula fomule = new TeXFormula(formula);
+	// TeXIcon ti = fomule.createTeXIcon(TeXConstants.STYLE_DISPLAY, 40);
+	try {
+	    b = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
+	} catch (IllegalArgumentException e) {
+	    // don't throw a full-blown exception whenever an user makes a mistake in the formula
+	    if (JlatexmathServlet.log.isTraceEnabled()) {
+		JlatexmathServlet.log.trace(e.getMessage());
+	    }
+	    return;
+	}
+	// Color transparent = new Color(0, true);
+	// ((Graphics2D)b.getGraphics()).setBackground(transparent);
+	// b.getGraphics().clearRect(0, 0, icon.getIconWidth(), icon.getIconHeight());
+
+	((Graphics2D) b.getGraphics()).setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.5F));
 	icon.paintIcon(new JLabel(), b.getGraphics(), 0, 0);
-	
+
 	response.setContentType("image/png");
 	OutputStream out = response.getOutputStream();
 	ImageIO.write(b, "png", out);
@@ -87,8 +108,8 @@ public class JlatexmathServlet extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
-	    IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
 	doGet(request, response);
     }
 }
