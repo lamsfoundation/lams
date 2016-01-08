0bbb9a9a382d45675727c2ac9c374d56774cfe34
FionaMalikoff <ozstitch@gmail.com>
2016-01-08 07:56:44 +0000

    LDEV-3631: Sort by likes implemented, used as a top 10 on the reflection page.

--- lams_central/src/java/org/lamsfoundation/lams/comments/CommentConstants.java
+++ lams_central/src/java/org/lamsfoundation/lams/comments/CommentConstants.java
@@ -47,7 +47,7 @@ public class CommentConstants {
     // for paging long topics & inlining reply
     public static final String PAGE_LAST_ID = "pageLastId";
     public static final String PAGE_SIZE = "pageSize";
-    public static final int DEFAULT_PAGE_SIZE = 5;
+    public static final int DEFAULT_PAGE_SIZE = 20;
     public static final String ATTR_NO_MORE_PAGES = "noMorePages";
 
     public static final int MAX_BODY_LENGTH = 5000;
