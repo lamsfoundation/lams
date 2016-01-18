af15ddf5aabd17c535dfdc520325a38d3c4c5f50
FionaMalikoff <ozstitch@gmail.com>
2016-01-18 02:19:08 +0000

    LDEV-3631: Converted to div

--- lams_central/web/comments/topicviewwrapper.jsp
+++ lams_central/web/comments/topicviewwrapper.jsp
@@ -12,7 +12,7 @@
 </c:set>
 <c:set var="ctxPath" value="${pageContext.request.contextPath}" scope="request" />
 <c:set var="sessionMap" value="${sessionScope[sessionMapID]}" />
-<script type="text/javascript" src="../includes/javascript/jquery.treetable.js"></script>
+<script type="text/javascript" src="${lams}includes/javascript/jquery.treetable.js"></script>
 
 <%@ include file="topicview.jsp"%>
 
--- lams_central/web/includes/javascript/comments.js
+++ lams_central/web/includes/javascript/comments.js
@@ -1,7 +1,3 @@
-	function resizeIframe() {
-		parent.resizeCommentFrame(document.body.scrollHeight); 
-	}						
-
 	function highlightMessage() {
 		$('.highlight').filter($('table')).css('background','none');
 		$('.highlight').filter($('div')).effect('highlight', {color: "#fcf0ad"}, 6000);
@@ -18,16 +14,17 @@
 		}
 	}
 
-	function reloadThread(response) {
+	
+	function reloadThread(response, lamsUrl, savedCannotRedisplayMessage, errorCannotRedisplayMessage) {
 	    var threadDiv = document.getElementById('thread'+response.threadUid);
        	var threadUid = response.threadUid;
        	var commentUid = response.commentUid;
 					
        	if ( commentUid ) {
         	if ( ! threadDiv) {
-        		alert('<fmt:message key="error.cannot.redisplay.please.refresh"/>');
+        		alert(savedCannotRedisplayMessage);
        		} else {
-    			var loadString = "viewTopicThread.do?&sessionMapID=" + response.sessionMapID + "&threadUid=" + threadUid+"&commentUid="+commentUid;
+    			var loadString = lamsUrl+"comments/viewTopicThread.do?&sessionMapID=" + response.sessionMapID + "&threadUid=" + threadUid+"&commentUid="+commentUid;
 				$(threadDiv).load(loadString, function() {
 					// expand up to the reply - in case it is buried down in a lot of replies
 					// don't need to do this if we have started a new thread.
@@ -36,13 +33,12 @@
 						$('#msg'+commentUid).focus();
 					}
 					highlightMessage();
-					resizeIframe();
 				});
 			}
     	} else if ( response.errMessage ) {
 	    	// No valid id? Something failed. Assume it is a response message coming back.
 			alert(response.errMessage);
 		} else {
-    		alert('<fmt:message key="error.please.refresh"/>');
+    		alert(errorCannotRedisplayMessage);
     	} 
 	}
