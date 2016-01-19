dd47f4013a5fb7cc3fc1342206e7722a9badebc7
MarcinCieslak <mgr.inz.marcin.cieslak@gmail.com>
2016-01-19 09:47:15 +0000

    LDEV-3292: Use JOIN instead of cartesian product in several named queries. Clean up and formatting.

--- lams_common/src/java/org/lamsfoundation/lams/learningdesign/ChosenGrouper.java
+++ lams_common/src/java/org/lamsfoundation/lams/learningdesign/ChosenGrouper.java
@@ -33,128 +33,137 @@ import org.apache.log4j.Logger;
 import org.lamsfoundation.lams.learningdesign.exception.GroupingException;
 import org.lamsfoundation.lams.usermanagement.User;
 
-
 /**
- * The chosen grouper algorithm implementation. It creates a new group for
- * the learners that the teacher requested.
+ * The chosen grouper algorithm implementation. It creates a new group for the learners that the teacher requested.
  * 
  * @author Jacky Fang
- * @since  2005-3-24
+ * @since 2005-3-24
  * @version 1.1
  * 
  */
-public class ChosenGrouper extends Grouper implements Serializable
-{
+public class ChosenGrouper extends Grouper implements Serializable {
 
-	private static final long serialVersionUID = -8498560084860150033L;
-	private static Logger log = Logger.getLogger(ChosenGrouper.class);
+    private static final long serialVersionUID = -8498560084860150033L;
+    private static Logger log = Logger.getLogger(ChosenGrouper.class);
 
-	/**
-     * @see org.lamsfoundation.lams.learningdesign.Grouper#doGrouping(org.lamsfoundation.lams.learningdesign.Grouping,java.lang.String, org.lamsfoundation.lams.usermanagement.User)
-     */
-    public void doGrouping(Grouping chosenGrouping, String groupName, User learner)
-    {
-        //convert the single user into a list.
-        List learners = new ArrayList();
-        learners.add(learner);
-        //delegate to do grouping for a list of learners.
-        doGrouping(chosenGrouping,groupName,learners);
-    }
-    
     /**
-     * @see org.lamsfoundation.lams.learningdesign.Grouper#doGrouping(org.lamsfoundation.lams.learningdesign.Grouping,java.lang.String, java.util.List)
+     * @see org.lamsfoundation.lams.learningdesign.Grouper#doGrouping(org.lamsfoundation.lams.learningdesign.Grouping,java.lang.String,
+     *      org.lamsfoundation.lams.usermanagement.User)
      */
-    public void doGrouping(Grouping chosenGrouping,String groupName, List learners)
-    {
-    	Group selectedGroup = null;
-
-    	String trimmedName = groupName != null ? groupName.trim() : null;
-    	if ( trimmedName == null || trimmedName.length() == 0 ) {
-    		trimmedName = generateGroupName(chosenGrouping);
-    	} else {
-	    	Iterator iter = chosenGrouping.getGroups().iterator();
-	    	while (iter.hasNext() && selectedGroup==null) {
-				Group group = (Group) iter.next();
-				if ( trimmedName.equals(group.getGroupName()) ) {
-					selectedGroup = group;
-				}
-			}
-    	}
-    	doGrouping(chosenGrouping, selectedGroup, trimmedName, learners);
+    @Override
+    public void doGrouping(Grouping chosenGrouping, String groupName, User learner) {
+	// convert the single user into a list.
+	List learners = new ArrayList();
+	learners.add(learner);
+	// delegate to do grouping for a list of learners.
+	doGrouping(chosenGrouping, groupName, learners);
     }
 
-	/**
-	 * @param chosenGrouping
-	 * @return
-	 */
-	private String generateGroupName(Grouping chosenGrouping) {
-		String trimmedName;
-		String prefix = getPrefix();
-		trimmedName = prefix+" "+System.currentTimeMillis();
-		log.info("Chosen grouper for grouping "+chosenGrouping.toString()+" did not get a group name. Selecting default name of "+trimmedName);
-		return trimmedName;
+    /**
+     * @see org.lamsfoundation.lams.learningdesign.Grouper#doGrouping(org.lamsfoundation.lams.learningdesign.Grouping,java.lang.String,
+     *      java.util.List)
+     */
+    @Override
+    public void doGrouping(Grouping chosenGrouping, String groupName, List learners) {
+	Group selectedGroup = null;
+
+	String trimmedName = groupName != null ? groupName.trim() : null;
+	if ((trimmedName == null) || (trimmedName.length() == 0)) {
+	    trimmedName = generateGroupName(chosenGrouping);
+	} else {
+	    Iterator iter = chosenGrouping.getGroups().iterator();
+	    while (iter.hasNext() && (selectedGroup == null)) {
+		Group group = (Group) iter.next();
+		if (trimmedName.equals(group.getGroupName())) {
+		    selectedGroup = group;
+		}
+	    }
 	}
+	doGrouping(chosenGrouping, selectedGroup, trimmedName, learners);
+    }
 
     /**
-     * @throws GroupingException 
-     * @see org.lamsfoundation.lams.learningdesign.Grouper#doGrouping(org.lamsfoundation.lams.learningdesign.Grouping,java.lang.Long, java.util.List)
+     * @param chosenGrouping
+     * @return
      */
-    public void doGrouping(Grouping chosenGrouping,Long groupId, List learners) throws GroupingException
-    {
-    	if ( groupId != null ) {
-	    	Iterator iter = chosenGrouping.getGroups().iterator();
-	    	Group selectedGroup = null;
-	    	while (iter.hasNext() && selectedGroup==null) {
-				Group group = (Group) iter.next();
-				if ( group.getGroupId().equals(groupId) ) {
-					selectedGroup = group;
-				}
-			}
-	    	if ( selectedGroup == null ) {
-	    		String error = "Tried to add users to group "+groupId+" but group cannot be found.";
-	    		log.error(error); 
-	    		throw new GroupingException(error);
-	    	}
-	    	doGrouping(chosenGrouping, selectedGroup, null, learners);
-    	} else {
-    		String groupName = generateGroupName(chosenGrouping);
-    		doGrouping(chosenGrouping, null, groupName, learners);
-    	}
+    private String generateGroupName(Grouping chosenGrouping) {
+	String trimmedName;
+	String prefix = getPrefix();
+	trimmedName = prefix + " " + System.currentTimeMillis();
+	ChosenGrouper.log.info("Chosen grouper for grouping " + chosenGrouping.toString()
+		+ " did not get a group name. Selecting default name of " + trimmedName);
+	return trimmedName;
     }
 
-    /** If the group exists add them to the group, otherwise creates a new group. If group is not supplied, then groupName must
-     * be supplied.
-     * @param chosenGrouping (Mandatory)
-     * @param group (Optional)
-     * @param groupName (Optional)
-     * @param learners (Mandatory)
+    /**
+     * @throws GroupingException
+     * @see org.lamsfoundation.lams.learningdesign.Grouper#doGrouping(org.lamsfoundation.lams.learningdesign.Grouping,java.lang.Long,
+     *      java.util.List)
      */
-    private void doGrouping(Grouping chosenGrouping, Group group, String groupName, List learners)
-    {
-    	if ( group != null ) {
-    		group.getUsers().addAll(learners);
-    	} else {
-    		chosenGrouping.getGroups().add(Group.createLearnerGroup(chosenGrouping,groupName,
-                                                                new HashSet(learners)));
-    	}
+    @Override
+    public void doGrouping(Grouping chosenGrouping, Long groupId, List learners) throws GroupingException {
+	if (groupId != null) {
+	    Iterator iter = chosenGrouping.getGroups().iterator();
+	    Group selectedGroup = null;
+	    while (iter.hasNext() && (selectedGroup == null)) {
+		Group group = (Group) iter.next();
+		if (group.getGroupId().equals(groupId)) {
+		    selectedGroup = group;
+		}
+	    }
+	    if (selectedGroup == null) {
+		String error = "Tried to add users to group " + groupId + " but group cannot be found.";
+		ChosenGrouper.log.error(error);
+		throw new GroupingException(error);
+	    }
+	    doGrouping(chosenGrouping, selectedGroup, null, learners);
+	} else {
+	    String groupName = generateGroupName(chosenGrouping);
+	    doGrouping(chosenGrouping, null, groupName, learners);
+	}
     }
-    
-    /** Create an empty group for the given grouping. If the current number of groups = max number of 
-     * groups then a grouping exception is thrown.
+
+    /**
+     * If the group exists add them to the group, otherwise creates a new group. If group is not supplied, then
+     * groupName must be supplied.
      * 
-     * @param grouping (mandatory)
-     * @param groupName (mandatory)
+     * @param chosenGrouping
+     *            (Mandatory)
+     * @param group
+     *            (Optional)
+     * @param groupName
+     *            (Optional)
+     * @param learners
+     *            (Mandatory)
      */
-    public Group createGroup(Grouping grouping, String name) throws GroupingException
-    {
-    	int currentSize = grouping.getGroups().size(); 
-    	if ( grouping.getMaxNumberOfGroups()!=null && currentSize == grouping.getMaxNumberOfGroups() ) {
-    		String error = "Tried to add group "+name+" to grouping "+grouping+". Exceeded max number of groups - current size is "+currentSize;
-    		log.error(error); 
-    		throw new GroupingException(error);
-    	}
-    	return super.createGroup(grouping, name);
-
+    private void doGrouping(Grouping chosenGrouping, Group group, String groupName, List learners) {
+	if (group != null) {
+	    group.getUsers().addAll(learners);
+	} else {
+	    chosenGrouping.getGroups().add(Group.createLearnerGroup(chosenGrouping, groupName, new HashSet(learners)));
 	}
+    }
+
+    /**
+     * Create an empty group for the given grouping. If the current number of groups = max number of groups then a
+     * grouping exception is thrown.
+     * 
+     * @param grouping
+     *            (mandatory)
+     * @param groupName
+     *            (mandatory)
+     */
+    @Override
+    public Group createGroup(Grouping grouping, String name) throws GroupingException {
+	int currentSize = grouping.getGroups().size();
+	if ((grouping.getMaxNumberOfGroups() != null) && (currentSize == grouping.getMaxNumberOfGroups())) {
+	    String error = "Tried to add group " + name + " to grouping " + grouping
+		    + ". Exceeded max number of groups - current size is " + currentSize;
+	    ChosenGrouper.log.error(error);
+	    throw new GroupingException(error);
+	}
+	return super.createGroup(grouping, name);
+
+    }
 
 }
