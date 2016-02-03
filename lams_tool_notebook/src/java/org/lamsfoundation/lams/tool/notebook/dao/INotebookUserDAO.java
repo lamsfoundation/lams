172877998cd7e8d077b720e7a90e1384bcdd5de2
FionaMalikoff <ozstitch@gmail.com>
2016-02-03 02:16:27 +0000

    LDEV-3588: Modify Notebook monitor UI to support paging, sort and search

--- lams_tool_notebook/src/java/org/lamsfoundation/lams/tool/notebook/dao/INotebookUserDAO.java
+++ lams_tool_notebook/src/java/org/lamsfoundation/lams/tool/notebook/dao/INotebookUserDAO.java
@@ -24,7 +24,11 @@
 
 package org.lamsfoundation.lams.tool.notebook.dao;
 
+import java.util.List;
+
 import org.lamsfoundation.lams.dao.IBaseDAO;
+import org.lamsfoundation.lams.notebook.service.ICoreNotebookService;
+import org.lamsfoundation.lams.tool.notebook.dto.StatisticDTO;
 import org.lamsfoundation.lams.tool.notebook.model.NotebookUser;
 
 /**
@@ -57,4 +61,15 @@ public interface INotebookUserDAO extends IBaseDAO{
 	 * @return
 	 */
 	NotebookUser getByUID(Long uid);
+	
+	/** Will return List<[NotebookUser, String, Date, Date]>
+	 * where the String is the notebook entry, the first date is the create date and the second date is the modified date. 
+	 * No notebook entries needed? Will return in their place.
+	 */
+	List<Object[]> getUsersForTablesorter(final Long sessionId, int page, int size, int sorting, String searchString, 
+		 ICoreNotebookService coreNotebookService);
+	int getCountUsersBySession(final Long sessionId, String searchString);
+	
+	/** Get the statistics for monitoring */
+	List<StatisticDTO> getStatisticsBySession(final Long contentId) ;
 }
--- lams_tool_notebook/src/java/org/lamsfoundation/lams/tool/notebook/dao/hibernate/NotebookUserDAO.java
+++ lams_tool_notebook/src/java/org/lamsfoundation/lams/tool/notebook/dao/hibernate/NotebookUserDAO.java
@@ -26,9 +26,17 @@ package org.lamsfoundation.lams.tool.notebook.dao.hibernate;
 
 import java.util.List;
 
+import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.commons.lang.StringUtils;
+import org.hibernate.Hibernate;
+import org.hibernate.SQLQuery;
+import org.hibernate.transform.Transformers;
 import org.lamsfoundation.lams.dao.hibernate.BaseDAO;
+import org.lamsfoundation.lams.notebook.service.ICoreNotebookService;
 import org.lamsfoundation.lams.tool.notebook.dao.INotebookUserDAO;
+import org.lamsfoundation.lams.tool.notebook.dto.StatisticDTO;
 import org.lamsfoundation.lams.tool.notebook.model.NotebookUser;
+import org.lamsfoundation.lams.tool.notebook.util.NotebookConstants;
 
 /**
  * DAO for accessing the NotebookUser objects - Hibernate specific code.
@@ -85,4 +93,121 @@ public class NotebookUserDAO extends BaseDAO implements INotebookUserDAO {
 
 		return (NotebookUser) list.get(0);
 	}
+	
+	@SuppressWarnings("unchecked")
+	/** Will return List<[NotebookUser, String, Date, Date]>
+	 * where the String is the notebook entry, the first date is the create date and the second date is the modified date. 
+	 * No notebook entries needed? Will return in their place.
+	 */
+	public List<Object[]> getUsersForTablesorter(final Long sessionId, int page, int size, int sorting, String searchString, 
+		ICoreNotebookService coreNotebookService) {
+	    String sortingOrder;
+	    switch (sorting) {
+	    case NotebookConstants.SORT_BY_USERNAME_ASC:
+		sortingOrder = "user.last_name ASC, user.first_name ASC";
+		break;
+	    case NotebookConstants.SORT_BY_USERNAME_DESC:
+		sortingOrder = "user.last_name DESC, user.first_name DESC";
+		break;
+	    case NotebookConstants.SORT_BY_DATE_ASC:
+		sortingOrder = "notebookCreateDate ASC";
+		break;
+	    case NotebookConstants.SORT_BY_DATE_DESC:
+		sortingOrder = "notebookCreateDate DESC";
+		break;
+	    case NotebookConstants.SORT_BY_COMMENT_ASC:
+		sortingOrder = "user.teachers_comment ASC";
+		break;
+	    case NotebookConstants.SORT_BY_COMMENT_DESC:
+		sortingOrder = "user.teachers_comment DESC";
+		break;
+	    default:
+		sortingOrder = "user.last_name, user.first_name";
+	    }
+
+	    String[] notebookEntryStrings = coreNotebookService.getNotebookEntrySQLStrings(sessionId.toString(),NotebookConstants.TOOL_SIGNATURE,"user.user_id",true );
+
+	    // Basic select for the user records
+	    StringBuilder queryText = new StringBuilder();
+	    queryText.append("SELECT user.* ");
+	    queryText.append(notebookEntryStrings[0]);
+	    queryText.append(" FROM tl_lantbk11_user user ");
+	    queryText.append(" JOIN tl_lantbk11_session session ON user.notebook_session_uid = session.uid and session.session_id = :sessionId");
+	    queryText.append(notebookEntryStrings[1]);
+
+	    // If filtering by name add a name based where clause
+	    buildNameSearch(searchString, queryText);
+
+	    // Now specify the sort based on the switch statement above.
+	    queryText.append(" ORDER BY " + sortingOrder);
+
+	    SQLQuery query =  getSession().createSQLQuery(queryText.toString());
+	    query.addEntity("user", NotebookUser.class)
+		.addScalar("notebookEntry", Hibernate.STRING)
+		.addScalar("notebookCreateDate", Hibernate.TIMESTAMP)
+		.addScalar("notebookModifiedDate", Hibernate.TIMESTAMP)
+	    	.setLong("sessionId", sessionId.longValue())
+	    	.setFirstResult(page * size)
+	    	.setMaxResults(size);
+	    return query.list();
+	}
+
+	private void buildNameSearch(String searchString, StringBuilder sqlBuilder) {
+	    if (!StringUtils.isBlank(searchString)) {
+		String[] tokens = searchString.trim().split("\\s+");
+		for (String token : tokens) {
+		    String escToken = StringEscapeUtils.escapeSql(token);
+		    sqlBuilder.append(" WHERE (user.first_name LIKE '%").append(escToken)
+		    .append("%' OR user.last_name LIKE '%").append(escToken)
+		    .append("%' OR user.login_name LIKE '%").append(escToken).append("%') ");
+		}
+	    }
+	} 
+
+	@SuppressWarnings("rawtypes")
+	public int getCountUsersBySession(final Long sessionId, String searchString) {
+
+	    StringBuilder queryText = new StringBuilder("SELECT count(*) FROM tl_lantbk11_user user ");
+	    queryText.append(" JOIN tl_lantbk11_session session ON user.notebook_session_uid = session.uid and session.session_id = :sessionId");
+	    buildNameSearch(searchString, queryText);
+	    
+	    List list = getSession().createSQLQuery(queryText.toString()).setLong("sessionId", sessionId.longValue()).list();
+	    if (list == null || list.size() == 0) {
+		return 0;
+	    }
+	    return ((Number) list.get(0)).intValue();
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public int getStatistics(final Long sessionId, String searchString) {
+
+	    StringBuilder queryText = new StringBuilder("SELECT count(*) FROM tl_lantbk11_user user ");
+	    queryText.append(" JOIN tl_lantbk11_session session ON user.notebook_session_uid = session.uid and session.session_id = :sessionId");
+	    buildNameSearch(searchString, queryText);
+	    
+	    List list = getSession().createSQLQuery(queryText.toString()).setLong("sessionId", sessionId.longValue()).list();
+	    if (list == null || list.size() == 0) {
+		return 0;
+	    }
+	    return ((Number) list.get(0)).intValue();
+	}
+
+	private static final String GET_STATISTICS = "SELECT session.session_id sessionId, session.session_name sessionName, "
+		+ " COUNT(user.uid) numLearners, SUM(user.finishedActivity) numLearnersFinished "
+		+ " FROM tl_lantbk11_notebook notebook, tl_lantbk11_session session, tl_lantbk11_user user "
+		+ " WHERE notebook.tool_content_id = :contentId and notebook.uid = session.notebook_uid AND user.notebook_session_uid = session.uid "
+		+ " GROUP BY session.session_id";
+	
+	@SuppressWarnings("unchecked")
+	public List<StatisticDTO> getStatisticsBySession(final Long contentId) {
+	    SQLQuery query =  getSession().createSQLQuery(GET_STATISTICS);
+	    query.addScalar("sessionId", Hibernate.LONG)
+		.addScalar("sessionName", Hibernate.STRING)
+		.addScalar("numLearners", Hibernate.INTEGER)
+		.addScalar("numLearnersFinished", Hibernate.INTEGER)
+		.setLong("contentId", contentId)
+	    	.setResultTransformer(Transformers.aliasToBean(StatisticDTO.class));
+	    return query.list();
+	}
+
 }
