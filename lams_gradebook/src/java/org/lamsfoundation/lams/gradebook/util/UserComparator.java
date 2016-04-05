da1be8146d2ef06173298f684230e39a691d707a
wkiaora <balandrey@gmail.com>
2016-04-05 19:01:37 +0000

    LDEV-3732 Sort gradebook excel export by name

---
+++ lams_gradebook/src/java/org/lamsfoundation/lams/gradebook/util/UserComparator.java
@@ -0,0 +1,16 @@
+package org.lamsfoundation.lams.gradebook.util;
+
+import java.util.Comparator;
+
+import org.apache.commons.lang.builder.CompareToBuilder;
+import org.lamsfoundation.lams.usermanagement.User;
+
+/**
+ * Sorts users by last names.
+ */
+public class UserComparator implements Comparator<User> {
+    public int compare(User o1, User o2) {
+	return new CompareToBuilder().append(o1.getLastName(), o2.getLastName())
+		.append(o1.getFirstName(), o2.getFirstName()).append(o1.getLogin(), o2.getLogin()).toComparison();
+    }
+}
