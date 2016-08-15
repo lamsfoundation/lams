aea8f2cfafb5aa6839e979a9128a20727af6b65e
erniegx <ernieg@gmail.com>
2016-08-15 07:30:46 +0000

    LDEV-3952: updating scripts to be compatible with MySQL 5.7

--- lams_tool_bbb/src/java/org/lamsfoundation/lams/tool/bbb/dbupdates/patch20151217.sql
+++ lams_tool_bbb/src/java/org/lamsfoundation/lams/tool/bbb/dbupdates/patch20151217.sql
@@ -5,12 +5,12 @@ SET FOREIGN_KEY_CHECKS=0;
 
 -- LDEV-3640 Add necessary cascades
 
-ALTER IGNORE TABLE tl_labbb10_session DROP FOREIGN KEY fk_labbb10_bbb_session_to_bbb;
+ALTER TABLE tl_labbb10_session DROP FOREIGN KEY fk_labbb10_bbb_session_to_bbb;
 ALTER TABLE tl_labbb10_session ADD CONSTRAINT fk_labbb10_bbb_session_to_bbb FOREIGN KEY (`bbb_uid`)
 REFERENCES `tl_labbb10_bbb` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 
-ALTER IGNORE TABLE tl_labbb10_user DROP FOREIGN KEY fk_labbb10_bbb_user_to_bbb_session;
+ALTER TABLE tl_labbb10_user DROP FOREIGN KEY fk_labbb10_bbb_user_to_bbb_session;
 ALTER TABLE tl_labbb10_user ADD CONSTRAINT fk_labbb10_bbb_user_to_bbb_session FOREIGN KEY (`bbb_session_uid`)
 REFERENCES `tl_labbb10_session` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE;
 
