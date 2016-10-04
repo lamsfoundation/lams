e2caf5cd352afc1fe07e8e1c24bbb86013bd79aa
MarcinCieslak <mgr.inz.marcin.cieslak@gmail.com>
2016-10-04 06:28:25 +0000

    LDEV-3984: Merge Learning actions into one.

---
+++ lams_tool_vote/src/java/org/lamsfoundation/lams/tool/vote/dbupdates/patch20161004.sql
@@ -0,0 +1,18 @@
+-- Turn off autocommit, so nothing is committed if there is an error
+SET AUTOCOMMIT = 0;
+SET FOREIGN_KEY_CHECKS=0;
+----------------------Put all sql statements below here-------------------------
+
+-- LDEV-3984 Rename actions to have more simple names
+ 
+UPDATE lams_tool SET learner_url = 'tool/lavote11/learning.do?dispatch=start&mode=learner',
+					 learner_preview_url = 'tool/lavote11/learning.do?dispatch=start&mode=author',
+					 learner_progress_url = 'tool/lavote11/learning.do?dispatch=start&mode=teacher'
+WHERE tool_signature = 'lavote11';
+
+----------------------Put all sql statements above here-------------------------
+
+-- If there were no errors, commit and restore autocommit to on
+COMMIT;
+SET AUTOCOMMIT = 1;
+SET FOREIGN_KEY_CHECKS=1;
