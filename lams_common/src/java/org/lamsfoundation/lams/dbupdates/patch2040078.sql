a63494f0d1d4183ab81cdbbaeef8999a2c13fcb8
SanjanaPabsetti <Sanjana.pabsetti@gmail.com>
2016-11-30 03:31:10 +0000

    LDEV-4030	:Disable login for a few minutes after X number of attempts

---
+++ lams_common/src/java/org/lamsfoundation/lams/dbupdates/patch2040078.sql
@@ -0,0 +1,14 @@
+SET AUTOCOMMIT = 0;
+set FOREIGN_KEY_CHECKS = 0;
+ 
+--  LDEV-4023 Implement password policy 
+insert into lams_configuration (config_key, config_value, description_key, header_name, format, required)
+values ('FailedAttempts','3', 'config.failed.attempts', 'config.header.system', 'LONG', 1);
+
+
+insert into lams_configuration (config_key, config_value, description_key, header_name, format, required)
+values ('LockOutTime','5', 'config.lock.out.time', 'config.header.system', 'LONG', 1);
+
+COMMIT;
+SET AUTOCOMMIT = 1;
+set FOREIGN_KEY_CHECKS = 1;
