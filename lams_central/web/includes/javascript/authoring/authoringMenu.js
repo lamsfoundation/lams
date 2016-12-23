90d134e4f1b7d4ff2ba734e3841b64c34e973632
FionaMalikoff <Fiona.Malikoff@gmail.com>
2016-12-23 01:35:44 +0000

    LDEV-4169: Scrollbar missing in preview in older versions of IE and Firefox.

--- lams_central/web/includes/javascript/authoring/authoringMenu.js
+++ lams_central/web/includes/javascript/authoring/authoringMenu.js
@@ -519,7 +519,7 @@ var MenuLib = {
 					success : function() {
 						// open preview pop up window
 						window.open(LAMS_URL + 'home.do?method=learner&mode=preview&lessonID='+lessonID,'Preview',
-									'width=1280,height=720,resizable,status=yes');
+									'width=1280,height=720,resizable,scrollbars=yes,status=yes');
 						previewButton.button('reset');
 					}
 				});
