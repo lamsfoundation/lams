1b44c36a88f382c24309af71dca1807781f11594
wkiaora <balandrey@gmail.com>
2017-02-16 04:31:48 +0000

    LDEV-3940 Move Java code from jsps to proper util/service Java classes

--- lams_bb_integration/src/org/lamsfoundation/ld/integration/blackboard/StartLessonServlet.java
+++ lams_bb_integration/src/org/lamsfoundation/ld/integration/blackboard/StartLessonServlet.java
@@ -19,76 +19,101 @@ package org.lamsfoundation.ld.integration.blackboard;
  * 
  * http://www.gnu.org/licenses/gpl.txt 
  * **************************************************************** 
- */ 
+ */
 
 import java.io.IOException;
+import java.text.ParseException;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.log4j.Logger;
 import org.lamsfoundation.ld.integration.util.BlackboardUtil;
+import org.xml.sax.SAXException;
 
+import blackboard.base.InitializationException;
+import blackboard.data.ValidationException;
+import blackboard.data.content.CourseDocument;
+import blackboard.data.course.Course;
 import blackboard.data.user.User;
+import blackboard.persist.BbPersistenceManager;
+import blackboard.persist.Id;
+import blackboard.persist.PersistenceException;
+import blackboard.platform.BbServiceException;
 import blackboard.platform.BbServiceManager;
+import blackboard.platform.context.Context;
 import blackboard.platform.context.ContextManager;
+import blackboard.platform.persistence.PersistenceServiceFactory;
+import blackboard.platform.plugin.PlugInException;
+import blackboard.platform.plugin.PlugInUtil;
 
 /**
- * Starts a lesson, returning the BB Content Id in JSON. Based on start_lesson_proc but uses the username
- * parameter as a basis for identifying the user. 
- * Return a server error rather than throw an exception as this will be consumed by AJAX call or the like.
+ * Starts LAMS lesson and stores its relevant content in Blackboard.
  */
 public class StartLessonServlet extends HttpServlet {
 
     private static final long serialVersionUID = -351131323404991332L;
     private static Logger logger = Logger.getLogger(StartLessonServlet.class);
-    
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-	process(request, response);
-    }
 
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-	process(request, response);
-    }
-
-    protected void process(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+	// Authorize current user for Course Control Panel (automatic redirect)
+	try {
+	    if (!PlugInUtil.authorizeForCourseControlPanel(request, response))
+		return;
+	} catch (PlugInException e) {
+	    throw new RuntimeException(e);
+	}
+	
+    	BbPersistenceManager bbPm = PersistenceServiceFactory.getInstance().getDbPersistenceManager();
 
 	ContextManager ctxMgr = null;
 	try {
 	    // get Blackboard context
 	    ctxMgr = (ContextManager) BbServiceManager.lookupService(ContextManager.class);
+	    Context ctx = ctxMgr.getContext();
 
+	    //store newly created LAMS lesson
+	    User user = ctx.getUser();
+	    BlackboardUtil.storeBlackboardContent(request, response, user);
+	    
+	    // constuct strReturnUrl
 	    String courseIdStr = request.getParameter("course_id");
 	    String contentIdStr = request.getParameter("content_id");
-	    String strTitle = BlackboardUtil.getTrimmedString(request, "title");
-	    String strSequenceID = BlackboardUtil.getTrimmedString(request, "sequence_id");	    
-	    
-	    if ( courseIdStr == null || contentIdStr == null || strSequenceID.length()==0 || strTitle.length() == 0) {
-		response.sendError(HttpServletResponse.SC_BAD_REQUEST, 
-			"Unable to create error - parameter missing. course_id, content_id, sequence_id and title required");
-	    } else {
+	    // internal Blackboard IDs for the course and parent content item
+	    Id courseId = bbPm.generateId(Course.DATA_TYPE, courseIdStr);
+	    Id folderId = bbPm.generateId(CourseDocument.DATA_TYPE, contentIdStr);
+	    String returnUrl = PlugInUtil.getEditableContentReturnURL(folderId, courseId);
+	    request.setAttribute("returnUrl", returnUrl);
 
-		String username = request.getParameter("username");
-		User user = BlackboardUtil.loadUserFromDB(username);
-		
-		String bbContentId = BlackboardUtil.storeBlackboardContent(request, response, user);
-
-		response.setContentType("application/json;charset=UTF-8");
-		response.getWriter().print("{\"content_id\":" + bbContentId + "}");
-	    }
-	    
-	} catch (Exception e) {
-	    logger.error("Unable to start lesson "+e.getMessage(), e);
-	    response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Unable to start lesson "+e.getMessage());
+	} catch (PersistenceException  e) {
+	    throw new ServletException(e);
+	} catch (ParseException e) {
+	    throw new ServletException(e);
+	} catch (ValidationException e) {
+	    throw new ServletException(e);
+	} catch (ParserConfigurationException e) {
+	    throw new ServletException(e);
+	} catch (SAXException e) {
+	    throw new ServletException(e);
+	} catch (InitializationException e) {
+	    throw new ServletException(e);
+	} catch (BbServiceException e) {
+	    throw new ServletException(e);
 	} finally {
-	    // make sure context is released
-	    if (ctxMgr != null) {
-		ctxMgr.releaseContext();
-	    }
+	    // important. make sure context is not released as otherwise <bbNG:genericPage> tag will throw an exception
+//	    if (ctxMgr != null) {
+//		ctxMgr.releaseContext();
+//	    }
 	}
+	
+	request.getRequestDispatcher("/modules/startLessonSuccess.jsp").forward(request, response);
+    }
 
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+	doGet(request, response);
     }
 
 }
-
