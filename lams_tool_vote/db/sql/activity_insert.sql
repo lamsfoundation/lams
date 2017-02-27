f3f37b621ddc8f9962a620182ad25a6bbfb2a4f5
erniegx <ernieg@gmail.com>
2017-02-27 22:36:00 +0000

    LDEV-4180 DB fixes for vote (as per Marcin's request)

--- lams_tool_vote/db/sql/activity_insert.sql
+++ lams_tool_vote/db/sql/activity_insert.sql
@@ -1,97 +1,30 @@
--- Copyright (C) 2005 LAMS Foundation (http://lamsfoundation.org)
--- =============================================================
--- License Information: http://lamsfoundation.org/licensing/lams/2.0/
--- 
--- This program is free software; you can redistribute it and/or modify
--- it under the terms of the GNU General Public License version 2.0 as 
--- published by the Free Software Foundation.
--- 
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU General Public License for more details.
--- 
--- You should have received a copy of the GNU General Public License
--- along with this program; if not, write to the Free Software
--- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
--- USA
--- 
--- http://www.gnu.org/licenses/gpl.txt
--- ****************************************************************
---
-
-
 INSERT INTO lams_learning_activity
 (
-activity_ui_id
-, description
+  description
 , title
 , help_text
-, xcoord
-, ycoord
-, parent_activity_id
-, parent_ui_id
 , learning_activity_type_id
 , grouping_support_type_id
 , apply_grouping_flag
-, grouping_id
-, grouping_ui_id
-, order_id
-, define_later_flag
-, learning_design_id
 , learning_library_id
 , create_date_time
-, run_offline_flag
-, max_number_of_options
-, min_number_of_options
-, options_instructions
 , tool_id
-, tool_content_id
 , activity_category_id
-, gate_activity_level_id
-, gate_open_flag
-, gate_start_time_offset
-, gate_end_time_offset
 , library_activity_ui_image
-, create_grouping_id
-, create_grouping_ui_id
-, library_activity_id
 , language_file
 )
 VALUES
 (
-NULL
-, 'Allows creation and use of voting format'
+  'Allows creation and use of voting format'
 , 'Voting'
 , 'Help text'
-, NULL
-, NULL
-, NULL
-, NULL
 , 1
 , 2
 , 0
-, NULL
-, NULL
-, NULL
-, 0
-, NULL
 , ${learning_library_id}
 , NOW()
-, 0
-, NULL
-, NULL
-, NULL
 , ${tool_id}
-, NULL
 , 6
-, NULL
-, NULL
-, NULL
-, NULL
 , 'tool/lavote11/images/icon_ranking.svg'
-, NULL
-, NULL
-, NULL
 ,'org.lamsfoundation.lams.tool.vote.ApplicationResources'
 )
