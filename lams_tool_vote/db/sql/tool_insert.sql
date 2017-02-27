bb9636f8038c20ea45b41680f3d822a0bc8561a5
erniegx <ernieg@gmail.com>
2017-02-27 22:36:00 +0000

    LDEV-4180 DB fixes for vote (as per Marcin's request)

--- lams_tool_vote/db/sql/tool_insert.sql
+++ lams_tool_vote/db/sql/tool_insert.sql
@@ -1,26 +1,3 @@
--- Copyright (C) 2005 LAMS Foundation (http://lamsfoundation.org)
--- =============================================================
--- License Information: http://lamsfoundation.org/licensing/lams/2.0/
--- 
--- This program is free software; you can redistribute it and/or modify
--- it under the terms of the GNU General Public License version 2.0 as 
--- published by the Free Software Foundation.
--- 
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU General Public License for more details.
--- 
--- You should have received a copy of the GNU General Public License
--- along with this program; if not, write to the Free Software
--- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
--- USA
--- 
--- http://www.gnu.org/licenses/gpl.txt
--- ****************************************************************
---
-
-
 INSERT INTO lams_tool
 (
 tool_signature,
@@ -29,8 +6,6 @@ tool_display_name,
 description,
 tool_identifier,
 tool_version,
-learning_library_id,
-default_tool_content_id,
 valid_flag,
 grouping_support_type_id,
 supports_run_offline_flag,
@@ -55,16 +30,14 @@ VALUES
 'Voting',
 'vote',
 '@tool_version@',
-NULL,
-NULL,
 0,
 2,
 1,
-'tool/lavote11/learningStarter.do?mode=learner',
-'tool/lavote11/learningStarter.do?mode=author',
-'tool/lavote11/learningStarter.do?mode=teacher',
-'tool/lavote11/authoringStarter.do',
-'tool/lavote11/monitoringStarter.do',
+'tool/lavote11/learning.do?dispatch=start&mode=learner',
+'tool/lavote11/learning.do?dispatch=start&mode=author',
+'tool/lavote11/learning.do?dispatch=start&mode=teacher',
+'tool/lavote11/authoring.do?dispatch=start',
+'tool/lavote11/monitoring.do?dispatch=start',
 'tool/lavote11/defineLaterStarter.do',
 'tool/lavote11/pedagogicalPlanner.do',
 'http://wiki.lamsfoundation.org/display/lamsdocs/lavote11',
@@ -73,4 +46,3 @@ NOW(),
 NOW(),
 1
 )
-
