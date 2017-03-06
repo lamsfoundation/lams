0484f09a01d9b6b6d0c8ef375206c651c6694dc1
FionaMalikoff <Fiona.Malikoff@gmail.com>
2017-03-06 12:34:45 +0000

    LDEV-4245: Removing logo upload from login maintenance screen.

--- lams_admin/src/java/org/lamsfoundation/lams/admin/web/LoginSaveAction.java
+++ lams_admin/src/java/org/lamsfoundation/lams/admin/web/LoginSaveAction.java
@@ -26,7 +26,6 @@ package org.lamsfoundation.lams.admin.web;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.nio.charset.Charset;
 
@@ -36,43 +35,20 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-import org.apache.struts.action.ActionMessage;
-import org.apache.struts.action.ActionMessages;
 import org.apache.struts.action.DynaActionForm;
-import org.apache.struts.upload.FormFile;
 import org.lamsfoundation.lams.util.Configuration;
 import org.lamsfoundation.lams.util.ConfigurationKeys;
 import org.lamsfoundation.lams.web.action.LamsDispatchAction;
 
 /**
- * <p>
- * <a href="LoginSaveAction.java.html"><i>View Source</i><a>
- * </p>
- *
  * Use DispatchAction for future extension convenience, e.g. add preview feature
  *
  * @author <a href="mailto:fyang@melcoe.mq.edu.au">Fei Yang</a>
  */
-
-/**
- * struts doclet
- *
- *
- * "method" scope = "request" input = ".loginmaintian" validate =
- * "false"
- *
- *
- *
- */
 public class LoginSaveAction extends LamsDispatchAction {
-
-    private static final String IMAGE_FOLDER_SUFFIX = File.separatorChar + "lams-www.war" + File.separatorChar
-	    + "images";
 
     private static final String NEWS_PAGE_PATH_SUFFIX = File.separatorChar + "lams-www.war" + File.separatorChar
 	    + "news.html";
-
-    private static final String LOGO_FILENAME = "lams_login.gif";
 
     public ActionForward save(ActionMapping mapping, ActionForm form, HttpServletRequest request,
 	    HttpServletResponse response) throws Exception {
@@ -82,68 +58,21 @@ public class LoginSaveAction extends LamsDispatchAction {
 	}
 
 	DynaActionForm loginMaintainForm = (DynaActionForm) form;
-	ActionMessages errors = new ActionMessages();
-	FormFile file = (FormFile) loginMaintainForm.get("logo");
-	if ((file != null) && (file.getFileSize() != 0)) {
-	    checkFile(errors, file);
-	}
-	if (errors.isEmpty()) {
-	    if ((file != null) && (file.getFileSize() != 0)) {
-		updateImageFile(file, LOGO_FILENAME);
-
-	    }
-	    updateNewsPage(loginMaintainForm.getString("news"));
-	} else {
-	    saveErrors(request, errors);
-	    return mapping.findForward("loginmaintain");
-	}
-	return mapping.findForward("sysadmin");
-    }
-
-    private void updateNewsPage(String news) throws IOException {
 	BufferedWriter bWriter = null;
 	try {
-	    OutputStreamWriter ow = new OutputStreamWriter(
-		    new FileOutputStream(Configuration.get(ConfigurationKeys.LAMS_EAR_DIR) + NEWS_PAGE_PATH_SUFFIX),
+	    OutputStreamWriter ow = new OutputStreamWriter(new FileOutputStream(
+		    Configuration.get(ConfigurationKeys.LAMS_EAR_DIR) + NEWS_PAGE_PATH_SUFFIX),
 		    Charset.forName("UTF-8"));
 	    bWriter = new BufferedWriter(ow);
-	    bWriter.write(news);
+	    bWriter.write(loginMaintainForm.getString("news"));
 	    bWriter.flush();
 	} finally {
 	    if (bWriter != null) {
 		bWriter.close();
 	    }
 	}
-    }
 
-    private void updateImageFile(FormFile file, String fileName) throws IOException {
-	File imagesFolder = new File(Configuration.get(ConfigurationKeys.LAMS_EAR_DIR) + IMAGE_FOLDER_SUFFIX);
-	if (!imagesFolder.exists()) {
-	    imagesFolder.mkdir();
-	}
-	String imageFilePath = Configuration.get(ConfigurationKeys.LAMS_EAR_DIR) + IMAGE_FOLDER_SUFFIX
-		+ File.separatorChar + fileName;
-	FileOutputStream out = null;
-	try {
-	    out = new FileOutputStream(imageFilePath);
-	    out.write(file.getFileData());
-	    out.flush();
-	} finally {
-	    if (out != null) {
-		out.close();
-	    }
-	}
-
-    }
-
-    private void checkFile(ActionMessages errors, FormFile file) {
-	boolean imgFormat = file.getContentType().contains("image");
-	if (!imgFormat) {
-	    errors.add("format", new ActionMessage("error.img.format"));
-	}
-	if (file.getFileSize() > 4096 * 1024) {
-	    errors.add("size", new ActionMessage("error.img.size"));
-	}
+	return mapping.findForward("sysadmin");
     }
 
 }
