e912afec9139205b810a097666550990db5cc47d
erniegx <ernieg@gmail.com>
2017-03-07 22:55:15 +0000

    LDEV-4180: fixing typos

--- lams_tool_task/src/java/org/lamsfoundation/lams/tool/taskList/dbupdates/patch20170101.sql
+++ lams_tool_task/src/java/org/lamsfoundation/lams/tool/taskList/dbupdates/patch20170101.sql
@@ -4,14 +4,13 @@ SET FOREIGN_KEY_CHECKS=0;
 ----------------------Put all sql statements below here-------------------------
 
 -- LDEV-4180
-ALTER TABLE tl_latask10_tasklist MODIFY COLUMN lock_on_finished TINYINT(1),
+ALTER TABLE tl_latask10_tasklist MODIFY COLUMN lock_when_finished TINYINT(1),
 							   	 MODIFY COLUMN content_in_use TINYINT(1),
 							   	 MODIFY COLUMN define_later TINYINT(1),
 							   	 MODIFY COLUMN is_sequential_order TINYINT(1),
 							   	 MODIFY COLUMN reflect_on_activity TINYINT(1),
 							   	 MODIFY COLUMN allow_contribute_tasks TINYINT(1),
-							   	 MODIFY COLUMN is_monitor_verification_required TINYINT(1),
-							   	 MODIFY COLUMN allow_contribute_tasks TINYINT(1);
+							   	 MODIFY COLUMN is_monitor_verification_required TINYINT(1);
 									 	
 ALTER TABLE tl_latask10_tasklist_item MODIFY COLUMN create_by_author TINYINT(1),
 								 	  MODIFY COLUMN is_required TINYINT(1),
