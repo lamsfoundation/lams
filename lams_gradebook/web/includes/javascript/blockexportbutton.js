c81f6bc91f57ba94de5f9631d889176668abc5af
FionaMalikoff <Fiona.Malikoff@gmail.com>
2017-05-11 05:54:01 +0000

    LDEV-4239: Making the gradebook buttons responsive.

--- lams_gradebook/web/includes/javascript/blockexportbutton.js
+++ lams_gradebook/web/includes/javascript/blockexportbutton.js
@@ -2,19 +2,20 @@
 //http://geekswithblogs.net/GruffCode/archive/2010/10/28/detecting-the-file-download-dialog-in-the-browser.aspx
 var fileDownloadCheckTimer;
 
-function blockExportButton(areaToBlock, exportExcelUrl, labelWait) {
+function blockExportButton(areaToBlock, exportExcelUrl, labelWait, smallVersion) {
 	var token = new Date().getTime(); //use the current timestamp as the token value
-		    
+	var fontSize = smallVersion ? '12px' : "16px";
+
 	$('#' + areaToBlock).block({
-		message: '<span style="font-size: 18px;color:#fff";>' + labelWait + '</span>',
+		message: '<span style="font-size: '+fontSize+';color:#fff";>' + labelWait + '</span>',
 		baseZ: 1000000,
 		fadeIn:  0, 
 		css: { 
 			border: 'none', 
-		    padding: '10px', 
+		    padding: '5px', 
 		    backgroundColor: '#000', 
-		    '-webkit-border-radius': '10px', 
-		    '-moz-border-radius': '10px', 
+		    '-webkit-border-radius': '5px', 
+		    '-moz-border-radius': '5px', 
 		    opacity: .98 
 		},
 		overlayCSS: {
