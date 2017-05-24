834da7229c30ebb1fb75204456bde0fe0d3c5764
FionaMalikoff <ozstitch@gmail.com>
2017-05-24 11:35:42 +0000

    LDEV-2999: Moving the database update into the tool as the Peer Review tool is not included in the standard build.

--- lams_common/src/java/org/lamsfoundation/lams/dbupdates/patch20170512.sql
+++ lams_common/src/java/org/lamsfoundation/lams/dbupdates/patch20170512.sql
@@ -39,34 +39,7 @@ UPDATE lams_rating_comment r
 JOIN tmp_imagegallery_rating_sessionid tmp ON r.uid = tmp.rating_uid
 SET r.tool_session_id = tmp.session_id;
 
--- Update existing Peer Review
-DROP TABLE IF EXISTS tmp_peerreview_rating_sessionid;
-CREATE TEMPORARY TABLE tmp_peerreview_rating_sessionid AS
-SELECT r.uid rating_uid, prs.session_id session_id
-FROM lams_rating r
-JOIN lams_rating_criteria rc ON r.rating_criteria_id = rc.rating_criteria_id
-JOIN tl_laprev11_peerreview pr ON rc.tool_content_id = pr.content_id
-JOIN tl_laprev11_session prs ON pr.uid = prs.peerreview_uid
-JOIN tl_laprev11_user pru ON r.user_id = pru.user_id AND prs.uid = pru.session_uid;
-
-UPDATE lams_rating r
-JOIN tmp_peerreview_rating_sessionid tmp ON r.uid = tmp.rating_uid
-SET r.tool_session_id = tmp.session_id;
-
-DROP TABLE tmp_peerreview_rating_sessionid;
-CREATE TEMPORARY TABLE tmp_peerreview_rating_sessionid AS
-SELECT r.uid rating_uid, prs.session_id session_id
-FROM lams_rating_comment r
-JOIN lams_rating_criteria rc ON r.rating_criteria_id = rc.rating_criteria_id
-JOIN tl_laprev11_peerreview pr ON rc.tool_content_id = pr.content_id
-JOIN tl_laprev11_session prs ON pr.uid = prs.peerreview_uid
-JOIN tl_laprev11_user pru ON r.user_id = pru.user_id AND prs.uid = pru.session_uid;
-
-UPDATE lams_rating_comment r
-JOIN tmp_peerreview_rating_sessionid tmp ON r.uid = tmp.rating_uid
-SET r.tool_session_id = tmp.session_id;
-
-DROP TABLE tmp_peerreview_rating_sessionid;
+-- Update existing Peer Review done by the Peer Review tool
 
 -- Update existing QA
 DROP TABLE IF EXISTS tmp_qa_rating_sessionid;
