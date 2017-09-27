554bc63c0a5714f75d3acc2e5e4144eaea825704
Andrey Balan <balandrey@gmail.com>
2017-09-27 20:43:04 +0300

    LDEV-4428 Make extraPlugin setting contain only extra plugins

--- lams_central/web/includes/javascript/ckconfig_custom.js
+++ lams_central/web/includes/javascript/ckconfig_custom.js
@@ -129,7 +129,7 @@ CKEDITOR.config.browserContextMenuOnCtrl = true;
 CKEDITOR.config.format_tags	= 'div;h1;h2;h3;h4;h5;h6;pre;address;p' ;
 CKEDITOR.config.enterMode = 'div';
 CKEDITOR.plugins.addExternal('wikilink', CKEDITOR.basePath + '../tool/lawiki10/wikilink/', 'plugin.js');
-CKEDITOR.config.extraPlugins = 'wikilink,jlatexmath,iframe,lineutils,widget,sourcedialog,dialog';
+CKEDITOR.config.extraPlugins = 'wikilink,jlatexmath';
 CKEDITOR.config.enterMode = CKEDITOR.ENTER_DIV; 
 CKEDITOR.config.removePlugins = 'elementspath';
 CKEDITOR.config.allowedContent = true;
