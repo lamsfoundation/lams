98d80295ca2398cfef80d796b9581cbc399a4077
Andrey Balan <balandrey@gmail.com>
2017-10-04 20:01:37 +0300

    LDEV-4432 Add new CKEditor plugin for working with Bootsrap snippets

---
+++ lams_central/web/ckeditor/plugins/bootsnippets/lang/en.js
@@ -0,0 +1,14 @@
+﻿/*
+Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang( 'bootsnippets', 'en', {
+	emptyListMsg: '(No snippets defined)',
+	insertOption: 'Replace actual contents',
+	options: 'Snippet Options',
+	title: 'Content snippets',
+	tableButton: 'Table Snippets',
+	imageButton: 'Image Snippets',
+	multimediaButton: 'Multimedia Snippets',
+	tabsButton: 'Tabs Snippets'
+} );
