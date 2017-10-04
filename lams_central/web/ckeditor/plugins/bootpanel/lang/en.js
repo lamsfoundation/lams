38961928fde359d35287ed2fb629d4ebb56072ef
Andrey Balan <balandrey@gmail.com>
2017-10-04 19:59:56 +0300

    LDEV-4432 Create plugin that will handle editing of Bootsrap panels
    The name of the plugin is 'bootpanel' and it allows editing some of
    panel's properties including making them clickable.

---
+++ lams_central/web/ckeditor/plugins/bootpanel/lang/en.js
@@ -0,0 +1,32 @@
+/*
+Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang( 'bootpanel', 'en', {
+	button: 'Bootstrap Element',
+	panelHeading: 'Panel Text',
+	border: 'Border',
+	btnUpload: 'Send it to the Server',
+	button2Img: 'Do you want to transform the selected image button on a simple image?',
+	hSpace: 'HSpace',
+	img2Button: 'Do you want to transform the selected image on a image button?',
+	infoTab: 'Image Info',
+	linkTab: 'Link',
+	lockRatio: 'Lock Ratio',
+	menu: 'Image Properties',
+	resetSize: 'Reset Size',
+	title: 'Editing Bootstrap Panel',
+	upload: 'Upload',
+	typeButton: 'Button',
+	typeLink: 'Link',
+	typePanel: 'Panel',
+	elementCategory: 'Element type',
+	bootClass: 'Color',
+	classDefault: 'Default',
+	classPrimary: 'Primary',
+	classSuccess: 'Success',
+	classInfo: 'Info',
+	classWarning: 'Warning',
+	classDanger: 'Danger',
+	isClickable: 'Is panel clickable'
+} );
