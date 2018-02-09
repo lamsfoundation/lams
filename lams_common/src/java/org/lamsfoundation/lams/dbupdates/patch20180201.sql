86eaa0ccf8539aef510325a640e219fdd23668fd
MarcinCieslak <mgr.inz.marcin.cieslak@gmail.com>
2018-02-09 09:42:16 +0100

    LDEV-4507 Add query allowing index to be created on existing DB

--- lams_common/src/java/org/lamsfoundation/lams/dbupdates/patch20180201.sql
+++ lams_common/src/java/org/lamsfoundation/lams/dbupdates/patch20180201.sql
@@ -2,6 +2,8 @@ SET AUTOCOMMIT = 0;
 set FOREIGN_KEY_CHECKS = 0;
  
 --  LDEV-4507 Make sure that duplicate groups are not created
+UPDATE lams_grouping AS gi JOIN lams_group AS g ON gi.staff_group_id = g.group_id SET g.order_id = 2;
+
 ALTER TABLE lams_group ADD UNIQUE KEY UQ_lams_group_1 (grouping_id, order_id);
  
 COMMIT;
