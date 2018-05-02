0a8b2d3c781ace2a03235c2d40e65be7fe6f24af
Andrey Balan <balandrey@gmail.com>
2018-05-02 20:51:09 +0300

    LDEV-4557 Fix Upload image dialog

--- lams_central/web/includes/javascript/ckconfig_custom.js
+++ lams_central/web/includes/javascript/ckconfig_custom.js
@@ -137,6 +137,7 @@ CKEDITOR.config.removePlugins = 'elementspath,about,specialchar';
 CKEDITOR.config.allowedContent = true;
 CKEDITOR.config.toolbarCanCollapse = true;
 CKEDITOR.config.embed_provider = '//ckeditor.iframe.ly/api/oembed?url={url}&callback={callback}';//default URL for 'embed' plugin
+CKEDITOR.config.filebrowserUploadMethod	 = 'form';
 //allow empty i and span tags (for font awesome)
 CKEDITOR.dtd.$removeEmpty['i'] = false;
 CKEDITOR.dtd.$removeEmpty['span'] = false;
