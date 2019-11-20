cd25fd46424cbe1430b34ce394ee2cd35c92cc04
MarcinCieslak <mgr.inz.marcin.cieslak@gmail.com>
2019-11-20 13:10:03 +0100

    LDEV-4746 Do not string img tags during migration

--- lams_common/src/java/org/lamsfoundation/lams/dbupdates/patch20190721.sql
+++ lams_common/src/java/org/lamsfoundation/lams/dbupdates/patch20190721.sql
@@ -1,11 +1,14 @@
 CREATE FUNCTION `strip_tags`($str MEDIUMTEXT) RETURNS MEDIUMTEXT
 BEGIN
     DECLARE $start, $end INT DEFAULT 1;
-    LOOP
+    DECLARE $tag CHAR(3);
+    main: LOOP
         SET $start = LOCATE("<", $str, $start);
-        IF (!$start) THEN RETURN $str; END IF;
+        IF (NOT $start) THEN RETURN $str; END IF;
         SET $end = LOCATE(">", $str, $start);
-        IF (!$end) THEN SET $end = $start; END IF;
+        IF (NOT $end) THEN SET $end = $start; END IF;
+        SET $tag = SUBSTRING($str, $start + 1, 3);
+        IF $tag = 'img' OR $tag = 'IMG' THEN SET $start = $end; ITERATE main; END IF;
         SET $str = INSERT($str, $start, $end - $start + 1, "");
     END LOOP;
 END;
