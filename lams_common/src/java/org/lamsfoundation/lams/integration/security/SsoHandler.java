03858df7730f75865fc4eec12d27554585617d90
MarcinCieslak <mgr.inz.marcin.cieslak@gmail.com>
2019-11-22 16:55:15 +0100

    LDEV-4030 Prevent failedAttempts overflow in DB

--- lams_common/src/java/org/lamsfoundation/lams/integration/security/SsoHandler.java
+++ lams_common/src/java/org/lamsfoundation/lams/integration/security/SsoHandler.java
@@ -158,17 +158,14 @@ public class SsoHandler implements ServletExtension {
 
 		}
 
-		// check if user is already logged in
-		HttpSession existingSession = SessionManager.getSessionForLogin(login);
-		
 		// store session so UniversalLoginModule can access it
 		SessionManager.startSession(request);
 
 		String oldSessionID = session.getId();
-		
+
 		// do the logging in UniversalLoginModule or cache
 		handler.handleRequest(exchange);
-		
+
 		// session ID was changed after log in
 		SessionManager.updateSessionID(oldSessionID);
 
@@ -186,15 +183,12 @@ public class SsoHandler implements ServletExtension {
 		} else {
 		    // clear after failed authentication, if it was set in LoginRequestServlet
 		    session.removeAttribute("integratedLogoutURL");
-		    
+
 		    Integer failedAttempts = user.getFailedAttempts();
-		    if (failedAttempts == null) {
-			failedAttempts = 1;
-		    } else {
-			failedAttempts++;
-		    }
-		    user.setFailedAttempts(failedAttempts);
 		    Integer failedAttemptsConfig = Configuration.getAsInt(ConfigurationKeys.FAILED_ATTEMPTS);
+		    // do not allow more failed attempts than limit in config as we may overflow failedAttempts column in DB
+		    failedAttempts = failedAttempts == null ? 1 : Math.min(failedAttempts + 1, failedAttemptsConfig);
+		    user.setFailedAttempts(failedAttempts);
 
 		    if (failedAttempts >= failedAttemptsConfig) {
 			Integer lockOutTimeConfig = Configuration.getAsInt(ConfigurationKeys.LOCK_OUT_TIME);
