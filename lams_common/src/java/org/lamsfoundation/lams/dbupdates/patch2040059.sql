31403cc1e0d4a622678bb8143d5ed7100ecb1afd
MarcinCieslak <mgr.inz.marcin.cieslak@gmail.com>
2020-01-03 06:40:07 +0100

    LDEV-4743 Squash core patches updating to version 3.0

--- lams_common/src/java/org/lamsfoundation/lams/dbupdates/patch2040059.sql
+++
@@ -1,8 +0,0 @@
--- Turn off autocommit, so nothing is committed if there is an error
-SET AUTOCOMMIT = 0;
-
--- LDEV-2949 Remove obsolete column
-ALTER TABLE lams_user DROP COLUMN chat_id;
-
-COMMIT;
-SET AUTOCOMMIT = 1;
