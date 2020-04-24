ec7d4d454cc34e581a656f3c507934aa512235cc
Ernie Ghiglione <ernieg@lamsfoundation.org>
2020-04-24 09:54:26 +0800

    LDEV-4997 Use UUID_V4 function to create a unique identifier for LAMS server

--- lams_common/src/java/org/lamsfoundation/lams/dbupdates/patch20200326.sql
+++ lams_common/src/java/org/lamsfoundation/lams/dbupdates/patch20200326.sql
@@ -6,7 +6,7 @@ set FOREIGN_KEY_CHECKS = 0;
 INSERT INTO lams_configuration (config_key, config_value, description_key, header_name, format, required)
 VALUES ('EtherpadServerUrl', 'http://localhost:9001', 'config.etherpad.server.url',  'config.header.etherpad', 'STRING', 0),
 	   ('EtherpadApiKey',    '', 					  'config.etherpad.api.key',     'config.header.etherpad', 'STRING', 0),
-	   ('EtherpadInstanceID','LAMS', 				  'config.etherpad.instance.id','config.header.etherpad', 'STRING', 0);
+	   ('EtherpadInstanceID',UUID_V4(), 				  'config.etherpad.instance.id','config.header.etherpad', 'STRING', 0);
 
 COMMIT;
 SET AUTOCOMMIT = 1;
