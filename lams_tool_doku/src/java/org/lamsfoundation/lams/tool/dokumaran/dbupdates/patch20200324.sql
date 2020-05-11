3be35f313edf8bef8b654f8fb53cfa19d526cd1c
Ernie Ghiglione <ernieg@lamsfoundation.org>
2020-05-11 13:06:52 +0800

    LDEV-5008 backporting to master

--- lams_tool_doku/src/java/org/lamsfoundation/lams/tool/dokumaran/dbupdates/patch20200324.sql
+++ lams_tool_doku/src/java/org/lamsfoundation/lams/tool/dokumaran/dbupdates/patch20200324.sql
@@ -1,12 +1,14 @@
 SET AUTOCOMMIT = 0;
 SET FOREIGN_KEY_CHECKS = 0;
 
--- This patch contains files patch20170303.sql to patch20190103.sql
--- It should upgrade this tool to version 3.1
-
 -- LDEV-4994 Add description field
 
 ALTER TABLE tl_ladoku11_dokumaran ADD COLUMN description MEDIUMTEXT;
+
+-- LDEV-5008 Rename doKumaran tool 
+-- Set default tool content
+UPDATE tl_ladoku11_dokumaran SET description = 'Instructions', instructions = 'Document' where UID = 1;
+
 
 -- If there were no errors, commit and restore autocommit to on
 COMMIT;
