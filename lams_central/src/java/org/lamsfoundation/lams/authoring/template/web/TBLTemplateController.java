b0aa5ff530f717d8edb5a08bc52336837c1e83c8
Marcin Cieślak <mgr.inz.marcin.cieslak@gmail.com>
2020-08-21 15:31:56 +0200

    Revert "LDEV-5015 Temporarily revert to MCQ as IRA for TBL template"
    This reverts commit 59f6851d952d42a5d7b0125959ad3d5f843e7273.

--- lams_central/src/java/org/lamsfoundation/lams/authoring/template/web/TBLTemplateController.java
+++ lams_central/src/java/org/lamsfoundation/lams/authoring/template/web/TBLTemplateController.java
@@ -148,15 +148,16 @@ public class TBLTemplateController extends LdTemplateController {
 			activityTitle, activityTitle));
 	    }
 
-	    // iRA Test - MCQ
+	    // iRA Test - Assessment
 	    currentActivityPosition = calcPositionNextRight(currentActivityPosition);
 	    activityTitle = data.getText("boilerplate.ira.title");
 	    ArrayNode testQuestionsArray = JsonUtil.readArray(data.testQuestions.values());
-	    Long iRAToolContentId = createMCQToolContent(userDTO, activityTitle,
-		    data.getText("boilerplate.ira.instructions"), false, data.confidenceLevelEnable, false,
-		    testQuestionsArray);
-	    ObjectNode iraActivityJSON = createMCQActivity(maxUIID, order++, currentActivityPosition, iRAToolContentId,
-		    data.contentFolderID, groupingUIID, null, null, activityTitle);
+
+	    Long iRAToolContentId = createAssessmentToolContent(userDTO, activityTitle,
+		    data.getText("boilerplate.ira.instructions"), null, false, true, data.confidenceLevelEnable, false,
+		    false, testQuestionsArray);
+	    ObjectNode iraActivityJSON = createAssessmentActivity(maxUIID, order++, currentActivityPosition,
+		    iRAToolContentId, data.contentFolderID, groupingUIID, null, null, activityTitle);
 	    activities.add(iraActivityJSON);
 
 	    // Leader Selection
