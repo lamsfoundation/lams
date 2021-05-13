9bf6bd63c0898b92badeafa0b421879d295d953e
Marcin Cieślak <mgr.inz.marcin.cieslak@gmail.com>
2021-05-13 08:05:17 +0200

    LDEV-5204 Code clean up

--- lams_tool_whiteboard/src/java/org/lamsfoundation/lams/tool/whiteboard/WhiteboardConstants.java
+++ lams_tool_whiteboard/src/java/org/lamsfoundation/lams/tool/whiteboard/WhiteboardConstants.java
@@ -32,28 +32,13 @@ public class WhiteboardConstants {
 
     public static final int COMPLETED = 1;
 
-    // for action forward name
-    public static final String SUCCESS = "success";
-
-    public static final String ERROR = "error";
-
-    public static final String DEFINE_LATER = "definelater";
-
     // for parameters' name
     public static final String PARAM_TOOL_CONTENT_ID = "toolContentID";
 
     public static final String PARAM_TOOL_SESSION_ID = "toolSessionID";
 
-    public static final String PARAM_CURRENT_INSTRUCTION_INDEX = "insIdx";
-
-    public static final String PARAM_OPEN_URL_POPUP = "popupUrl";
-
-    public static final String PARAM_TITLE = "title";
-
     // for request attribute name
     public static final String ATTR_TOOL_CONTENT_ID = "toolContentID";
-
-    public static final String ATTR_TOOL_SESSION_ID = "toolSessionID";
 
     public static final String ATTR_WHITEBOARD = "whiteboard";
     
@@ -65,10 +50,6 @@ public class WhiteboardConstants {
 
     public static final String ATTR_SUMMARY_LIST = "summaryList";
     
-    public static final String ATTR_HAS_FAULTY_SESSION = "hasFaultySession";
-
-    public static final String ATTR_USER_LIST = "userList";
-
     public static final String ATTR_FINISH_LOCK = "finishedLock";
 
     public static final String ATTR_LOCK_ON_FINISH = "lockOnFinish";
@@ -79,10 +60,6 @@ public class WhiteboardConstants {
 
     public static final String ATTR_RESOURCE_FORM = "whiteboardForm";
 
-    public static final String ATTR_ADD_RESOURCE_TYPE = "addType";
-
-    public static final String ATTR_FILE_TYPE_FLAG = "fileTypeFlag";
-
     public static final String ATTR_TITLE = "title";
 
     public static final String ATTR_INSTRUCTIONS = "instructions";
@@ -90,22 +67,8 @@ public class WhiteboardConstants {
     public static final String ATTR_USER_FINISHED = "userFinished";
 
     public static final String ATTR_IS_GROUPED_ACTIVITY = "isGroupedActivity";
-    
-    public static final String ATTR_SECONDS_LEFT = "secondsLeft";
-    
-    public static final String KEY_WHITEBOARD_SERVER_URL = "whiteboardServerUrl";
-    
-    public static final String DEFAULT_PAD_NAME = "LAMS-pad";
-    
-    public static final String PREFIX_REGULAR_GROUP = "LAMS-group-";
-    
-    public static final String PREFIX_SHARED_GROUP = "LAMS-sharedGroup-";
-
-    // error message keys
 
     public static final String PAGE_EDITABLE = "isPageEditable";
-
-    public static final String MODE_AUTHOR_SESSION = "author_session";
 
     public static final String ATTR_REFLECTION_ON = "reflectOn";
 
@@ -114,8 +77,6 @@ public class WhiteboardConstants {
     public static final String ATTR_REFLECTION_ENTRY = "reflectEntry";
 
     public static final String ATTR_REFLECT_LIST = "reflectList";
-
-    public static final String ATTR_USER_UID = "userUid";
 
     public static final String MANUAL_GRADING_DEFINITION_NAME = "manual.grading";
 }
