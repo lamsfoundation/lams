d25fe80a3034172a51168d74cd1476895df0e8cd
Marcin Cieslak <mgr.inz.marcin.cieslak@gmail.com>
2022-04-03 08:03:46 +0200

    LDEV-5304 Update CSRF Guard source code to version 4.1.3

--- 3rdParty_sources/csrfguard/org/owasp/csrfguard/action/SessionAttribute.java
+++ 3rdParty_sources/csrfguard/org/owasp/csrfguard/action/SessionAttribute.java
@@ -1,19 +1,19 @@
-/**
+/*
  * The OWASP CSRFGuard Project, BSD License
- * Eric Sheridan (eric@infraredsecurity.com), Copyright (c) 2011 
+ * Copyright (c) 2011, Eric Sheridan (eric@infraredsecurity.com)
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
- *    1. Redistributions of source code must retain the above copyright notice,
- *       this list of conditions and the following disclaimer.
- *    2. Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *    3. Neither the name of OWASP nor the names of its contributors may be used
- *       to endorse or promote products derived from this software without specific
- *       prior written permission.
+ *     1. Redistributions of source code must retain the above copyright notice,
+ *        this list of conditions and the following disclaimer.
+ *     2. Redistributions in binary form must reproduce the above copyright
+ *        notice, this list of conditions and the following disclaimer in the
+ *        documentation and/or other materials provided with the distribution.
+ *     3. Neither the name of OWASP nor the names of its contributors may be used
+ *        to endorse or promote products derived from this software without specific
+ *        prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -26,24 +26,33 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
 package org.owasp.csrfguard.action;
 
-import javax.servlet.http.*;
+import org.owasp.csrfguard.CsrfGuard;
+import org.owasp.csrfguard.CsrfGuardException;
+import org.owasp.csrfguard.config.properties.ConfigParameters;
+import org.owasp.csrfguard.session.LogicalSession;
 
-import org.owasp.csrfguard.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
 
+/**
+ * Saves the thrown CsrfGuardException object after a token validation to the session, bound to the attribute name extracted from the properties file.
+ */
 public final class SessionAttribute extends AbstractAction {
 
 	private static final long serialVersionUID = 1367492926060283228L;
 
 	@Override
-	public void execute(HttpServletRequest request, HttpServletResponse response, CsrfGuardException csrfe, CsrfGuard csrfGuard) throws CsrfGuardException {
-		String attributeName = getParameter("AttributeName");
-		HttpSession session = request.getSession(false);
+	public void execute(final HttpServletRequest request, final HttpServletResponse response, final CsrfGuardException csrfGuardException, final CsrfGuard csrfGuard) {
+		final String attributeName = getParameter(ConfigParameters.ACTION_ATTRIBUTE_NAME);
 
-		if (session != null) {
-			session.setAttribute(attributeName, csrfe);
+		final LogicalSession logicalSession = CsrfGuard.getInstance().getLogicalSessionExtractor().extract(request);
+
+		if (Objects.nonNull(logicalSession)) {
+			logicalSession.setAttribute(attributeName, csrfGuardException);
 		}
 	}
-	
 }
