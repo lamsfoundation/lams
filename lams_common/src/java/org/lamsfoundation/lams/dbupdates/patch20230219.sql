338146684d996c23fd8ffcad109f44a8a4789da4
Marcin Cieslak <mgr.inz.marcin.cieslak@gmail.com>
2023-02-24 16:18:46 +0100

    SP-4 Remove duplicates before introducing unique index

--- lams_common/src/java/org/lamsfoundation/lams/dbupdates/patch20230219.sql
+++ lams_common/src/java/org/lamsfoundation/lams/dbupdates/patch20230219.sql
@@ -4,6 +4,8 @@ SET FOREIGN_KEY_CHECKS=0;
 -- Put all sql statements below here
 
 -- SP-4 Prevent duplicates in ext user mapping table
+ALTER TABLE lams_ext_user_userid_map ADD KEY UQ_lams_ext_user_userid_map_2 (external_username);
+DELETE a FROM lams_ext_user_userid_map AS a JOIN (SELECT sid, ext_server_org_map_id, external_username FROM lams_ext_user_userid_map GROUP BY ext_server_org_map_id, external_username) AS b USING (ext_server_org_map_id, external_username) WHERE a.sid <> b.sid;
 ALTER TABLE lams_ext_user_userid_map ADD UNIQUE KEY UQ_lams_ext_user_userid_map_1 (ext_server_org_map_id, external_username);
 
 -- Put all sql statements above here
