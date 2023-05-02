187325e829e75b95c41a171d783fafb003fb1ba6
Marcin Cieslak <mgr.inz.marcin.cieslak@gmail.com>
2023-05-02 08:49:34 +0200

    LDEV-5389 Sort learners by names by default in Peer Review monitoring
    Previous default was to sort them by rating descending, so top learners are on top.

--- lams_tool_preview/src/java/org/lamsfoundation/lams/tool/peerreview/web/controller/MonitoringController.java
+++ lams_tool_preview/src/java/org/lamsfoundation/lams/tool/peerreview/web/controller/MonitoringController.java
@@ -23,20 +23,10 @@
 
 package org.lamsfoundation.lams.tool.peerreview.web.controller;
 
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.TimeZone;
-
-import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.log4j.Logger;
 import org.lamsfoundation.lams.logevent.service.ILogEventService;
 import org.lamsfoundation.lams.rating.dto.StyledCriteriaRatingDTO;
@@ -63,17 +53,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.HtmlUtils;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-import com.fasterxml.jackson.databind.node.ObjectNode;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Controller
 @RequestMapping("/monitoring")
@@ -200,11 +190,9 @@ public class MonitoringController {
 	String sortOrder = WebUtil.readStrParam(request, PeerreviewConstants.PARAM_SORD);
 	String sortBy = WebUtil.readStrParam(request, PeerreviewConstants.PARAM_SIDX, true);
 
-	int sorting = PeerreviewConstants.SORT_BY_AVERAGE_RESULT_DESC;
+	int sorting =  PeerreviewConstants.SORT_BY_USERNAME_ASC;
 	if (criteria.isRankingStyleRating()) {
 	    sorting = PeerreviewConstants.SORT_BY_AVERAGE_RESULT_ASC;
-	} else if (criteria.isCommentRating()) {
-	    sorting = PeerreviewConstants.SORT_BY_USERNAME_ASC;
 	}
 
 	if (sortBy != null && sortBy.equals(PeerreviewConstants.PARAM_SORT_NAME)) {
